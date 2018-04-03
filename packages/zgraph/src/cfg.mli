7d586b3078373ee9803ba1258f4d8410121345bc
Dmitry Boldyrev <ribkatt@gmail.com>
2018-04-04 00:38:45 +0300

    cleanup

--- packages/zgraph/src/cfg.mli
+++
@@ -1,8 +0,0 @@
-module type X = sig
-  type jx  (* extension at join point *)
-  type fx  (* extension at fork point *)
-  type nx  (* extension at non-fork/join nodes *)
-  val jx : unit -> jx
-  val fx : unit -> fx
-  val nx : unit -> nx
-end
