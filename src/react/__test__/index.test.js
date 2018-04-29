5c7858da6fd82788ccad5274e354a669e3facace
andretshurotshka <andwebar@gmail.com>
2018-04-29 17:05:53 +0500

    Update React connector test

--- src/react/__test__/index.test.js
+++ src/react/__test__/index.test.js
@@ -11,8 +11,9 @@ import {
 import {createReactState} from '..'
 
 test('basic', () => {
- const store = createStore('hello')
+ const store = createStore('foo')
  const changeText = createEvent('change text')
+ changeText.to(store)
 
  const {Provider, Consumer} = createReactState(store)
 
@@ -23,6 +24,8 @@ test('basic', () => {
    </Consumer>
   </Provider>
  )
+ expect(tree.text()).toMatchSnapshot()
+ changeText('bar')
  expect(tree.text()).toMatchSnapshot()
  tree.unmount()
 })
