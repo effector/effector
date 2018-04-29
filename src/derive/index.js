9ef15750a609734ef4c47f5d46c0a4d3455242bc
Dmitry Boldyrev <ribkatt@gmail.com>
2018-04-29 05:53:38 +0300

    Remove transaction

--- src/derive/index.js
+++ src/derive/index.js
@@ -7,7 +7,7 @@ export {lens} from './lens'
 export {isDerivable, isAtom, isLens, isDerivation} from './types'
 export {struct} from './struct'
 export {unpack} from './unpack'
-export {transact, transaction, atomic, atomically} from './transactions'
+export {atomic, atomically} from './transactions'
 
 // export {Reactor as __Reactor} from './reactors'
 // export {captureDereferences as __captureDereferences} from './parents'
