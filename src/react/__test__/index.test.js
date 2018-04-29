8b61d103d66b249a0f3f3fafd34b777e3239ceca
andretshurotshka <andwebar@gmail.com>
2018-04-29 19:41:01 +0500

    Update react connect tests

--- src/react/__test__/index.test.js
+++ src/react/__test__/index.test.js
@@ -1,14 +1,9 @@
 //@flow
 
 import * as React from 'react'
-import {shallow, mount, render} from 'enzyme'
-import {
- createEvent,
- createStore,
- type Event,
- type Store,
-} from '../../effector'
-import {createReactState} from '..'
+import {mount} from 'enzyme'
+import {createEvent, createStore} from '../../effector'
+import {createReactState, connect} from '..'
 
 test('basic', () => {
  const store = createStore('foo')
@@ -19,13 +14,68 @@ test('basic', () => {
 
  const tree = mount(
   <Provider>
-   <Consumer>
-    {({value, ref}) => <span>{value}</span>}
-   </Consumer>
-  </Provider>
+   <Consumer>{({value, ref}) => <span>{value}</span>}</Consumer>
+  </Provider>,
  )
  expect(tree.text()).toMatchSnapshot()
  changeText('bar')
+ expect(tree.text()).toMatchSnapshot()
+ tree.unmount()
+})
+
+test('refined api', () => {
+ const store = createStore('foo')
+ const changeText = createEvent('change text')
+ changeText.to(store)
+
+ class Display extends React.Component<{
+  text: string,
+  count: number,
+ }> {
+  render() {
+   return (
+    <span>
+     Text: {this.props.text}
+     Counter: {this.props.count}
+    </span>
+   )
+  }
+ }
+ const mappedStore = store.map(text => ({text}))
+ const ConnectedDisplay = mappedStore.thru(connect(Display))
+
+ const tree = mount(<ConnectedDisplay count={1} />)
+ expect(tree.text()).toMatchSnapshot()
+ changeText('bar')
+ expect(tree.text()).toMatchSnapshot()
+ tree.unmount()
+})
+
+test('click counter', () => {
+ const store = createStore(0)
+ const increment = createEvent('increment')
+
+ store.on(increment, (state) => state + 1)
+
+ class Display extends React.Component<{
+  count: number,
+ }> {
+  render() {
+   return (
+    <span>
+     Counter: {this.props.count}
+     <button id="increment" onClick={() => increment()}>Increment</button>
+    </span>
+   )
+  }
+ }
+ const ConnectedDisplay = store
+  .map(count => ({count}))
+  .thru(connect(Display))
+
+ const tree = mount(<ConnectedDisplay />)
+ expect(tree.text()).toMatchSnapshot()
+ tree.find('#increment').simulate('click')
  expect(tree.text()).toMatchSnapshot()
  tree.unmount()
 })
--- src/react/index.js
+++ src/react/index.js
@@ -4,12 +4,16 @@ import * as React from 'react'
 
 import type {Store} from 'effector'
 
-export function createReactState<State>(store: Store<State>): {
-  Provider: React$ComponentType<any>,
-  Consumer: React$ComponentType<{children: (current: {
-    value: State,
-    ref: Store<State>,
-  }) => React$Node}>
+export function createReactState<State>(
+ store: Store<State>,
+): {
+ Provider: React.ComponentType<{children: React.Node}>,
+ Consumer: React.ComponentType<{
+  children: (current: {
+   value: State,
+   ref: Store<State>,
+  }) => React.Node,
+ }>,
 } {
  const {Provider, Consumer} = React.createContext({
   value: store.getState(),
@@ -20,33 +24,55 @@ export function createReactState<State>(store: Store<State>): {
   {
    children: React.Node,
   },
-  { 
-    current: {
-      value: State,
-      ref: Store<State>,
-    }
+  {
+   current: {
+    value: State,
+    ref: Store<State>,
+   },
   },
  > {
   state = {
    current: {
     value: store.getState(),
     ref: store,
-   }
+   },
   }
   componentDidMount() {
    store.watch(value => this.setState({current: {value, ref: store}}))
   }
   render() {
-   return (
-    <Provider value={this.state.current}>
-     {this.props.children}
-    </Provider>
-   )
+   return <Provider value={this.state.current}>{this.props.children}</Provider>
   }
  }
 
  return {
-  Provider: StoreProvider, 
+  Provider: StoreProvider,
   Consumer,
  }
+}
+
+export function createStoreComponent<
+ State: Object,
+ Com: React.ComponentType<*>,
+>(
+ store: Store<State>,
+ Component: Com,
+): React.ComponentType<$Exact<$Diff<React.ElementConfig<Com>, State>>> {
+ const {Provider, Consumer} = createReactState(store)
+ const ConnectedComponent = props => (
+  <Provider>
+   <Consumer>{({value}) => <Component {...props} {...value} />}</Consumer>
+  </Provider>
+ )
+ ConnectedComponent.displayName = `Connect(${Component.displayName
+  || 'Unknown'})`
+ return ConnectedComponent
+}
+
+export function connect<State: Object, Com: React.ComponentType<*>>(
+ Component: Com,
+): (
+ store: Store<State>,
+) => React.ComponentType<$Exact<$Diff<React.ElementConfig<Com>, State>>> {
+ return store => createStoreComponent(store, Component)
 }
