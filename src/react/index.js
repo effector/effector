dcf194a881502330812799f40349b6f3abfc3376
andretshurotshka <andwebar@gmail.com>
2018-04-29 20:31:49 +0500

    Remove duplicate Provider

--- src/react/index.js
+++ src/react/index.js
@@ -1,68 +1,20 @@
 //@flow
 
 import * as React from 'react'
+import {createStoreComponent} from './createStoreComponent'
 
 import type {Store} from 'effector'
 
-export function createReactState<State>(
- store: Store<State>,
-): {
- Provider: React.ComponentType<{children: React.Node}>,
- Consumer: React.ComponentType<{
-  children: (current: {
-   value: State,
-   ref: Store<State>,
-  }) => React.Node,
- }>,
-} {
- const {Provider, Consumer} = React.createContext({
-  value: store.getState(),
-  ref: store,
- })
-
- class StoreProvider extends React.Component<
-  {
-   children: React.Node,
-  },
-  {
-   current: {
-    value: State,
-    ref: Store<State>,
-   },
-  },
- > {
-  state = {
-   current: {
-    value: store.getState(),
-    ref: store,
-   },
-  }
-  componentDidMount() {
-   store.watch(value => this.setState({current: {value, ref: store}}))
-  }
-  render() {
-   return <Provider value={this.state.current}>{this.props.children}</Provider>
-  }
- }
-
- return {
-  Provider: StoreProvider,
-  Consumer,
- }
-}
-
-export function createStoreComponent<
+export function createReactState<
  State: Object,
  Com: React.ComponentType<*>,
 >(
  store: Store<State>,
  Component: Com,
 ): React.ComponentType<$Exact<$Diff<React.ElementConfig<Com>, State>>> {
- const {Provider, Consumer} = createReactState(store)
+ const Store = createStoreComponent(store)
  const ConnectedComponent = props => (
-  <Provider>
-   <Consumer>{({value}) => <Component {...props} {...value} />}</Consumer>
-  </Provider>
+  <Store>{state => <Component {...props} {...state} />}</Store>
  )
  ConnectedComponent.displayName = `Connect(${Component.displayName
   || 'Unknown'})`
@@ -74,5 +26,5 @@ export function connect<State: Object, Com: React.ComponentType<*>>(
 ): (
  store: Store<State>,
 ) => React.ComponentType<$Exact<$Diff<React.ElementConfig<Com>, State>>> {
- return store => createStoreComponent(store, Component)
+ return store => createReactState(store, Component)
 }
