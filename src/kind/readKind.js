ccb99f61d0faaeb80bb6ed2834c5e8ff7fad6bcb
Dmitry Boldyrev <ribkatt@gmail.com>
2018-05-06 08:57:51 +0300

    Remove support for string kinds

--- src/kind/readKind.js
+++ src/kind/readKind.js
@@ -14,29 +14,14 @@ import type {
 function safeKind(kind): Kind {
  // prettier-ignore
  switch (kind) {
-  case (1: Store):
-  case 'store':
-   return (1: Store)
-  case (2: Event):
-  case 'event':
-   return (2: Event)
-  case (3: Effect):
-  case 'effect':
-   return (3: Effect)
-  case (20: Derivation):
-  case 'DERIVATION':
-   return (20: Derivation)
-  case (21: Atom):
-  case 'ATOM':
-   return (21: Atom)
-  case (22: Lens):
-  case 'LENS':
-   return (22: Lens)
-  case (23: Reactor):
-  case 'REACTOR':
-   return (23: Reactor)
-  default:
-   return (-1: None)
+  case (1: Store): return (1: Store)
+  case (2: Event): return (2: Event)
+  case (3: Effect): return (3: Effect)
+  case (20: Derivation): return (20: Derivation)
+  case (21: Atom): return (21: Atom)
+  case (22: Lens): return (22: Lens)
+  case (23: Reactor): return (23: Reactor)
+  default: return (-1: None)
  }
 }
 
