852c51b5bea19e0a3ed9c16c4d964e6362353742
andretshurotshka <andwebar@gmail.com>
2018-06-02 15:13:02 +0500

    Add combineStores function to migrate from redux to effector

---
+++ src/redux/__test__/combineStores.test.js
@@ -0,0 +1,21 @@
+//@flow
+
+import {combineStores} from '../combineStores'
+
+import {createStore, type Store} from 'redux'
+import {createDomain, type Store as EffStore} from 'effector'
+
+test('combineStores', () => {
+ const domain = createDomain()
+ const reduxStore: Store<{test: boolean}> = createStore((state: {test: boolean}) => state)
+ const effectorStore: EffStore<{test2: number}> = domain.store({test2: 1})
+
+ const store = combineStores(reduxStore, effectorStore, (a, b) => ({
+  ...a,
+  ...b,
+ }))
+
+ store.watch(state => {
+  
+ })
+})
