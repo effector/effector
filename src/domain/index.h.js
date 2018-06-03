b9908718b5db6bee433001c0c0f1146927178766
Dmitry Boldyrev <ribkatt@gmail.com>
2018-06-03 21:43:16 +0300

    Add domain hooks to subscribe to events, effects, stores and subdomains creation

--- src/domain/index.h.js
+++ src/domain/index.h.js
@@ -1,15 +1,27 @@
 //@flow
 
 import type {CompositeName} from '../compositeName'
+import type {Subscription} from '../effector/index.h'
 import type {Event} from 'effector/event'
 import type {Effect} from 'effector/effect'
 import type {Store} from 'effector/store'
 
 export type Domain = {
  /*:: +*/ id: string,
+ onCreateEvent(hook: (newEvent: Event<any>) => any): Subscription,
+ onCreateEffect(hook: (newEffect: Effect<any, any, any>) => any): Subscription,
+ onCreateStore(hook: (newStore: Store<any>) => any): Subscription,
+ onCreateDomain(hook: (newDomain: Domain) => any): Subscription,
  event<Payload>(name?: string): Event<Payload>,
  effect<Params, Done, Fail>(name?: string): Effect<Params, Done, Fail>,
  domain(name?: string): Domain,
  store<State>(defaultState: State): Store<State>,
  compositeName: CompositeName,
+}
+
+export type DomainHooks = {
+ domain: Event<Domain>,
+ effect: Event<Effect<any, any, any>>,
+ event: Event<Event<any>>,
+ store: Event<Store<any>>,
 }
