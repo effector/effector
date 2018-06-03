a6bf9bbe31a68cb70d0ef61f66e40b2a7b95a570
Dmitry Boldyrev <ribkatt@gmail.com>
2018-06-03 22:29:33 +0300

    Add tests for domain

---
+++ src/domain/__tests__/name.test.js
@@ -0,0 +1,32 @@
+//@flow
+
+import {createDomain} from '..'
+
+test("should return it's own name on domain.getType()", () => {
+ expect(createDomain('foo').getType()).toBe('foo')
+})
+test('subdomains should has full path in name', () => {
+ const domain = createDomain('dom')
+ const subdomain = domain.domain('subdom')
+ expect(domain.domain('foo').getType()).toBe('dom/foo')
+ expect(subdomain.domain('bar').getType()).toBe('dom/subdom/bar')
+})
+test('empty domain name should be skipped', () => {
+ const domain = createDomain()
+ const subdomain = domain.domain('subdom')
+ expect(domain.domain('foo').getType()).toBe('foo')
+ expect(subdomain.domain('bar').getType()).toBe('subdom/bar')
+})
+describe('empty name support', () => {
+ test('createDomain() should create domain with empty string used as name', () => {
+  expect(createDomain().getType()).not.toBe(undefined)
+  expect(createDomain().getType()).toBe('')
+ })
+ test('domain.domain() should fallback to parent domain name', () => {
+  const domain = createDomain('dom')
+  expect(domain.domain().getType()).not.toBe(undefined)
+  expect(domain.domain().getType()).not.toBe('')
+  expect(domain.domain().getType()).toBe('dom')
+  expect(domain.effect().getType()).not.toBe('dom/')
+ })
+})
