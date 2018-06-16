f0d833a39f34a24f30e254c95acfde2be4e1e44f
Dmitry Boldyrev <ribkatt@gmail.com>
2018-06-17 00:05:37 +0300

    Add test example for filter type inference

--- src/event/__tests__/index.test.js
+++ src/event/__tests__/index.test.js
@@ -39,6 +39,25 @@ test('event.watch(fn)', () => {
  ])
 })
 
+test('event.filter should infer type', () => {
+ const num: Event<number | '-1'> = createEvent('number')
+
+ const evenNum = num.filter(n => {
+  if (n !== '-1') return n
+ })
+
+ evenNum.watch(e => spy(e))
+
+ num(0)
+ num('-1')
+ num(2)
+ num('-1')
+ num(4)
+ ;(evenNum: Event<number>) //Should not fail
+
+ expect(getSpyCalls()).toEqual([[0], [2], [4]])
+})
+
 test('event.filter should drop undefined values', () => {
  const num: Event<number> = createEvent('number')
  const evenNum = num.filter(n => {
