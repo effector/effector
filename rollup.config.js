9da5bea0a542c76923c30821998bb33212481d00
Dmitry Boldyrev <ribkatt@gmail.com>
2018-06-17 01:08:03 +0300

    Update deps, add browser bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import alias from './rollup-alias'
 import {uglify} from 'rollup-plugin-uglify'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
 import visualizer from 'rollup-plugin-visualizer'
 import bucklescript from 'rollup-plugin-bucklescript'
 
@@ -9,34 +11,41 @@ import {resolve as resolvePath} from 'path'
 import {readPackageList, writePackages} from './scripts/monorepoTools'
 
 const packages = readPackageList()
-writePackages(packages)
-const {plugins, presets} = require('./.babelrc.js')
+if (!process.env.BUILD_UMD) writePackages(packages)
 
 const staticPlugins = [
  babel({
-  presets,
-  plugins,
+  // presets,
+  // plugins,
   runtimeHelpers: true,
-  exclude: /\.re/,
+  exclude: /(\.re|node_modules.*)/,
  }),
  bucklescript({module: 'es6', inSource: true}),
- uglify(
-  {
-   mangle: {
-    toplevel: true,
-   },
-   compress: {
-    pure_getters: true,
-   },
-   output: {
-    comments: /#/i,
-    // beautify: true,
-    // indent_level: 2,
-   },
-  },
-  require('uglify-es').minify,
- ),
+ // uglify(
+ //  {
+ //   mangle: {
+ //    toplevel: true,
+ //   },
+ //   compress: {
+ //    pure_getters: true,
+ //   },
+ //   output: {
+ //    comments: /#/i,
+ //    // beautify: true,
+ //    // indent_level: 2,
+ //   },
+ //  },
+ //  require('uglify-es').minify,
+ // ),
 ]
+if (process.env.BUILD_UMD) {
+ staticPlugins.push(
+  replace({
+   'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+  commonjs({}),
+ )
+}
 const rollupPlugins = [
  alias({
   pathMap: new Map([
@@ -55,6 +64,20 @@ const rollupPlugins = [
 ]
 
 function createBuild(name) {
+ if (process.env.BUILD_UMD)
+  return {
+   input: resolvePath(__dirname, 'packages', name, 'index.js'),
+   plugins: [...rollupPlugins],
+   external: ['react'],
+   output: [
+    {
+     file: resolvePath(__dirname, 'npm', name, `${name}.bundle.js`),
+     format: 'iife',
+     name: name.replace(/\-/gi, ''),
+     sourcemap: true,
+    },
+   ],
+  }
  return {
   input: resolvePath(__dirname, 'packages', name, 'index.js'),
   plugins: [
