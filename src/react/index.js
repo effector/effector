cd54ed6c6d2974e5279012809bb691d4a11ed10a
andretshurotshka <andwebar@gmail.com>
2018-11-28 04:27:14 +0500

    Add createContextComponent

--- src/react/index.js
+++ src/react/index.js
@@ -3,6 +3,7 @@
 export {createStoreConsumer} from './createStoreConsumer'
 export {unstable_createStoreProvider} from './createStoreProvider'
 
+export {createContextComponent} from './createContextComponent'
 export {createComponent} from './createComponent'
 export {connect} from './connect'
 export {createReactState} from './createReactState'
