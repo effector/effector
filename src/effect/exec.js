5625f05909d30e6001027f022d77da703a7cfb64
Dmitry <ribkatt@gmail.com>
2018-12-01 13:18:11 +0300

    Use plain promises (fix #25)

--- src/effect/exec.js
+++ src/effect/exec.js
@@ -1,6 +1,6 @@
 //@flow
 
-import {Future} from './future'
+import type {Future} from './future'
 import type {Thunk, Callbacks} from './index.h'
 
 export const exec = <Args, Done, Fail>(
@@ -12,7 +12,7 @@ export const exec = <Args, Done, Fail>(
   let syncError: Fail /*:: = throwSymbol*/
   let req: Promise<Done> | Done /*:: = doneSymbol*/
   let successSync = false
-  const future = new Future(args)
+  let fpromise
   try {
     req = cbs[2](args)
     successSync = true
@@ -20,11 +20,14 @@ export const exec = <Args, Done, Fail>(
     syncError = err
   }
   if (successSync === false) {
-    future.cache = () => undefined
-    future.anyway = () => Promise.resolve(undefined)
-    ;(future: any).then = (rs, rj) => Promise.reject(syncError).then(rs, rj)
+    fpromise = new Promise((_, rj) => {
+      rj(syncError)
+    })
+    fpromise.cache = () => undefined
+    const anyway = Promise.resolve(undefined)
+    fpromise.anyway = () => anyway
     cbs[1]({error: syncError, params: args})
-    return future
+    return fpromise
   }
   if (
     typeof req === 'object'
@@ -32,31 +35,28 @@ export const exec = <Args, Done, Fail>(
     && typeof req.then === 'function'
   ) {
     const then: Promise<Done> = (req: any)
-    future.anyway = () =>
-      new Promise(rs => {
-        then.then(
-          () => {
-            rs()
-          },
-          () => {
-            rs()
-          },
-        )
-      })
-    ;(future: any).then = (rs, rj) => then.then(rs, rj)
-    then.then(
+    fpromise = then.then(
       result => {
-        future.cache = () => result
+        fpromise.cache = () => result
         cbs[0]({result, params: args})
+        return result
       },
-      error => void cbs[1]({error, params: args}),
+      error => {
+        cbs[1]({error, params: args})
+        throw error
+      },
     )
-    return future
+    const anyway = fpromise.then(() => {}, () => {})
+    fpromise.anyway = () => anyway
+    return fpromise
   }
   const done: Done = (req: any)
-  future.cache = () => done
-  future.anyway = () => Promise.resolve(undefined)
-  ;(future: any).then = (rs, rj) => Promise.resolve(done).then(rs, rj)
+  fpromise = new Promise(rs => {
+    rs(done)
+  })
+  fpromise.cache = () => done
+  const anyway = Promise.resolve(undefined)
+  fpromise.anyway = () => anyway
   cbs[0]({result: done, params: args})
-  return future
+  return fpromise
 }
