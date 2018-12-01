83f3a11e55a0977262255349a155c500de3b63cf
Dmitry <ribkatt@gmail.com>
2018-12-01 05:14:53 +0300

    Move typedef to stdlib

---
+++ src/stdlib/typedef.js
@@ -0,0 +1,29 @@
+//@flow
+import invariant from 'invariant'
+export type TypeDef<+Type, +Group> = {
+  +type: Type,
+  +group: Group,
+  +data: any,
+}
+
+function fabricHandler(create) {
+  if (typeof create === 'function') return create
+  return _ => _
+}
+
+declare export function typeDef<T: {+[key: string]: any}, Group>(
+  group: Group,
+  t: T,
+): $ObjMapi<T, <K>(k: K) => (data: any) => TypeDef<K, Group>>
+export function typeDef(group, t) {
+  const result = {}
+  for (const key in t) {
+    const handler = fabricHandler(t[key])
+    result[key] = (...args) => ({
+      type: key,
+      group,
+      data: handler(...args),
+    })
+  }
+  return result
+}
