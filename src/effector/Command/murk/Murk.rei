98f9ba6effe2ea59188693b5b9559ad424b59a67
Dmitry <ribkatt@gmail.com>
2018-12-07 00:40:19 +0300

    Remove unused code

--- src/effector/Command/murk/Murk.rei
+++
@@ -1,9 +0,0 @@
-type t;
-
-let murking: (. 'a) => t;
-
-let demurking: (. t) => 'a;
-
-let lifting: (. t, 'a => 'b) => t;
-
-let murk: (. unit) => t;
--- src/effector/Command/murk/index.js
+++
@@ -1,8 +0,0 @@
-//@flow
-
-export opaque type Murk = any
-export const creating = (): Murk => {}
-export const murking = <T>(val: T): Murk => val
-export const demurking = <T>(val: Murk): T => val
-
-export const lifting = <A, B>(val: Murk, fn: A => B): Murk => fn(val)
