7fea8fdde4a88bbce20617f30c654cc9d88d562d
Dmitry <ribkatt@gmail.com>
2018-12-06 17:22:00 +0300

    Remove unused kind methods

--- src/stdlib/kind.js
+++ src/stdlib/kind.js
@@ -7,37 +7,6 @@ export opaque type kind =
   | 'event'
   | 'effect'
 
-export function matchKind<T>(
-  value: kind,
-  matcher: $Shape<{
-    +none: T,
-    +store: T,
-    +event: T,
-    +effect: T,
-    +__: T,
-  }>,
-) {
-  // assertKind(value)
-  if (value in matcher) return matcher[value]
-  if ('__' in matcher) return matcher.__
-}
-// export function
-export function readKind(value: mixed): kind {
-  let kindValue
-  if (typeof value === 'object' && value !== null) {
-    kindValue = value.kind
-    if (typeof kindValue === 'string') {
-      assertKind(kindValue)
-      //$off
-      return kindValue
-    }
-  } else if (typeof value === 'function' && typeof value.kind === 'string') {
-    assertKind(value.kind)
-    return value.kind
-  }
-  throw new Error(`wrong kind ${String(value)}`)
-}
-
 export const Kind: {
   +none: kind,
   +store: kind,
@@ -48,16 +17,4 @@ export const Kind: {
   store: 'store',
   event: 'event',
   effect: 'effect',
-}
-
-function assertKind(kind: mixed) {
-  switch (kind) {
-    case 'none':
-    case 'store':
-    case 'event':
-    case 'effect':
-      return
-    default:
-      throw new Error(`wrong kind "${String(kind)}"`)
-  }
 }
