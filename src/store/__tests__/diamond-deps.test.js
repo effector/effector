f643a4108038b7b70b75d758ffe5904cf13f114a
Dmitry <ribkatt@gmail.com>
2018-12-06 10:36:00 +0300

    Add diamonds test

---
+++ src/store/__tests__/diamond-deps.test.js
@@ -0,0 +1,18 @@
+//@flow
+
+import {createEvent} from 'effector/event'
+import {createStore} from '..'
+import {combine} from '../../effector'
+
+test('diamonds', () => {
+  const fn = jest.fn()
+  const trigger = createEvent('trigger')
+  const nodeA = createStore(1).on(trigger, x => x + 1)
+  const nodeB = nodeA.map(x => x + 1)
+  const nodeC = nodeB.map(x => x + 1)
+  const nodeD = nodeA.map(x => x + 1)
+  const nodeE = combine(nodeC, nodeD, (c, d) => c + d)
+  nodeE.watch(x => fn(x))
+  trigger()
+  expect(fn.mock.calls).toEqual([[5], [7]])
+})
