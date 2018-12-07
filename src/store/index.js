11940285059bcd397851177c39abdd3334189256
andretshurotshka <andwebar@gmail.com>
2018-12-07 08:54:59 +0500

    Add store naming helper

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
 
 export {storeFabric, getDisplayName} from './storeFabric'
 export {createStore} from './createStore'
-export {setStoreName} from './setStoreName'
+export {setStoreName, storeNaming} from './setStoreName'
 export {createStoreObject, extract} from './createStoreObject'
 export {createApi} from './createApi'
 export {restore, restoreEvent, restoreEffect, restoreObject} from './restore'
