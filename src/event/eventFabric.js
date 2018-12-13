133f46e54fee9d1dad9dd89c2d89dec3618da16c
Dmitry <ribkatt@gmail.com>
2018-12-13 19:06:53 +0300

    merge concreteFabric back to eventFabric

--- src/event/eventFabric.js
+++ src/event/eventFabric.js
@@ -2,7 +2,11 @@
 //@jsx fx
 import fx from 'effector/stdlib/fx'
 
-import {pushNext} from 'effector/stdlib/typedef'
+import {
+  pushNext,
+  type GraphiteMeta,
+  type TypeDef,
+} from 'effector/stdlib/typedef'
 import $$observable from 'symbol-observable'
 import type {Subscription} from '../effector/index.h'
 import type {Event} from './index.h'
@@ -15,8 +19,6 @@ import {Kind, type kind} from 'effector/stdlib/kind'
 import {walkEvent} from 'effector/graphite'
 import {eventRefcount} from '../refcount'
 import {type CompositeName, createName} from '../compositeName'
-
-import fabric from './concreteFabric'
 
 export function eventFabric<Payload>({
   name: nameRaw,
@@ -147,4 +149,74 @@ function makeName(name: string, compositeName?: CompositeName) {
     return name
   }
   return '' + fullName + '/' + name
+}
+const fabric = {
+  event(args: {fullName: string}): GraphiteMeta {
+    const nextSteps = <multi />
+    const stepFull = (
+      <seq>
+        <single>
+          <emit subtype="event" fullName={args.fullName} />
+        </single>
+        {nextSteps}
+      </seq>
+    )
+    const graphite = {next: nextSteps, seq: stepFull}
+    return graphite
+  },
+  prependEvent(args: {|
+    fn: Function,
+    graphite: GraphiteMeta,
+    parentGraphite: GraphiteMeta,
+  |}) {
+    const {fn, graphite, parentGraphite} = args
+    pushNext(
+      <seq>
+        <single>
+          <compute reduce={(_, newValue, ctx) => fn(newValue)} />
+        </single>
+        {parentGraphite.seq}
+      </seq>,
+      graphite.next,
+    )
+  },
+  mapEvent(args: {|
+    fn: Function,
+    graphite: GraphiteMeta,
+    parentGraphite: GraphiteMeta,
+  |}) {
+    const {fn, graphite, parentGraphite} = args
+    pushNext(
+      <seq>
+        <single>
+          <compute reduce={(_, newValue, ctx) => fn(newValue)} />
+        </single>
+        {graphite.seq}
+      </seq>,
+      parentGraphite.next,
+    )
+  },
+  filterEvent(args: {|
+    fn: Function,
+    graphite: GraphiteMeta,
+    parentGraphite: GraphiteMeta,
+  |}) {
+    const {fn, graphite, parentGraphite} = args
+    pushNext(
+      <seq>
+        <single>
+          <compute reduce={(_, newValue, ctx) => fn(newValue)} />
+        </single>
+        <single>
+          <filter
+            filter={(result, ctx: TypeDef<'emitCtx', 'ctx'>) =>
+              result !== undefined
+            }
+          />
+        </single>
+        {graphite.seq}
+      </seq>,
+      parentGraphite.next,
+    )
+  },
 }
