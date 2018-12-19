8722cad1aa3d03ae4101676ac4c009592866b731
Dmitry <ribkatt@gmail.com>
2018-12-19 20:56:00 +0300

    add invariant module

---
+++ src/invariant/index.js
@@ -0,0 +1,34 @@
+'use strict'
+//@flow
+/* eslint-disable */
+import {__DEV__} from 'effector/flags'
+
+var invariant = function(condition, format, a, b, c, d, e, f) {
+  if (__DEV__) {
+    if (format === undefined) {
+      throw new Error('invariant requires an error message argument');
+    }
+  }
+
+  if (!condition) {
+    var error;
+    if (format === undefined) {
+      error = new Error(
+        'Minified exception occurred; use the non-minified dev environment ' +
+        'for the full error message and additional helpful warnings.'
+      );
+    } else {
+      var args = [a, b, c, d, e, f];
+      var argIndex = 0;
+      error = new Error(
+        format.replace(/%s/g, function() { return args[argIndex++]; })
+      );
+      error.name = 'Invariant Violation';
+    }
+
+    error.framesToPop = 1; // we don't care about invariant's own frame
+    throw error;
+  }
+};
+
+module.exports = invariant;
