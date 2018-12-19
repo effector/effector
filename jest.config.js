6552737ec081100ba452a556256e0ea09ec67eb5
Dmitry <ribkatt@gmail.com>
2018-12-19 20:02:50 +0300

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,63 +1,82 @@
 //@flow
 
-//$off
-const {loadYamlSync} = require('./scripts/yaml-util')
-
-const projects = addProjects(loadYamlSync('./jestproject.yml'))
+const createDefaultConfig = () => ({
+  globals: {
+    __DEV__: true,
+  },
+  automock: false,
+  browser: false,
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  // moduleNameMapper: {},
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['node_modules/(?!(bs-platform)/)'],
+  // roots: ['<rootDir>/src/'],
+})
+const projects = [
+  'effector',
+  'effect',
+  'event',
+  'store',
+  'domain',
+  'graphite',
+  'stdlib',
+  'babel',
+]
 module.exports = {
-  projects,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.js',
-    '!**/node_modules/**',
-    '!**/__tests__/**',
-    '!**/store/epic.js',
-    '!**/store/epic.js',
-    '!**/DataStructures/**',
-    '!**/graphite/tarjan/**',
-    '!**/babel/**',
-    '!**/fixtures/**',
-  ],
-}
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   '**/*.js',
+  //   '!**/node_modules/**',
+  //   '!**/__tests__/**',
+  //   '!**/store/epic.js',
+  //   '!**/store/epic.js',
+  //   '!**/DataStructures/**',
+  //   '!**/graphite/tarjan/**',
+  //   '!**/babel/**',
+  //   '!**/fixtures/**',
+  // ],
 
-function packageTest(displayName, opts = {}) {
-  return Object.assign(
-    {
-      displayName,
+  watchPlugins: ['jest-runner-eslint/watch-fix'],
+  projects: [
+    ...projects.map(displayName =>
+      Object.assign({}, createDefaultConfig(), {
+        displayName,
+        testMatch: [
+          `<rootDir>/src/${displayName}/__tests__/**/*.(test|spec).js`,
+          `<rootDir>/src/${displayName}/**/__tests__/**/*.(test|spec).js`,
+        ],
+        collectCoverage: true,
+        collectCoverageFrom: [
+          `<rootDir>/src/${displayName}/**/*.js`,
+          // `!<rootDir>/src/${displayName}/__tests__/**/*.(test|spec).js`,
+          // `!<rootDir>/src/${displayName}/**/__tests__/**/*.(test|spec).js`,
+          // '!**/node_modules/**',
+          '!**/__tests__/**',
+          // '!**/store/epic.js',
+          // '!**/store/epic.js',
+          // '!**/DataStructures/**',
+          // '!**/graphite/tarjan/**',
+          // '!**/babel/**',
+          // '!**/fixtures/**',
+        ],
+      }),
+    ),
+    Object.assign({}, createDefaultConfig(), {
+      displayName: 'react',
+      testEnvironment: 'jsdom',
       testMatch: [
-        `<rootDir>/src/${displayName}/__tests__/**/*.(test|spec).js`,
-        `<rootDir>/src/${displayName}/**/__tests__/**/*.(test|spec).js`,
+        `<rootDir>/src/react/__tests__/**/*.(test|spec).js`,
+        `<rootDir>/src/react/**/__tests__/**/*.(test|spec).js`,
       ],
-      globals: {
-        __DEV__: true,
-      },
-      // collectCoverage: true,
-      automock: false,
-      browser: false,
-      testEnvironment: 'node',
-      transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-      },
-      moduleNameMapper: {},
-      testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-      transformIgnorePatterns: ['node_modules/(?!(bs-platform)/)'],
-      roots: ['<rootDir>/src/'],
+      setupFiles: ['<rootDir>/src/fixtures/performance.mock.js'],
+    }),
+    {
+      runner: 'jest-runner-eslint',
+      displayName: 'lint',
+      testMatch: ['<rootDir>/src/**/*.js', '!**/DataStructures/**'],
     },
-    opts,
-  )
-}
-
-function addProjects({tests, include}) {
-  const projects = []
-  for (const pkg of tests) {
-    if (typeof pkg === 'string') {
-      if (include[pkg]) projects.push(packageTest(pkg))
-    } else {
-      const [name, config] = pkg
-      if (include[name]) {
-        projects.push(packageTest(name, config))
-      }
-    }
-  }
-  return projects
+  ],
 }
