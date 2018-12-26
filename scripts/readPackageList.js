7f39b1eceb16a76e96d39ff0d698e6c5689bae3f
andretshurotshka <andwebar@gmail.com>
2018-12-27 02:11:15 +0500

    Include bucklescript packages in package list

--- scripts/readPackageList.js
+++ scripts/readPackageList.js
@@ -6,7 +6,8 @@ const {readdirSync} = require('fs-extra')
 const rootDir = process.cwd()
 
 module.exports = function readPackageList() {
-  return readdirSync(join(rootDir, 'packages')).filter(
-    e => ['bs-effector', 'bs-effector-react'].includes(e) === false,
-  )
+  return readdirSync(join(rootDir, 'packages'))
+  // .filter(
+  //   e => ['bs-effector', 'bs-effector-react'].includes(e) === false,
+  // )
 }
