e09cc9923eb144aab0d0fab167725870fc98ef57
andretshurotshka <andwebar@gmail.com>
2018-12-27 10:48:13 +0500

    Disable toplevel mangle in umd

--- tools/builder/rollup.js
+++ tools/builder/rollup.js
@@ -15,10 +15,18 @@ import {sizeSnapshot} from 'rollup-plugin-size-snapshot'
 
 import {dir, getSourcemapPathTransform} from './utils'
 // import
-const minifyConfig = ({prettify}: {prettify: boolean}) => ({
+
+const minifyConfig = ({
+  prettify,
+  toplevel = true,
+}: {
+  prettify: boolean,
+  top_retain?: ?RegExp,
+  toplevel?: boolean,
+}) => ({
   ecma: 8,
   mangle: {
-    toplevel: true,
+    toplevel,
   },
   compress: {
     pure_getters: true,
@@ -34,7 +42,10 @@ const minifyConfig = ({prettify}: {prettify: boolean}) => ({
     },
 })
 
-export const getPlugins = () => ({
+export const getPlugins = (
+  name: string,
+  type: 'umd' | 'cjs' | 'es' = 'cjs',
+) => ({
   babel: babel({
     // runtimeHelpers: true,
     // exclude: /(\.re|node_modules.*)/,
@@ -44,15 +55,21 @@ export const getPlugins = () => ({
   }),
   commonjs: commonjs({}),
   resolve: resolve({}),
-  terser: terser(minifyConfig({prettify: !!process.env.PRETTIFY})),
+  terser: terser(
+    minifyConfig({
+      prettify: !!process.env.PRETTIFY,
+      toplevel: type !== 'umd',
+    }),
+  ),
   sizeSnapshot: sizeSnapshot(),
 })
 
 export async function rollupEffector() {
+  const name = 'effector'
   const run = async output => {
-    const plugins = getPlugins()
+    const plugins = getPlugins(name, output.format)
     const build = await rollup({
-      input: (dir('packages/effector/index.js'): string),
+      input: (dir(`packages/${name}/index.js`): string),
       external: [
         'warning',
         'invariant',
@@ -73,10 +90,10 @@ export async function rollupEffector() {
     await build.write(output)
   }
   async function umd() {
-    const plugins = getPlugins()
+    const plugins = getPlugins(name, 'umd')
     //$off
     const build = await rollup({
-      input: String(dir('packages/effector/index.js')),
+      input: String(dir(`packages/${name}/index.js`)),
       plugins: [
         plugins.resolve,
         plugins.babel,
@@ -89,26 +106,160 @@ export async function rollupEffector() {
     })
 
     await build.write({
-      file: (dir('npm/effector/effctor.bundle.js'): string),
+      file: (dir(`npm/${name}/${name}.bundle.js`): string),
       format: 'iife',
-      name: 'effector',
+      name,
       sourcemap: true,
     })
   }
   await Promise.all([
     run({
-      file: dir('npm/effector/effector.cjs.js'),
+      file: dir(`npm/${name}/${name}.cjs.js`),
       format: 'cjs',
-      name: 'effector',
-      sourcemap: ((true: any): boolean | 'inline'),
-      sourcemapPathTransform: getSourcemapPathTransform('effector'),
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
     }),
     run({
-      file: dir('npm/effector/effector.es.js'),
+      file: dir(`npm/${name}/${name}.es.js`),
       format: 'es',
-      name: 'effector',
-      sourcemap: ((true: any): boolean | 'inline'),
-      sourcemapPathTransform: getSourcemapPathTransform('effector'),
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
+    }),
+    umd(),
+  ])
+}
+
+export async function rollupEffectorReact() {
+  const name = 'effector-react'
+  const run = async output => {
+    const plugins = getPlugins(name, output.format)
+    const build = await rollup({
+      input: (dir(`packages/${name}/index.js`): string),
+      external: [
+        'warning',
+        'invariant',
+        'react',
+        'vue',
+        'most',
+        'symbol-observable',
+        'effector',
+      ],
+      plugins: [
+        plugins.resolve,
+        plugins.babel,
+        plugins.terser,
+        plugins.sizeSnapshot,
+      ],
+    })
+
+    await build.write(output)
+  }
+  async function umd() {
+    const plugins = getPlugins(name, 'umd')
+    //$off
+    const build = await rollup({
+      input: String(dir(`packages/${name}/index.js`)),
+      plugins: [
+        plugins.resolve,
+        plugins.babel,
+        plugins.replace,
+        plugins.commonjs,
+        plugins.terser,
+        plugins.sizeSnapshot,
+      ],
+      external: ['react', 'effector'],
+    })
+
+    await build.write({
+      file: (dir(`npm/${name}/${name}.bundle.js`): string),
+      format: 'iife',
+      name: 'effectorReact',
+      sourcemap: true,
+    })
+  }
+  await Promise.all([
+    run({
+      file: dir(`npm/${name}/${name}.cjs.js`),
+      format: 'cjs',
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
+    }),
+    run({
+      file: dir(`npm/${name}/${name}.es.js`),
+      format: 'es',
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
+    }),
+    umd(),
+  ])
+}
+
+export async function rollupEffectorVue() {
+  const name = 'effector-vue'
+  const run = async output => {
+    const plugins = getPlugins(name, output.format)
+    const build = await rollup({
+      input: (dir(`packages/${name}/index.js`): string),
+      external: [
+        'warning',
+        'invariant',
+        'react',
+        'vue',
+        'most',
+        'symbol-observable',
+        'effector',
+      ],
+      plugins: [
+        plugins.resolve,
+        plugins.babel,
+        plugins.terser,
+        plugins.sizeSnapshot,
+      ],
+    })
+
+    await build.write(output)
+  }
+  async function umd() {
+    const plugins = getPlugins(name, 'umd')
+    //$off
+    const build = await rollup({
+      input: String(dir(`packages/${name}/index.js`)),
+      plugins: [
+        plugins.resolve,
+        plugins.babel,
+        plugins.replace,
+        plugins.commonjs,
+        plugins.terser,
+        plugins.sizeSnapshot,
+      ],
+      external: ['vue', 'effector'],
+    })
+
+    await build.write({
+      file: (dir(`npm/${name}/${name}.bundle.js`): string),
+      format: 'iife',
+      name: 'effectorVue',
+      sourcemap: true,
+    })
+  }
+  await Promise.all([
+    run({
+      file: dir(`npm/${name}/${name}.cjs.js`),
+      format: 'cjs',
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
+    }),
+    run({
+      file: dir(`npm/${name}/${name}.es.js`),
+      format: 'es',
+      name,
+      sourcemap: true,
+      sourcemapPathTransform: getSourcemapPathTransform(name),
     }),
     umd(),
   ])
