5ea895b12a3fcdee3ad9ca7f4e875495ecfee70c
Dmitry <ribkatt@gmail.com>
2018-12-27 12:17:37 +0300

    Add Edge as build target

--- babel.config.js
+++ babel.config.js
@@ -56,6 +56,7 @@ module.exports = api => {
             'last 2 Chrome versions',
             'last 2 Firefox versions',
             'last 2 Safari versions',
+            'last 2 Edge versions',
           ],
         },
       },
