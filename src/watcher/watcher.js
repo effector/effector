598610b29c34ae854aedd9cf7957e68316eeeb29
Dmitry <ribkatt@gmail.com>
2019-01-23 20:10:02 +0300

    inline disposer

--- src/watcher/watcher.js
+++ src/watcher/watcher.js
@@ -7,25 +7,24 @@ const noopIndexOf = () => -1
 //eslint-disable-next-line no-unused-vars
 const noopSplice = (i: number, n: number) => []
 
-function unsubscriber() {
+function disposer() {
   const i = this.indexOf()
   if (i === -1) return
   this.splice(i, 1)
   this.indexOf = noopIndexOf
   this.splice = noopSplice
 }
-const initUnsubscribe = (obj): Watcher => {
-  const result = unsubscriber.bind(obj)
-  result.unsubscribe = result
-  return (result: $todo)
-}
+
 export const createWatcher = (opts: {
   child: TypeDef<*, 'cmd' | 'step'>,
   parent: GraphiteMeta,
 }): Watcher => {
   const subscribers = opts.parent.next.data
-  return initUnsubscribe({
+  const instance = {
     indexOf: subscribers.indexOf.bind(subscribers, opts.child),
     splice: subscribers.splice.bind(subscribers),
-  })
+  }
+  const result = disposer.bind(instance)
+  result.unsubscribe = disposer.bind(instance)
+  return (result: $todo)
 }
