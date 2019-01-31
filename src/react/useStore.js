8fc2348255fdb483a7a982f6a40e2f90391606c2
andretshurotshka <andwebar@gmail.com>
2019-01-31 17:54:11 +0500

    Use classic useStore

--- src/react/useStore.js
+++ src/react/useStore.js
@@ -4,13 +4,7 @@ import type {Store} from 'effector'
 import {useState, useEffect} from 'react'
 
 export function useStore<State>(store: Store<State>): State {
-  const [state, setState] = useState(store.getState())
-  useEffect(
-    () =>
-      store.watch(state => {
-        setState(state)
-      }),
-    [],
-  )
+  const [state, setState] = useState(store.getState)
+  useEffect(() => store.watch(setState), [store])
   return state
 }
