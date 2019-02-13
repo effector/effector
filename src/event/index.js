c87c3ca0105e4d244fecc9609d080d7fdaa4a608
Dmitry <ribkatt@gmail.com>
2019-02-13 16:57:55 +0300

    export relay and relayShape

--- src/event/index.js
+++ src/event/index.js
@@ -1,6 +1,8 @@
 //@flow
 
-export {eventFabric, forward} from './eventFabric'
+export {eventFabric} from './eventFabric'
+export {forward} from './forward'
+export {relay, relayShape} from './relay'
 export {createEvent} from './createEvent'
 export type {Event} from './index.h'
 export {fromObservable} from './fromObservable'
