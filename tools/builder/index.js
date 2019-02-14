180c2c244f55c820070e584d788aeb69e5b4be6a
andretshurotshka <andwebar@gmail.com>
2019-02-14 18:49:20 +0500

    Add README builder

--- tools/builder/index.js
+++ tools/builder/index.js
@@ -17,6 +17,7 @@ import {
   loadYaml,
 } from './utils'
 import {taskList} from './taskList'
+import table from 'markdown-table'
 
 const scope = {
   '@effector/babel-plugin': [
@@ -192,6 +193,47 @@ taskList({
       async() => {
         const configRaw = await loadYaml('packages.yml')
         const config = validateConfig(configRaw)
+        const packagesByCategory = new Map()
+        for (const pkg of config.packages) {
+          if (!packagesByCategory.has(pkg.category)) {
+            packagesByCategory.set(pkg.category, [pkg])
+          } else {
+            packagesByCategory.set(pkg.category, [
+              ...packagesByCategory.get(pkg.category),
+              pkg,
+            ])
+          }
+        }
+
+        const npmVersionBadge = name =>
+          `[![npm](https://img.shields.io/npm/v/${name}.svg?maxAge=3600)](https://www.npmjs.com/package/${name})`
+        const npmDepsBadge = name =>
+          `[![Dependency Status](https://david-dm.org/zerobias/effector.svg?path=packages/${name})](https://david-dm.org/zerobias/effector?path=packages/${name})`
+        const markdown = `
+# Monorepo
+${config.categories
+    .map(({name, description}) => {
+      const mdHeader = `### ${description}`
+      const data = packagesByCategory.get(name) || []
+      const rawTable = [
+        ['Package', 'Version', 'Dependencies'],
+        ...data.map(pkg => [
+          `[\`${pkg.name}\`](${pkg.name})`,
+          npmVersionBadge(pkg.name),
+          npmDepsBadge(pkg.name),
+        ]),
+      ]
+      const mdTable = table(rawTable, {align: 'c'})
+      return `
+
+${mdHeader}
+
+${mdTable}
+`
+    })
+    .join('\n')}
+`
+        await fs.outputFile('./packages/README.md', markdown)
       },
     ],
   },
