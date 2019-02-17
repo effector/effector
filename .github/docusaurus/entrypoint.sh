de0d2436ab714b51c9349ff0ac728690a678c6cd
andretshurotshka <andwebar@gmail.com>
2019-02-18 00:22:09 +0500

    Use global user.email and user.name in Docusaurus action

--- .github/docusaurus/entrypoint.sh
+++ .github/docusaurus/entrypoint.sh
@@ -4,8 +4,8 @@ set -eu
 
 cd website
 
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 yarn install && GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
