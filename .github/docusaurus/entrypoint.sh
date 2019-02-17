d878dd642c357fd1d4e2c3aec4483d8e064df0cb
andretshurotshka <andwebar@gmail.com>
2019-02-17 15:53:19 +0500

    Update Docusaurus action

--- .github/docusaurus/entrypoint.sh
+++ .github/docusaurus/entrypoint.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
+set -eu
+
 cd website
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-echo "machine github.com login ${GITHUB_ACTOR password ${GITHUB_TOKEN}" > ~/.netrc
+echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 yarn install && GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
