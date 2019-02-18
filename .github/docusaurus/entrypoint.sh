ffcf196ad5759298e8aafc145dbbc72af983690b
andretshurotshka <andwebar@gmail.com>
2019-02-18 05:14:59 +0500

    Build Try page before deploying to Github

--- .github/docusaurus/entrypoint.sh
+++ .github/docusaurus/entrypoint.sh
@@ -8,4 +8,6 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
-yarn install && GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
+yarn install
+yarn build:try
+GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
