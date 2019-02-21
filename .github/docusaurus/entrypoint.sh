24fdbbf8e577eeb502f43670ec856c5b3536d159
Dmitry <ribkatt@gmail.com>
2019-02-21 03:18:42 +0300

    add now.sh to docusaurus script

--- .github/docusaurus/entrypoint.sh
+++ .github/docusaurus/entrypoint.sh
@@ -9,5 +9,10 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.netrc
 
 yarn install
+yarn add -D now
 yarn build:try
-GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
+yarn build
+# GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
+cd build/effector
+echo '{"name": "effector", "alias": ["effector.now.sh"], "version": 2}' > now.json
+npx now --token "${NOW_TOKEN}"
