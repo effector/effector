0f0bedfaffd6aae499afcbc3c766ac25976e8b23
andretshurotshka <andwebar@gmail.com>
2019-02-21 06:36:18 +0500

    Update entrypoint.sh

--- .github/docusaurus/entrypoint.sh
+++ .github/docusaurus/entrypoint.sh
@@ -11,9 +11,9 @@ echo "machine github.com login ${GITHUB_ACTOR} password ${GITHUB_TOKEN}" > ~/.ne
 yarn install
 yarn add -D now
 yarn build:try
-SITE_URL=https://effector.js.org yarn build
 SITE_URL=https://effector.js.org GIT_USER="${GITHUB_ACTOR}" yarn run publish-gh-pages
 
+SITE_URL=https://effector.now.sh yarn build
 cd build/effector
 echo '{"name": "effector", "alias": ["effector.now.sh"], "version": 2}' > now.json
 npx now --token "${NOW_TOKEN}" --force --no-clipboard
