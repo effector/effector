143c837ff7d019b149fea8c358277ad8f6b0ca6a
Dmitry <ribkatt@gmail.com>
2019-02-24 02:28:42 +0300

    use prism highlight

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -83,7 +83,7 @@ const siteConfig = {
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} zerobias`,
-
+  usePrism: ['jsx', 'flow', 'typescript'],
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
