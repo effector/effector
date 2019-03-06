9382442475adc86111562308c0f9d4f8c731db16
Zero Bias <ribkatt@gmail.com>
2019-03-06 07:20:05 +0300

    add isDomain

--- src/stdlib/kind.js
+++ src/stdlib/kind.js
@@ -5,15 +5,18 @@ export opaque type kind: string =
   | 'store'
   | 'event'
   | 'effect'
+  | 'domain'
 
 export const Kind: {
   +store: kind,
   +event: kind,
   +effect: kind,
+  +domain: kind,
 } = {
   store: 'store',
   event: 'event',
   effect: 'effect',
+  domain: 'domain',
 }
 
 export const isUnit = (obj: mixed) =>
@@ -27,3 +30,5 @@ export const isStore = (obj: mixed) => isUnit(obj) && obj.kind === 'store'
 export const isEvent = (obj: mixed) => isUnit(obj) && obj.kind === 'event'
 //$off
 export const isEffect = (obj: mixed) => isUnit(obj) && obj.kind === 'effect'
+//$off
+export const isDomain = (obj: mixed) => isUnit(obj) && obj.kind === 'domain'
