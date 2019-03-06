9b4c3edbc6f545c2d27a07b389f6d64aa5c72459
andretshurotshka <andwebar@gmail.com>
2019-03-06 14:48:05 +0500

    Update forms types

--- packages/@effector/forms/index.d.ts
+++ packages/@effector/forms/index.d.ts
@@ -6,34 +6,34 @@ type SpreadProps<A, B> = A & B
 //   ...$Call<(empty => {||}) & (<V>(V) => $Exact<V>), B>,
 // }>
 
-export type FormApi<Values, Errors: {[key: string]: any}> = {
-  values: Store<Values>,
-  errors: Store<Errors>,
-  isValid: Store<boolean>,
-  submitted: Store<boolean>,
-  loading: Store<boolean>,
-  handleSubmit: Event<void | {preventDefault(): void}>,
-  reset: Event<void>,
-  handle: $ObjMap<Values, <V>(V) => Event<{currentTarget: V}>>,
-  api: $ObjMap<Values, <V>(V) => Event<V>>,
+export type FormApi<Values, Errors extends {[key: string]: any}> = {
+  values: Store<Values>
+  errors: Store<Errors>
+  isValid: Store<boolean>
+  submitted: Store<boolean>
+  loading: Store<boolean>
+  handleSubmit: Event<void | {preventDefault(): void}>
+  reset: Event<void>
+  handle: {[K in keyof Values]: Event<{currentTarget: Values[K]}>}
+  api: {[K in keyof Values]: Event<Values[K]>}
 }
 
-export declare function createFormApi<
-  Form: {[key: string]: any},
-  External: {[key: string]: any},
-  Errors: {[key: string]: any},
+export function createFormApi<
+  Form extends {[key: string]: any},
+  External extends {[key: string]: any},
+  Errors extends {[key: string]: any},
   Data,
-  Error,
+  Error
 >(config: {
-  fields: Form,
+  fields: Form
   validate?: (
     values: SpreadProps<Form, External>,
-    data: {result?: Data, error?: Error},
-  ) => Errors,
-  external?: Store<External>,
-  submitEvent?: Event<SpreadProps<Form, External>>,
-  submitEffect?: Effect<SpreadProps<Form, External>, Data, Error>,
-  initialValues?: Store<?Form> | Store<Form>,
-  domain?: Domain,
-  resetOnSubmit?: boolean,
+    data: {result?: Data; error?: Error},
+  ) => Errors
+  external?: Store<External>
+  submitEvent?: Event<SpreadProps<Form, External>>
+  submitEffect?: Effect<SpreadProps<Form, External>, Data, Error>
+  initialValues?: Store<Form | null> | Store<Form>
+  domain?: Domain
+  resetOnSubmit?: boolean
 }): FormApi<Form, Errors>
