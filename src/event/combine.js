f191041eca9a711a75ce455d252eddf7a3c12a7f
andretshurotshka <andwebar@gmail.com>
2019-03-10 22:37:11 +0500

    Add event combine

---
+++ src/event/combine.js
@@ -0,0 +1,34 @@
+//@flow
+
+import {unitObjectArrayName} from 'effector/naming'
+
+import type {Event} from './index.h'
+import {eventFabric} from './eventFabric'
+
+export function combine<T>(
+  f: (...args: any[]) => T,
+  ...events: $ReadOnlyArray<Event<any>>
+): Event<T> {
+  const unit = eventFabric({
+    name: unitObjectArrayName(events),
+  })
+  const l = events.length
+  let awaiting = l
+  const values = new Array(l)
+  const hasValue = new Array(l).fill(false)
+  for (const [index, event] of events.entries()) {
+    event.watch(value => {
+      if (awaiting > 0) {
+        if (!hasValue[index]) {
+          hasValue[index] = true
+          awaiting -= 1
+        }
+      }
+      values[index] = value
+      if (awaiting === 0) {
+        unit(f(...values))
+      }
+    })
+  }
+  return unit
+}
