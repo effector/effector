abb2dacc9ea20ae6001ed3aae640ad5e2ba3baaf
andretshurotshka <andwebar@gmail.com>
2019-03-17 11:14:53 +0500

    Use 1 Edge version

--- babel.config.js
+++ babel.config.js
@@ -65,7 +65,7 @@ module.exports = api => {
             'last 2 Chrome versions',
             'last 2 Firefox versions',
             'last 2 Safari versions',
-            'last 2 Edge versions',
+            'last 1 Edge versions',
           ],
         },
       },
