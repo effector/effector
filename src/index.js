1a98818fdb91dcbe9593213734bfe2209e7a136a
andretshurotshka <andwebar@gmail.com>
2019-03-22 21:40:20 +0500

    Disable relay and relayShape

--- src/index.js
+++ src/index.js
@@ -6,13 +6,7 @@ export type {Domain} from 'effector/domain'
 export {createDomain} from 'effector/domain'
 
 export type {Event} from 'effector/event'
-export {
-  createEvent,
-  forward,
-  fromObservable,
-  relay,
-  relayShape,
-} from 'effector/event'
+export {createEvent, forward, fromObservable} from 'effector/event'
 
 export type {Effect} from 'effector/effect'
 export {createEffect} from 'effector/effect'
@@ -40,4 +34,7 @@ export {
   isDomain,
 } from 'effector/stdlib'
 export type {kind} from 'effector/stdlib'
+
+export {default as invariant} from 'invariant'
+export {default as warning} from 'warning'
 export {version} from 'effector/flags'
