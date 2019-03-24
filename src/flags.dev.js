ca9af19749abdd309db7a3e856131af458b351b8
Zero Bias <ribkatt@gmail.com>
2019-03-24 11:54:00 +0300

    export version from package.json

--- src/flags.dev.js
+++ src/flags.dev.js
@@ -2,7 +2,6 @@
 
 export const __DEBUG__ = true
 export const __DEV__ = process.env.NODE_ENV !== 'production'
-
 export const __CANARY__ = false
 
-export const version = '0.18.3'
+export {version} from '../packages/effector/package.json'
--- src/flags.prod.js
+++ src/flags.prod.js
@@ -4,4 +4,4 @@ export const __DEBUG__ = false
 export const __DEV__ = process.env.NODE_ENV !== 'production'
 export const __CANARY__ = false
 
-export const version = '0.18.3'
+export {version} from '../packages/effector/package.json'
