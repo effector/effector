25c13bee9281a364943cd87a96303ca3ff2e4b20
Zero Bias <ribkatt@gmail.com>
2019-03-25 21:43:00 +0300

    refactor rest of kernel modules

--- src/graphite/index.h.js
+++ src/graphite/index.h.js
@@ -10,22 +10,17 @@ export type CtxKind =
   | 'run'
   | 'combine'
 export type CommonCtx = {arg: any, ...}
-export type Reg = {
-  isChanged: boolean,
-  isFailed: boolean,
-}
-export type Meta = {
+
+export type Meta = {|
   callstack: Array<TypeDef<*, *>>,
   stop: boolean,
-  transactions: Array<() => void>,
   scope: Array<any>,
   pendingEvents: Array<{
     event: (data: any) => any,
     data: any,
   }>,
-  reg: Reg,
   val: {[name: string]: StateRef},
-}
+|}
 
 export type Command<Local> = {
   cmd(meta: Meta, local: Local): void,
@@ -42,11 +37,28 @@ export type CommandList = $ReadOnly<{
 }>
 
 export type StepVisitor = (meta: Meta) => void
-export type ScopeIterator = {
-  +index: number,
-  +body: Array<any>,
-  value(): any,
-  bottom(): any,
-  down(): boolean,
-  up(): boolean,
-}
+
+export type Line = {|
+  +step: TypeDef<*, 'step'>,
+  +pre: Array<PostAction>,
+  +post: Array<PostAction>,
+|}
+
+export type Area = {|
+  +list: $ReadOnlyArray<Line>,
+  +pre: Array<PostAction>,
+  +post: Array<PostAction>,
+  index: number,
+  active: boolean,
+|}
+export type Pos = {|
+  head: number,
+  +stack: Array<Area>,
+|}
+export type PostAction =
+  | {|+type: 'meta/?stop'|}
+  | {|+type: 'meta/!stop'|}
+  | {|+type: 'head/--'|}
+  | {|+type: 'callstack/pop'|}
+  | {|+type: 'scope/size', +size: number|}
+  | {|+type: 'scope/push', +scope: {+arg: any}|}
--- src/graphite/meta.js
+++ src/graphite/meta.js
@@ -1,56 +1,17 @@
 //@flow
-import {type TypeDef, createStateRef} from 'effector/stdlib'
+import type {TypeDef} from 'effector/stdlib'
 
 import type {Meta, CommonCtx} from './index.h'
 
-export const cloneMeta = (meta: Meta): Meta => {
-  const val: typeof meta.val = cloneVals(meta.val)
-  const transactions: typeof meta.transactions = []
-  const reg: typeof meta.reg = {
-    isChanged: meta.reg.isChanged,
-    isFailed: meta.reg.isFailed,
-  }
-  return {
-    callstack: meta.callstack.slice(),
-    reg,
-    stop: meta.stop,
-    scope: meta.scope.slice(),
-    val,
-    //TODO avoid this
-    transactions,
-    pendingEvents: [],
-  }
-}
 export const val = (tag: string, meta: Meta) => meta.val[tag].current
 export const step = (meta: Meta): TypeDef<*, 'step'> =>
   meta.callstack[meta.callstack.length - 1]
 export const single = (meta: Meta) => step(meta).data.data
-export const newMeta = (ctx: CommonCtx): Meta => ({
+export const newMeta = (/*ctx: CommonCtx*/): Meta => ({
   callstack: [],
-  transactions: [],
   pendingEvents: [],
   stop: false,
-  ctx,
-  reg: {
-    isChanged: true,
-    isFailed: false,
-  },
+  // ctx,
   scope: [],
   val: {},
 })
-
-const cloneVals = obj => {
-  const result = {}
-  for (const key in obj) {
-    result[key] = createStateRef(obj[key].current)
-  }
-  return result
-}
-
-const stateRefs = obj => {
-  const result = {}
-  for (const key in obj) {
-    result[key] = createStateRef(obj[key])
-  }
-  return result
-}
