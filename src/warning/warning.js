70015c8916acf50834871d845944a2f1b8403484
Zero Bias <ribkatt@gmail.com>
2019-03-25 22:54:05 +0300

    fix output in warning

--- src/warning/warning.js
+++ src/warning/warning.js
@@ -1,13 +1,11 @@
 //@flow
-/* eslint-disable */
-import {formatter} from '../effector/logger'
 import {__DEV__} from 'effector/flags'
 
 let warning = function(/*::condition: any, format: string, ...args: any*/) {}
 if (__DEV__) {
   warning = function(condition: any, format: string, ...args: any) {
     if (!condition) {
-      console.error('Warning: ' + formatter(format, ...args))
+      console.error('Warning: ' + format, ...args)
     }
   }
 }
