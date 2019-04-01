ed24cafd92a5ad5ad6f3370e4097cb9018eaf5c0
Zero Bias <ribkatt@gmail.com>
2019-04-02 02:33:20 +0300

    add getGraph, add clearNode

--- src/stdlib/graph.js
+++ src/stdlib/graph.js
@@ -1,6 +1,6 @@
 //@flow
 
-import type {Graph, Cmd} from './index.h'
+import type {Graph, Graphite, Cmd} from './index.h'
 
 export const createNode = (...node: Array<Cmd>): Graph<> => createGraph({node})
 
@@ -34,3 +34,22 @@ export function createGraph({
     val,
   }
 }
+export const clearNode = (
+  graphite: Graphite,
+  {deep}: {deep?: boolean} = {},
+) => {
+  const graph = getGraph(graphite)
+  if (deep) {
+    graph.next.forEach(node => clearNode(node, {deep}))
+  }
+  graph.from.forEach(node => {
+    const index = node.next.indexOf(graph)
+    if (index === -1) return
+    node.next.splice(index, 1)
+  })
+  graph.from.length = 0
+  graph.next.length = 0
+}
+
+export const getGraph = (graph: Graphite): Graph<any> =>
+  (graph: any).graphite ?? graph
