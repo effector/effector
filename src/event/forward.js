d80a9cc8521b4398da5f8f10fb14782cfabbb9a6
Zero Bias <ribkatt@gmail.com>
2019-04-05 23:14:05 +0300

    merge forward and linkGraphs

--- src/event/forward.js
+++ src/event/forward.js
@@ -1,24 +1,11 @@
 //@flow
-import type {Graph} from 'effector/stdlib'
+import {type Graphite, getGraph} from 'effector/stdlib'
 import type {Subscription} from '../effector/index.h'
 import {createWatcher} from 'effector/watcher'
 
-export function forward(opts: {
-  from: {+graphite: Graph<any>, ...},
-  to: {+graphite: Graph<any>, ...},
-}): Subscription {
-  return linkGraphs({
-    from: opts.from.graphite,
-    to: opts.to.graphite,
-  })
-}
-export const linkGraphs = ({
-  from,
-  to,
-}: {
-  from: Graph<any>,
-  to: Graph<any>,
-}): Subscription => {
+export const forward = (opts: {from: Graphite, to: Graphite}): Subscription => {
+  const from = getGraph(opts.from)
+  const to = getGraph(opts.to)
   from.next.push(to)
   //TODO push parent to .from field
   // to.from.push(from)
