deeb8402c1c0a0bc3a0fc3a01f2fa7263e0b4607
Zero Bias <ribkatt@gmail.com>
2019-04-08 04:10:36 +0300

    remove one intermediate state update in case of concurrent diamond deps

--- src/store/__tests__/diamond-deps.test.js
+++ src/store/__tests__/diamond-deps.test.js
@@ -78,7 +78,6 @@ test('olympic', async() => {
     ['text: "text" length: 4 empty: false'],
     ['text: "word" length: 4 empty: false'],
     ['text: "" length: 0 empty: true'],
-    ['text: "" length: 1 empty: false'],
     ['text: "end" length: 3 empty: false'],
   ])
 
