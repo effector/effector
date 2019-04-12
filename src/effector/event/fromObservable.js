bcd362a3cdb3b997a27f27b310b8fdf79cbab519
Zero Bias <ribkatt@gmail.com>
2019-04-12 17:46:13 +0300

    fix fromObservable

--- src/effector/event/fromObservable.js
+++ src/effector/event/fromObservable.js
@@ -2,6 +2,7 @@
 
 import invariant from 'invariant'
 import $$observable from 'symbol-observable'
+import {clearNode} from 'effector/stdlib'
 import {createEvent} from './createEvent'
 import type {Event} from './index.h'
 
@@ -18,9 +19,12 @@ export function fromObservable<T>(observable: mixed): Event<T> {
   )
   //$off
   const observableItem = observable[(($$observable: any): '@@observable')]()
-  const event = createEvent<T>()
-  observableItem({
+  const event: Event<T> = createEvent()
+  const disposer = clearNode.bind(null, event, {})
+  observableItem.subscribe({
     next: event,
+    error: disposer,
+    complete: disposer,
   })
   return event
 }
