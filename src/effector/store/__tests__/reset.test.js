026407366bd7eeaef7b8d7193918cf08e3e1e15a
Zero Bias <ribkatt@gmail.com>
2019-04-14 02:16:14 +0300

    group similar tests

--- src/effector/store/__tests__/reset.test.js
+++ src/effector/store/__tests__/reset.test.js
@@ -4,55 +4,112 @@ import {createStore} from '..'
 import {createEvent} from 'effector/event'
 import {argumentHistory} from 'effector/fixtures'
 
-test('reset before computation', () => {
-  const fn = jest.fn()
-  const A = createEvent('A')
-  const B = A.map(d => `${d}->B`)
+describe('reset before computation', () => {
+  test('reset before computation', () => {
+    const fn = jest.fn()
+    const A = createEvent('A')
+    const B = A.map(d => `${d}->B`)
 
-  const target = createStore('init')
-    .reset(B)
-    .on(A, (state, d) => `${state} + ${d}`)
+    const target = createStore('init')
+      .reset(B)
+      .on(A, (state, d) => `${state} + ${d}`)
 
-  target.watch(e => {
-    fn(e)
+    target.watch(e => {
+      fn(e)
+    })
+
+    A('[1]')
+    A('[2]')
+
+    expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+          Array [
+            "init",
+            "init + [1]",
+            "init",
+            "init + [2]",
+            "init",
+          ]
+      `)
   })
 
-  A('[1]')
-  A('[2]')
+  it("doesnt depend on methods' ordering", () => {
+    const fn = jest.fn()
+    const A = createEvent('A')
+    const B = A.map(d => `${d}->B`)
 
-  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
-    Array [
-      "init",
-      "init + [1]",
-      "init",
-      "init + [2]",
-      "init",
-    ]
-  `)
+    const target = createStore('init')
+      .on(A, (state, d) => `${state} + ${d}`)
+      .reset(B)
+
+    target.watch(e => {
+      fn(e)
+    })
+
+    A('[1]')
+    A('[2]')
+
+    expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+          Array [
+            "init",
+            "init + [1]",
+            "init",
+            "init + [2]",
+            "init",
+          ]
+      `)
+  })
 })
 
-test('computation before reset', () => {
-  const fn = jest.fn()
-  const A = createEvent('A')
-  const B = A.map(d => `${d}->B`)
+describe('computation before reset', () => {
+  test('computation before reset', () => {
+    const fn = jest.fn()
+    const A = createEvent('A')
+    const B = A.map(d => `${d}->B`)
 
-  const target = createStore('init')
-    .reset(A)
-    .on(B, (state, d) => `${state} + ${d}`)
+    const target = createStore('init')
+      .reset(A)
+      .on(B, (state, d) => `${state} + ${d}`)
 
-  target.watch(e => {
-    fn(e)
+    target.watch(e => {
+      fn(e)
+    })
+
+    A('[1]')
+    A('[2]')
+
+    expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+      Array [
+        "init",
+        "init + [1]->B",
+        "init",
+        "init + [2]->B",
+      ]
+    `)
   })
 
-  A('[1]')
-  A('[2]')
+  it("doesnt depend on methods' ordering", () => {
+    const fn = jest.fn()
+    const A = createEvent('A')
+    const B = A.map(d => `${d}->B`)
 
-  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
-    Array [
-      "init",
-      "init + [1]->B",
-      "init",
-      "init + [2]->B",
-    ]
-  `)
+    const target = createStore('init')
+      .on(B, (state, d) => `${state} + ${d}`)
+      .reset(A)
+
+    target.watch(e => {
+      fn(e)
+    })
+
+    A('[1]')
+    A('[2]')
+
+    expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+      Array [
+        "init",
+        "init + [1]->B",
+        "init",
+        "init + [2]->B",
+      ]
+    `)
+  })
 })
