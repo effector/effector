b357a0702803d2b8d27f39dc5f3884c437586bcf
andretshurotshka <andwebar@gmail.com>
2019-04-16 22:28:12 +0500

    Add service worker

---
+++ website/try/serviceworker.js
@@ -0,0 +1,51 @@
+//@flow
+
+const CACHE_NAME = 'try-effector-cache-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://effector.now.sh/css/main.css',
+  'https://effector.now.sh/img/comet.png',
+  'https://effector.now.sh/js/codetabs.js',
+]
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache')
+      return cache.addAll(urlsToCache)
+    }),
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(response => {
+          // Check if we received a valid response
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== 'basic'
+          ) {
+            return response
+          }
+
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          const responseToCache = response.clone()
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache)
+          })
+
+          return response
+        })
+      )
+    }),
+  )
+})
