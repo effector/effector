458456a70d4ba8e9af17400162c67e75642a467f
Zero Bias <ribkatt@gmail.com>
2019-04-19 08:03:31 +0300

    allow to use object with stores in createComponent

--- src/react/createComponent.js
+++ src/react/createComponent.js
@@ -1,13 +1,23 @@
 //@flow
 
 import * as React from 'react'
-import type {Store} from 'effector'
+import {type Store, is, invariant, createStoreObject} from 'effector'
 import {createStoreConsumer} from './createStoreConsumer'
 
 export function createComponent<Props: {}, State>(
-  store: Store<State>,
+  shape: Store<State>,
   renderProp: (props: Props, state: State) => React.Node,
 ): React.ComponentType<Props> {
+  let store
+  if (is.store(shape)) {
+    store = shape
+  } else {
+    invariant(
+      typeof shape === 'object' && shape !== null,
+      'shape should be store or object with stores',
+    )
+    store = createStoreObject(shape)
+  }
   const Consumer = createStoreConsumer(store)
   return class RenderComponent extends React.Component<Props> {
     renderProp = (state: State) => renderProp(this.props, state)
