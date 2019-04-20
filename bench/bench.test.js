44f23013200c10b333b28dbc9371f769fee1a3b1
Zero Bias <ribkatt@gmail.com>
2019-04-20 13:09:23 +0300

    update old bench

--- bench/bench.test.js
+++ bench/bench.test.js
@@ -1,19 +1,24 @@
 'use strict'
 //@flow
-
+process.env.NODE_ENV = 'production'
+//$off
+const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
+const {Record, Repeat} = require('immutable')
+//$off
+const {default: produce, setAutoFreeze, setUseProxies} = require('immer')
 const {test, beforeAll, afterAll} = require('./benchmark')
 
-beforeAll(() => {
- const {Record, Repeat} = require('immutable')
- //$off
- const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
- //$off
- const {default: produce, setAutoFreeze, setUseProxies} = require('immer')
-})
+// beforeAll(() => {
+//  const {Record, Repeat} = require('immutable')
+//  //$off
+//  const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
+//  //$off
+//  const {default: produce, setAutoFreeze, setUseProxies} = require('immer')
+// })
 
 afterAll(results => {
- const {printResults} = require('./printResults')
- printResults(results)
+  const {printResults} = require('./printResults')
+  printResults(results)
 })
 
 // describe('performance', () => {
@@ -30,41 +35,43 @@ const MAX = 100000
 const MODIFY_FACTOR = 0.1
 
 function getItem(any, i) {
- return {
-  todo: `todo_${i}`,
-  done: false,
-  someThingCompletelyIrrelevant: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
- }
+  return {
+    todo: `todo_${i}`,
+    done: false,
+    someThingCompletelyIrrelevant: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  }
 }
 function generateDraft() {
- const draft = []
- for (let i = 0; i < MAX; i++) {
-  draft.push(getItem(undefined, i))
- }
- return draft
+  const draft = []
+  for (let i = 0; i < MAX; i++) {
+    draft.push(getItem(undefined, i))
+  }
+  return draft
 }
+
+const commonDrafts = Array.from({length: 7}, () => generateDraft())
+
 // Produce the frozen bazeState
 // frozenBazeState = deepFreeze(cloneDeep(baseState))
 
 // generate immutalbeJS base state
 
 test('immutableJS', prepared => {
- const {Record, Repeat} = require('immutable')
- const todoRecord = Record({
-  todo: '',
-  done: false,
-  someThingCompletelyIrrelevant: [],
- })
- const draft = Repeat(undefined, MAX)
-  .map((_, i) => todoRecord(getItem(_, i)))
-  .toList()
- prepared()
- const result = draft.withMutations(state => {
-  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-   state.setIn([i, 'done'], true)
-  }
- })
- return result
+  const todoRecord = Record({
+    todo: '',
+    done: false,
+    someThingCompletelyIrrelevant: [],
+  })
+  const draft = Repeat(undefined, MAX)
+    .map((_, i) => todoRecord(getItem(_, i)))
+    .toList()
+  prepared()
+  const result = draft.withMutations(state => {
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      state.setIn([i, 'done'], true)
+    }
+  })
+  return result
 })
 
 //  test('just mutate, freeze', () => {
@@ -146,68 +153,89 @@ test('immutableJS', prepared => {
 // })
 
 test('immer (proxy) - without autofreeze', prepared => {
- //$off
- const {default: produce, setAutoFreeze, setUseProxies} = require('immer')
- const draft = generateDraft()
- prepared()
+  const draft = commonDrafts[0]
+  setUseProxies(true)
+  setAutoFreeze(false)
+  prepared()
 
- setUseProxies(true)
- setAutoFreeze(false)
- produce(draft, draft => {
-  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-   draft[i].done = true
-  }
- })
- return draft
+  produce(draft, draft => {
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      draft[i].done = true
+    }
+  })
+  return draft
+})
+test('immer (proxy) - with autofreeze', prepared => {
+  const draft = commonDrafts[1]
+
+  setUseProxies(true)
+  setAutoFreeze(true)
+  prepared()
+  produce(draft, draft => {
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      draft[i].done = true
+    }
+  })
+  return draft
+})
+test('immer (no proxy) - without autofreeze', prepared => {
+  const draft = commonDrafts[2]
+
+  setUseProxies(false)
+  setAutoFreeze(false)
+  prepared()
+  produce(draft, draft => {
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      draft[i].done = true
+    }
+  })
+  return draft
 })
 
 test('effector (mutable inner update)', prepared => {
- //$off
- const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
- const updateEvent = createEvent('update')
- const effectorStore = createStore(generateDraft()).on(updateEvent, draft => {
-  const newDraft = draft.concat([])
-  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-   newDraft[i].done = true
-  }
-  return newDraft
- })
- prepared()
- updateEvent()
- return effectorStore.getState()
+  const draft = commonDrafts[3]
+  const updateEvent = createEvent('update')
+  const effectorStore = createStore(draft).on(updateEvent, draft => {
+    const newDraft = draft.slice()
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      newDraft[i].done = true
+    }
+    return newDraft
+  })
+  prepared()
+  updateEvent()
+  return effectorStore.getState()
 })
 
 test('effector (immutable inner update)', prepared => {
- //$off
- const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
- const updateEvent = createEvent('update')
- const effectorStore = createStore(generateDraft()).on(updateEvent, draft => {
-  const newDraft = draft.concat([])
-  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-   newDraft[i] = Object.assign({}, newDraft[i], {done: true})
-  }
-  return newDraft
- })
- prepared()
- updateEvent()
- return effectorStore.getState()
+  const draft = commonDrafts[4]
+  const updateEvent = createEvent('update')
+  const effectorStore = createStore(draft).on(updateEvent, draft => {
+    const newDraft = draft.slice()
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      newDraft[i] = Object.assign({}, newDraft[i], {done: true})
+    }
+    return newDraft
+  })
+  prepared()
+  updateEvent()
+  return effectorStore.getState()
 })
 
 test('effector (immutable subscribe)', prepared => {
- //$off
- const {createEvent, createStore} = require('../npm/effector/effector.cjs.js')
- const updateEvent = createEvent('update')
- const effectorStore = createStore(generateDraft()).on(updateEvent, draft => {
-  const newDraft = draft.concat([])
-  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-   newDraft[i] = Object.assign({}, newDraft[i], {done: true})
-  }
-  return newDraft
- })
- prepared()
- effectorStore.subscribe(() => {})
- updateEvent()
- return effectorStore.getState()
+  const draft = commonDrafts[5]
+  const updateEvent = createEvent('update')
+  const effectorStore = createStore(draft).on(updateEvent, draft => {
+    const newDraft = draft.slice()
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      newDraft[i] = Object.assign({}, newDraft[i], {done: true})
+    }
+    return newDraft
+  })
+  prepared()
+  effectorStore.watch(() => {})
+  updateEvent()
+  return effectorStore.getState()
 })
 
 // test('immer (proxy) - with autofreeze', () => {
@@ -242,10 +270,10 @@ test('effector (immutable subscribe)', prepared => {
 // })
 
 test('just mutate', prepared => {
- const draft = generateDraft()
- prepared()
- for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
-  draft[i].done = true
- }
- return draft
+  const draft = commonDrafts[6]
+  prepared()
+  for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+    draft[i].done = true
+  }
+  return draft
 })
