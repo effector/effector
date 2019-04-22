3766feadf6a06501783a8208765302f01fbcfadf
Zero Bias <ribkatt@gmail.com>
2019-04-22 21:07:40 +0300

    make createComponent flat

--- src/react/createComponent.js
+++ src/react/createComponent.js
@@ -2,7 +2,8 @@
 
 import * as React from 'react'
 import {type Store, is, invariant, createStoreObject} from 'effector'
-import {createStoreConsumer} from './createStoreConsumer'
+
+type Unsubscribe = () => void
 
 export function createComponent<Props: {}, State>(
   shape: Store<State>,
@@ -18,11 +19,45 @@ export function createComponent<Props: {}, State>(
     )
     store = createStoreObject(shape)
   }
-  const Consumer = createStoreConsumer(store)
-  return class RenderComponent extends React.Component<Props> {
+  return class RenderComponent extends React.Component<
+    Props,
+    {currentState: State},
+  > {
+    static displayName = `${store.shortName}.View`
+    _unsubscribe: Unsubscribe | null = null
+    _hasUnmounted: boolean = false
+    state = {currentState: store.getState()}
+    componentDidMount() {
+      this.subscribe()
+    }
+    componentWillUnmount() {
+      this.unsubscribe()
+      this._hasUnmounted = true
+    }
     renderProp = (state: State) => renderProp(this.props, state)
     render() {
-      return <Consumer>{this.renderProp}</Consumer>
+      return renderProp(this.props, this.state.currentState)
+    }
+    subscribe() {
+      this._unsubscribe = store.watch((state: State) => {
+        if (this._hasUnmounted) {
+          return
+        }
+
+        this.setState(prevState => {
+          if (state === prevState.currentState) {
+            return null
+          }
+          return {currentState: state}
+        })
+      })
+    }
+    unsubscribe() {
+      if (typeof this._unsubscribe === 'function') {
+        this._unsubscribe()
+      }
+
+      this._unsubscribe = null
     }
   }
 }
