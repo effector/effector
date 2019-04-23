8c080d3640603289af1833c036c692c61ef84a39
Zero Bias <ribkatt@gmail.com>
2019-04-23 03:15:21 +0300

    implement chord

--- src/effector/index.js
+++ src/effector/index.js
@@ -13,7 +13,7 @@ export type {Domain} from 'effector/domain'
 export {createDomain} from 'effector/domain'
 
 export type {Event} from 'effector/event'
-export {createEvent, forward, fromObservable} from 'effector/event'
+export {createEvent, forward, fromObservable, chord} from 'effector/event'
 
 export type {Effect} from 'effector/effect'
 export {createEffect} from 'effector/effect'
