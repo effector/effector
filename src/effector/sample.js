49d270215a2f3bebd018e248c8979519403ab65c
Zero Bias <ribkatt@gmail.com>
2019-05-03 20:25:26 +0300

    split sample cases back

--- src/effector/sample.js
+++ src/effector/sample.js
@@ -16,13 +16,7 @@ import {noop} from './blocks'
 
 import invariant from 'invariant'
 
-export function sample(source: any, sampler: Graphite): any {
-  invariant(
-    is.unit(source),
-    'sample: First argument should be Event, ' +
-      'Store or Effect, but you passed %s',
-    source,
-  )
+const sampleStore = (source, sampler) => {
   const name = source.shortName
   const parent = source.domainName
   const state = createStateRef()
@@ -41,29 +35,11 @@ export function sample(source: any, sampler: Graphite): any {
   ]
   const linkScope = {}
   linkScope.state = state
-  //TODO better split cases back
-  if (is.store(source)) {
-    target = storeFabric({
-      currentState: writeRef(state, source.getState()),
-      config: {name},
-      parent,
-    })
-  } else {
-    target = eventFabric({name, parent})
-    const hasValue = createStateRef(false)
-    linkScope.hasValue = hasValue
-    readerScope.hasValue = hasValue
-    linkNode[0] = step.filter({
-      fn: (upd, {hasValue}) => readRef(hasValue),
-    })
-    readerNode.push(
-      step.tap({
-        fn(upd, {hasValue}) {
-          writeRef(hasValue, true)
-        },
-      }),
-    )
-  }
+  target = storeFabric({
+    currentState: writeRef(state, source.getState()),
+    config: {name},
+    parent,
+  })
 
   createLink(source, {
     scope: readerScope,
@@ -75,6 +51,65 @@ export function sample(source: any, sampler: Graphite): any {
     node: linkNode,
   })
   return target
+}
+
+const sampleEvent = (source, sampler) => {
+  const name = source.shortName
+  const parent = source.domainName
+  const state = createStateRef()
+  let target
+  const readerNode = [step.update({store: state})]
+  const readerScope = {}
+  const linkNode = [
+    noop,
+    step.barrier({
+      barrierID: nextBarrierID(),
+      priority: 'sampler',
+    }),
+    step.compute({
+      fn: (upd, {state}) => readRef(state),
+    }),
+  ]
+  const linkScope = {}
+  linkScope.state = state
+
+  target = eventFabric({name, parent})
+  const hasValue = createStateRef(false)
+  linkScope.hasValue = hasValue
+  readerScope.hasValue = hasValue
+  linkNode[0] = step.filter({
+    fn: (upd, {hasValue}) => readRef(hasValue),
+  })
+  readerNode.push(
+    step.tap({
+      fn(upd, {hasValue}) {
+        writeRef(hasValue, true)
+      },
+    }),
+  )
+
+  createLink(source, {
+    scope: readerScope,
+    node: readerNode,
+  })
+  createLink(sampler, {
+    scope: linkScope,
+    child: [target],
+    node: linkNode,
+  })
+  return target
+}
+
+export function sample(source: any, sampler: Graphite): any {
+  invariant(
+    is.unit(source),
+    'sample: First argument should be Event, ' +
+      'Store or Effect, but you passed %s',
+    source,
+  )
+  return is.store(source)
+    ? sampleStore(source, sampler)
+    : sampleEvent(source, sampler)
 }
 
 const createLink = (from, config) => {
