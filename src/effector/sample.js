584ac296afe9d0b03e3e14c93c9b73767fa31886
Zero Bias <ribkatt@gmail.com>
2019-05-03 14:36:00 +0300

    skip target updates before first source update

--- src/effector/sample.js
+++ src/effector/sample.js
@@ -9,6 +9,7 @@ import {
   type Graphite,
   createStateRef,
   readRef,
+  writeRef,
   nextBarrierID,
 } from 'effector/stdlib'
 import {noop} from './blocks'
@@ -22,40 +23,58 @@ export function sample(source: any, sampler: Graphite): any {
       'Store or Effect, but you passed %s',
     source,
   )
-  let initial
-  let target
   const name = source.shortName
   const parent = source.domainName
+  const state = createStateRef()
+  let target
+  const readerNode = [step.update({store: state})]
+  const readerScope = {}
+  const linkNode = [
+    noop,
+    step.barrier({
+      barrierID: nextBarrierID(),
+      priority: 'sampler',
+    }),
+    step.compute({
+      fn: (upd, {state}) => readRef(state),
+    }),
+  ]
+  const linkScope = {}
+  linkScope.state = state
+  //TODO better split cases back
   if (is.store(source)) {
-    initial = source.getState()
     target = storeFabric({
-      currentState: initial,
+      currentState: writeRef(state, source.getState()),
       config: {name},
       parent,
     })
   } else {
     target = eventFabric({name, parent})
+    const hasValue = createStateRef(false)
+    linkScope.hasValue = hasValue
+    readerScope.hasValue = hasValue
+    linkNode[0] = step.filter({
+      fn: (upd, {hasValue}) => readRef(hasValue),
+    })
+    readerNode.push(
+      step.tap({
+        fn(upd, {hasValue}) {
+          writeRef(hasValue, true)
+        },
+      }),
+    )
   }
-  const state = createStateRef(initial)
   const link = createGraph({
-    scope: {state},
+    scope: linkScope,
     child: [target],
-    node: [
-      noop,
-      step.barrier({
-        barrierID: nextBarrierID(),
-        priority: 'sampler',
-      }),
-      step.compute({
-        fn: (upd, {state}) => readRef(state),
-      }),
-    ],
+    node: linkNode,
   })
 
   forward({
     from: source,
     to: createGraph({
-      node: [step.update({store: state})],
+      scope: readerScope,
+      node: readerNode,
     }),
   })
   forward({
