b013e7f4b18a85604e2fb8fef59b848207285b81
Zero Bias <ribkatt@gmail.com>
2019-05-04 23:55:22 +0300

    add createLink internal helper

--- src/effector/event/forward.js
+++ src/effector/event/forward.js
@@ -1,7 +1,21 @@
 //@flow
-import {type Graphite, getGraph} from 'effector/stdlib'
+import {type Graphite, type Cmd, getGraph, createGraph} from 'effector/stdlib'
 import type {Subscription} from '../index.h'
 import {createWatcher} from 'effector/watcher'
+
+export const createLink = (
+  from: Graphite,
+  opts: {|
+    +node: Array<Cmd>,
+    +child?: Array<Graphite>,
+    scope?: {[name: string]: any},
+    meta?: {[name: string]: any},
+  |},
+) =>
+  forward({
+    from,
+    to: createGraph(opts),
+  })
 
 export const forward = (opts: {from: Graphite, to: Graphite}): Subscription => {
   const from = getGraph(opts.from)
--- src/effector/event/index.js
+++ src/effector/event/index.js
@@ -1,7 +1,7 @@
 //@flow
 
 export {eventFabric} from './eventFabric'
-export {forward} from './forward'
+export {forward, createLink} from './forward'
 export {createEvent} from './createEvent'
 export type {Event} from './index.h'
 export {fromObservable} from './fromObservable'
