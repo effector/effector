98b28bc0d466bec3cdc68b4d1378e2af8ee2b201
andretshurotshka <andwebar@gmail.com>
2019-05-08 15:38:12 +0500

    Add isomorphic useEffect

--- src/react/useStore.js
+++ src/react/useStore.js
@@ -1,9 +1,12 @@
 //@flow
 
 import {type Store, is, invariant} from 'effector'
-import {useReducer, useLayoutEffect} from 'react'
+import {useReducer, useEffect, useLayoutEffect} from 'react'
 
 const stateReducer = (_: any, payload: any) => payload
+
+const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
 export function useStore<State>(store: Store<State>): State {
   invariant(
@@ -12,6 +15,6 @@ export function useStore<State>(store: Store<State>): State {
     store,
   )
   const dispatch = useReducer(stateReducer, undefined, store.getState)[1]
-  useLayoutEffect(() => store.watch(dispatch), [store])
+  useIsomorphicLayoutEffect(() => store.watch(dispatch), [store])
   return store.getState()
 }
