567780cd9e51cb658375938f81cccac452347596
Zero Bias <ribkatt@gmail.com>
2019-05-13 15:42:03 +0300

    disable service worker

--- website/editor/src/index.js
+++ website/editor/src/index.js
@@ -8,25 +8,5 @@ import './flow/dynamic'
 import './logs/dynamic'
 import view from './view'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    ?.register('./serviceworker.js', {
-      scope: '/try/',
-    })
-    .then(
-      registration => {
-        // Registration was successful
-        console.log(
-          'ServiceWorker registration successful with scope: ',
-          registration.scope,
-        )
-      },
-      err => {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err)
-      },
-    )
-}
-
 const domNode = document.getElementById('try-wrapper')
 domNode && ReactDOM.render(view, domNode)
