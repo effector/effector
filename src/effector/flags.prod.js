00eb18de311ac664ad83a191ab059f163d593fca
Zero Bias <ribkatt@gmail.com>
2019-05-14 06:11:15 +0300

    fix flags version import

--- src/effector/flags.prod.js
+++ src/effector/flags.prod.js
@@ -4,4 +4,4 @@ export const __DEBUG__ = false
 export const __DEV__ = process.env.NODE_ENV !== 'production'
 export const __CANARY__ = false
 
-export {version} from '../packages/effector/package.json'
+export {version} from '../../packages/effector/package.json'
