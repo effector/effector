8c28b4693f93586eeb3741b038fbce0526444ef5
Zero Bias <ribkatt@gmail.com>
2019-05-14 06:35:09 +0300

    simplify setStoreName

--- src/effector/store/setStoreName.js
+++ src/effector/store/setStoreName.js
@@ -20,42 +20,27 @@ export function setStoreName<State>(store: Store<State>, rawName: string) {
   store.compositeName.fullName = compositeName.fullName
 }
 
-function isStoreObject(store: Store<any>) {
-  return (
-    is.store(store)
-    //$todo
-    && typeof store.defaultShape !== 'undefined'
-  )
-}
-
 export function storeNaming<Obj: {[key: string]: Store<any> | Object}>(
   object: Obj,
   parent?: Store<any>,
 ) {
   for (const storeName in object) {
     const store: Store<any> = object[storeName]
-    if (parent && is.store(store)) {
-      store.domainName = parent.compositeName || store.domainName
-    }
-
-    if (isStoreObject(store)) {
-      setStoreName(store, storeName)
-      //$todo
-      storeNaming(store.defaultShape, store)
-      continue
-    }
-
-    if (is.store(store)) {
-      setStoreName(store, storeName)
-      continue
-    }
-
-    if (__DEBUG__)
+    if (!is.store(store)) {
       console.warn(
         'effector: Key "%s" must be store but instead received %s',
         storeName,
         store.kind || '"' + typeof store + '"',
         store,
       )
+      continue
+    }
+    if (parent) {
+      store.domainName = parent.compositeName || store.domainName
+    }
+    setStoreName(store, storeName)
+    if (store.defaultShape !== undefined) {
+      storeNaming(store.defaultShape, store)
+    }
   }
 }
