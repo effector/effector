4759e2ac2fef501c1366b21c483769270f2bcb11
Zero Bias <ribkatt@gmail.com>
2019-05-14 05:37:13 +0300

    move flags to src/effector dir

--- babel.config.js
+++ babel.config.js
@@ -36,7 +36,8 @@ const aliases = {
   'effector/naming': 'effector/naming',
   invariant: 'effector/validate/invariant',
   warning: 'effector/validate/warning',
-  'effector/flags': ({isBuild}) => (isBuild ? 'flags.prod' : 'flags.dev'),
+  'effector/flags': ({isBuild}) =>
+    isBuild ? 'effector/flags.prod' : 'effector/flags.dev',
   'effector/fixtures': 'fixtures',
   '@effector/forms': 'forms',
   'effector-react': 'react',
