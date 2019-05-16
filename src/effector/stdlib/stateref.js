e3cb1d726cee5d5d99e7d63bf414721d75ee2e3d
Zero Bias <ribkatt@gmail.com>
2019-05-17 00:04:37 +0300

    allow to use *Ref methods with plain {current} objects

--- src/effector/stdlib/stateref.js
+++ src/effector/stdlib/stateref.js
@@ -7,5 +7,6 @@ export const createStateRef = (current: any): StateRef => ({
   current,
 })
 
-export const readRef = ({current}: StateRef) => current
-export const writeRef = (ref: StateRef, value: any) => (ref.current = value)
+export const readRef = ({current}: StateRef | {current: any}) => current
+export const writeRef = (ref: StateRef | {current: any}, value: any) =>
+  (ref.current = value)
