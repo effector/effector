a0fbe32fdd9b1a3962c9d54c681bbb39b7e024f5
andretshurotshka <andwebar@gmail.com>
2019-05-30 22:35:08 +0500

    Use comment syntax

--- jest.config.js
+++ jest.config.js
@@ -135,9 +135,9 @@ function createProjectList(items) {
 }
 
 function boolean(
-  value: string | boolean | null | void,
-  defaults: boolean,
-): boolean {
+  value/*: string | boolean | null | void*/,
+  defaults/*: boolean*/,
+)/*: boolean*/ {
   switch (value) {
     case 'no':
     case 'false':
