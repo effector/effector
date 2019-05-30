a54d22cfbff873ed83930e4eb39d0b2fb6d23786
andretshurotshka <andwebar@gmail.com>
2019-05-30 22:03:50 +0500

    Use inexact objects

--- src/effector/index.h.js
+++ src/effector/index.h.js
@@ -4,11 +4,13 @@ export type Subscriber<A> = {
   +next?: (value: A) => void,
   // error(err: Error): void,
   //complete(): void,
+  ...
 }
 
 export type Subscription = {
   /*::[[call]](): void,*/
   unsubscribe(): void,
+  ...
 }
 
 export type Unsubscribe = () => void
