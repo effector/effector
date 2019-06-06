1d885d5fb9b108617304c95cd105f5f587b23bf3
Zero Bias <ribkatt@gmail.com>
2019-06-06 20:11:06 +0300

    add tests for event.filter with config

--- src/effector/event/__tests__/filter.test.js
+++ src/effector/event/__tests__/filter.test.js
@@ -1,8 +1,33 @@
 //@flow
 
 import {createEvent, type Event} from 'effector'
-import {getSpyCalls, spy} from 'effector/fixtures'
+import {getSpyCalls, spy, argumentHistory} from 'effector/fixtures'
 import {show} from 'effector/fixtures/showstep'
+
+describe('event.filter with config {fn: (T) => boolean}', () => {
+  it('filter out when fn returns falsy', () => {
+    const fn = jest.fn()
+    const event = createEvent()
+    const filtered = event.filter({
+      fn: x => x > 0,
+    })
+    filtered.watch(x => fn(x))
+    event(2)
+    event(-1)
+    expect(argumentHistory(fn)).toEqual([2])
+  })
+  it('works with undefined as well', () => {
+    const fn = jest.fn()
+    const event = createEvent()
+    const filtered = event.filter({
+      fn: x => x === undefined,
+    })
+    filtered.watch(x => fn(x))
+    event(2)
+    event()
+    expect(argumentHistory(fn)).toEqual([undefined])
+  })
+})
 
 describe('event.filter with function (T) => S | void', () => {
   test('event.filter should infer type', () => {
