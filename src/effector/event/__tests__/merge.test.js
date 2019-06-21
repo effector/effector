6ad4b802124cd4554cf95b9d9bde3775069262a4
andretshurotshka <andwebar@gmail.com>
2019-06-21 15:54:26 +0500

    Make `merge` accept array of Events

---
+++ src/effector/event/__tests__/merge.test.js
@@ -0,0 +1,24 @@
+// @flow
+
+import {merge, createEvent} from 'effector'
+import {argumentHistory} from 'effector/fixtures'
+
+test('merge', () => {
+  const fn = jest.fn()
+  const foo = createEvent<number>()
+  const bar = createEvent<number>()
+
+  const baz = merge([foo, bar])
+
+  baz.watch(v => fn(v))
+
+  foo(1)
+  bar(2)
+
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      1,
+      2,
+    ]
+  `)
+})
--- src/effector/event/merge.js
+++ src/effector/event/merge.js
@@ -2,7 +2,7 @@
 
 import {createEvent, forward, type Event} from 'effector'
 
-export function merge<T>(...events: $ReadOnlyArray<Event<T>>): Event<T> {
+export function merge<T>(events: $ReadOnlyArray<Event<T>>): Event<T> {
   const result = createEvent()
   for (const event of events) {
     forward({from: event, to: result})
