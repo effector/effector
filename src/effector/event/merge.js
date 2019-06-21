5947ace60f94f11df88dd773489498db1325f647
andretshurotshka <andwebar@gmail.com>
2019-06-21 16:36:52 +0500

    Add unitObjectName to event merge

--- src/effector/event/merge.js
+++ src/effector/event/merge.js
@@ -1,9 +1,14 @@
 // @flow
 
-import {createEvent, forward, type Event} from 'effector'
+import type {Event} from './index.h'
+import {eventFabric} from './eventFabric'
+import {unitObjectName} from '../naming'
+import {forward} from './forward'
 
 export function merge<T>(events: $ReadOnlyArray<Event<T>>): Event<T> {
-  const result = createEvent()
+  const result = eventFabric({
+    name: unitObjectName(events, 'merge'),
+  })
   for (const event of events) {
     forward({from: event, to: result})
   }
