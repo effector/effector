fd901a83c8e868f96f6a4171e463a4adc9f945f2
Zero Bias <ribkatt@gmail.com>
2019-06-23 14:50:31 +0300

    update showstep (graph text render)

--- src/fixtures/showstep.js
+++ src/fixtures/showstep.js
@@ -1,49 +1,85 @@
 //@flow
-import type {Graph, Cmd} from '../effector/stdlib'
+import type {Graphite, Cmd} from '../effector/stdlib'
+import {blocks, createStore} from '../effector'
 
-const showCmdDef = (_: Cmd) =>
-  `${_.type} {${JSON.stringify(
-    _.data,
-    (key, val) => {
-      if (key === 'meta') return
-      if (_.type === 'update' && key === 'store') return {current: val.current}
-      return val
-    },
-    2,
-  ).replace(/\"/gi, '')}}`
-export function show(
-  value: Graph,
-  showCmd: (cmd: Cmd) => string = showCmdDef,
-): string {
-  const print = {
-    multi(value) {
-      if (value.length === 0) return '[(empty list)]'
-      const inner = [...value]
-        .map(e => move1(`  *  ${move2(show(e, showCmd))}`))
-        .join(`,\n`)
-      return `Next [\n${inner}\n]`
-    },
-    seq(value) {
-      if (value.length === 0) return 'Seq []'
-      const inner = value
-        .map((e, n) => move1(`  ${n + 1}. ${move2(showCmd(e))}`))
-        .join(`,\n`)
-      return `Seq [\n${inner}\n]`
-    },
+const SHOW_FUN_SOURCE = false
+const printFn = (fn: Function) => {
+  const name = fn.name
+  if (name && name !== '' && name !== 'fn') return name
+  if (SHOW_FUN_SOURCE) return fn.toString()
+  return 'Function'
+}
+const showCmd = (_: Cmd) => {
+  const replacer = (key, val) => {
+    if (key === 'meta') return
+    if (_.type === 'filter' && key === 'fn') {
+      return val.toString()
+    }
+    if (_.type === 'update' && key === 'store')
+      return {
+        state: val.current,
+      }
+    return val
   }
+  switch (_.type) {
+    case 'filter':
+      switch (_.data.fn.toString()) {
+        case storeOldStateComparator:
+          return 'filter: upd => upd !== state'
+        case isNotUndefined:
+          return 'filter: upd => upd !== undefined'
+        default:
+          return `filter: ${printFn(_.data.fn)}`
+      }
+    case 'compute':
+      switch (_.data.fn.toString()) {
+        case noop:
+          return `compute: x => x`
+        default:
+          return `compute: ${printFn(_.data.fn)}`
+      }
+    case 'run':
+      return `run: watcher`
+  }
+  const view = JSON.stringify(_.data, replacer, 2).replace(/\"/gi, '')
+  //.replace(/^ /gim, '') // move whole json print one space left
+  return `${_.type}: ${view}`
+}
+const storeOldStateComparator = createStore(
+  null,
+).graphite.seq[2].data.fn.toString()
+const isNotUndefined = createStore(null).graphite.seq[0].data.fn.toString()
+const noop = blocks.noop.data.fn.toString()
+const print = {
+  multi(value) {
+    if (value.length === 0) return 'next: []'
+    const inner = [...value].map(e => move1(`  ${move2(show(e))}`)).join(`,\n`)
+    return `next: [\n${inner}\n]`
+  },
+  seq(value) {
+    if (value.length === 0) return 'seq: []'
+    const inner = value
+      .map((e, n) => move1(`  ${move2(showCmd(e))}`))
+      .join(`,\n`)
+    return `seq: [\n${inner}\n]`
+  },
+}
+export function show(unit: any): string {
+  const value = unit.graphite ? unit.graphite : unit
   const seq = addIndent(print.seq(value.seq))
   const next = addIndent(print.multi(value.next))
-  return `Graph {\n${seq}\n${next}}`
+  if (value.seq.length === 0 && value.next.length === 0)
+    return 'Node {seq: [], next: []}'
+  return `Node {\n${seq},\n${next}\n}`
 }
 const addIndent = str => {
   const splitted = str.split(/\n/gi)
   const padded = splitted.map(line => `  ${line}`)
   return padded.join(`\n`)
 }
-const map1 = (s, i) => (i > 0 ? `  ${s}` : s)
+const map1 = (s, i) => (i > 0 ? ` ${s}` : s)
 const map2 = (s, i, l) => {
   if (i === 0) return s
-  if (i === l.length - 1) return s
   return ` ${s}`
 }
 const move1 = e => splitJoin(e, map1)
