c3307639494253b9ab8bbb8c227db67f49accd1e
andretshurotshka <andwebar@gmail.com>
2019-06-24 15:56:58 +0500

    Re-enable type-at-pos

--- website/editor/src/flow/dynamic.js
+++ website/editor/src/flow/dynamic.js
@@ -4,8 +4,6 @@ import {flow, typeAtPos as _typeAtPos} from '@zerobias/codebox'
 import {sourceCode} from '../domain'
 import {checkContent, typeAtPos, typeHint} from './domain'
 
-const USE_TYPECHECK = false
-
 checkContent.fail.watch(data => console.error('checkContent', data))
 checkContent.done.watch(data => console.log('checkContent', data))
 typeAtPos.fail.watch(data => console.error(data))
@@ -18,12 +16,6 @@ typeAtPos.use(({filename, body, line, col}) =>
   }),
 )
 
-//sourceCode.watch(code => checkContent(code))
-
-typeHint.watch(console.log)
-
-if (USE_TYPECHECK) {
-  typeHint
-    .on(typeAtPos.done, (_, {result}) => result.code.c)
-    .reset(typeAtPos.fail)
-}
+typeHint
+  .on(typeAtPos.done, (_, {result}) => result.code.c)
+  .reset(typeAtPos.fail)
