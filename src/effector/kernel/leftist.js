5a39ed1347dd2cbfd817ae7fd562f5f40b2d522e
Zero Bias <ribkatt@gmail.com>
2019-06-26 08:51:48 +0300

    remove infinite loop from leftist implementation

--- src/effector/kernel/leftist.js
+++ src/effector/kernel/leftist.js
@@ -27,18 +27,86 @@ export class Leftist {
  */
 export type leftist = null | Leftist
 export const insert = (x: Layer, t: leftist): leftist =>
-  merge(new Leftist(x, 1, null, null), t)
+  mergeNoLoop(new Leftist(x, 1, null, null), t)
 
 export const deleteMin = (param: leftist): leftist => {
   if (param) {
-    return merge(param.left, param.right)
+    return mergeNoLoop(param.left, param.right)
   }
   return null
+}
+const mergeNoLoop = (x: leftist, y: leftist): leftist => {
+  if (x === null) return y
+  if (y === null) return x
+  let temp: leftist
+  if (layerComparator(x.value, y.value)) {
+    temp = x
+    x = y
+    y = temp
+  }
+  // there was x.right = mergeWiki(x.right, y)
+  let right = mergeNoLoop(x.right, y)
+  let left = x.left
+  let rank = x.rank
+  if (left === null) {
+    // left child doesn't exist, so move right child to the left side
+    left = right
+    right = null
+    // rank was, and remains, 1
+  } else {
+    // left child does exist, so compare ranks
+    if (left.rank < right.rank) {
+      temp = left
+      left = right
+      right = temp
+    }
+    // since we know the right child has the lower rank, we can just
+    // add one to its rank
+    rank = right.rank + 1
+  }
+  return new Leftist(x.value, rank, left, right)
+}
+
+const mergeMutable = (x: leftist, y: leftist): leftist => {
+  if (x === null) return y
+  if (y === null) return x
+  let temp: leftist = null
+  if (layerComparator(x.value, y.value)) {
+    temp = x
+    x = y
+    y = temp
+    temp = null
+  }
+  // there was x.right = mergeMutable(x.right, y)
+  let right = mergeMutable(x.right, y)
+  let left = x.left
+  let rank = x.rank
+  if (left === null) {
+    // left child doesn't exist, so move right child to the left side
+    left = right
+    right = null
+    // rank was, and remains, 1
+  } else {
+    // left child does exist, so compare ranks
+    if (left.rank < right.rank) {
+      temp = left
+      left = right
+      right = temp
+      temp = null
+    }
+    // since we know the right child has the lower rank, we can just
+    // add one to its rank
+    rank = right.rank + 1
+  }
+  x.rank = rank
+  x.left = left
+  x.right = right
+  return x
 }
 /**
  * Used to maintain priority queue being sorted
  */
-const merge = (_t1: leftist, _t2: leftist): leftist => {
+const mergeOriginal = (_t1: leftist, _t2: leftist): leftist => {
   let t2
   let t1
   let k1
@@ -58,7 +126,7 @@ const merge = (_t1: leftist, _t2: leftist): leftist => {
       _t1 = t2
       continue
     }
-    merged = merge(t1.right, t2)
+    merged = mergeOriginal(t1.right, t2)
     rank_left = l?.rank || 0
     rank_right = merged?.rank || 0
     if (rank_left >= rank_right) {
