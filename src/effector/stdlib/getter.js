89e4054f89fb07c58322f66bcd1f2138617ce8e2
Zero Bias <ribkatt@gmail.com>
2019-06-26 12:16:34 +0300

    add stdlib/getter

---
+++ src/effector/stdlib/getter.js
@@ -0,0 +1,8 @@
+//@flow
+
+import type {Graphite, Graph} from './index.h'
+
+export const getGraph = (graph: Graphite): Graph =>
+  (graph: any).graphite || graph
+export const getOwners = (node: Graph) => node.family.owners
+export const getLinks = (node: Graph) => node.family.links
