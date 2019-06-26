8386cf728ed3ebefd0531a67d59322e798b88a7e
Zero Bias <ribkatt@gmail.com>
2019-06-26 09:10:53 +0300

    Omit config defaults, replace invariant in createStore

--- src/effector/store/createStore.js
+++ src/effector/store/createStore.js
@@ -1,22 +1,17 @@
 //@flow
 
-import invariant from 'invariant'
-
 import type {Store} from './index.h'
 import {storeFabric} from './storeFabric'
 import {type Config, type StoreConfigPart, normalizeConfig} from '../config'
 
 export function createStore<State>(
-  state: State,
-  config: Config<StoreConfigPart> = {},
+  currentState: State,
+  config?: Config<StoreConfigPart>,
 ): Store<State> {
-  invariant(
-    typeof state !== 'undefined',
-    "createStore: First argument can't be undefined, use null instead.",
-  )
-  const opts = normalizeConfig(config)
+  if (currentState === undefined)
+    throw Error("current state can't be undefined, use null instead")
   return storeFabric({
-    currentState: state,
-    config: opts,
+    currentState,
+    config: normalizeConfig(config),
   })
 }
