2fd40059063a3ca0a6294dd6fd9b619ee534a11b
Zero Bias <ribkatt@gmail.com>
2019-06-27 12:54:43 +0300

    add tests for useStoreMap

--- src/react/__tests__/useStore.test.js
+++ src/react/__tests__/useStore.test.js
@@ -4,7 +4,7 @@ import * as React from 'react'
 //$todo
 import {render, cleanup, act} from 'react-testing-library'
 import {createStore, createEvent} from 'effector'
-import {useStore} from '../useStore'
+import {useStore, useStoreMap} from '../useStore'
 
 afterEach(cleanup)
 
@@ -39,4 +39,57 @@ describe('useStore', () => {
       render(<ErrorDisplay />)
     }).toThrowErrorMatchingSnapshot()
   })
+})
+
+test('useStoreMap', () => {
+  const removeUser = createEvent()
+  const changeUserAge = createEvent()
+  const users = createStore({
+    alex: {age: 20, name: 'Alex'},
+    john: {age: 30, name: 'John'},
+  })
+  users.on(removeUser, (users, nickname) => {
+    const upd = {...users}
+    delete upd[nickname]
+    return upd
+  })
+  users.on(changeUserAge, (users, {nickname, age}) => ({
+    ...users,
+    [nickname]: {...users[nickname], age},
+  }))
+
+  const Card = ({nickname}) => {
+    const {name, age} = useStoreMap({
+      store: users,
+      keys: [nickname],
+      fn: (users, [nickname]) => users[nickname],
+    })
+    return (
+      <li>
+        {name}: {age}
+      </li>
+    )
+  }
+
+  const Cards = () => {
+    const userList = Object.keys(useStore(users))
+    return (
+      <ul>
+        {userList.map(name => (
+          <Card nickname={name} key={name} />
+        ))}
+      </ul>
+    )
+  }
+  const {container} = render(<Cards />)
+  expect(container.firstChild).toMatchSnapshot()
+  act(() => {
+    changeUserAge({nickname: 'alex', age: 21})
+  })
+
+  expect(container.firstChild).toMatchSnapshot()
+  act(() => {
+    removeUser('alex')
+  })
+  expect(container.firstChild).toMatchSnapshot()
 })
