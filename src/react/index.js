070283e29de6eda9b10554080d2899443d472476
Zero Bias <ribkatt@gmail.com>
2019-06-27 11:58:23 +0300

    implement useStoreMap

--- src/react/index.js
+++ src/react/index.js
@@ -5,7 +5,7 @@ export {unstable_createStoreProvider} from './createStoreProvider'
 
 export {createContextComponent} from './createContextComponent'
 export {createComponent} from './createComponent'
-export {useStore} from './useStore'
+export {useStore, useStoreMap} from './useStore'
 export {connect} from './connect'
 export {createReactState} from './createReactState'
 export {createGate, useGate} from './createGate'
--- src/react/useStore.js
+++ src/react/useStore.js
@@ -1,7 +1,7 @@
 //@flow
 
-import {type Store, is} from 'effector'
-import {useReducer, useEffect, useLayoutEffect} from 'react'
+import {type Store, is, clearNode} from 'effector'
+import {useReducer, useEffect, useLayoutEffect, useMemo} from 'react'
 
 const stateReducer = (_: any, payload: any) => payload
 
@@ -13,4 +13,25 @@ export function useStore<State>(store: Store<State>): State {
   const dispatch = useReducer(stateReducer, undefined, store.getState)[1]
   useIsomorphicLayoutEffect(() => store.watch(dispatch), [store])
   return store.getState()
+}
+
+export function useStoreMap<State, Result, Keys: $ReadOnlyArray<any>>({
+  store,
+  keys = [],
+  fn,
+}: {|
+  +store: Store<State>,
+  +keys?: Keys,
+  fn(state: State, keys: Keys): Result,
+|}): Result {
+  if (!is.store(store)) throw Error('useStoreMap expects a store')
+  if (!Array.isArray(keys)) throw Error('useStoreMap expects an array as keys')
+  if (typeof fn !== 'function') throw Error('useStoreMap expects a function')
+  const result: Store<Result> = useMemo(
+    () => store.map(state => fn(state, keys)),
+    keys,
+  )
+  const state = useStore(result)
+  useIsomorphicLayoutEffect(() => clearNode(result, {deep: true}), keys)
+  return state
 }
