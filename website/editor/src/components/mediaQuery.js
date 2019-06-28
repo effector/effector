9a388473791bd3a15e2ed1c6fa213f189521f5af
Zero Bias <ribkatt@gmail.com>
2019-06-28 14:01:44 +0300

    add mediaQuery component fabric

---
+++ website/editor/src/components/mediaQuery.js
@@ -0,0 +1,22 @@
+//@flow
+
+import * as React from 'react'
+import {createStore, createEvent} from 'effector'
+import {useStore} from 'effector-react'
+
+export function mediaQuery(
+  query: string,
+): React.ComponentType<{children: React.Node}> {
+  const queryChange = createEvent('query change')
+  const mediaQueryList = window.matchMedia(query)
+  mediaQueryList.addListener(queryChange)
+  const isQueryMatches = createStore(mediaQueryList.matches).on(
+    queryChange,
+    (state, e) => e.matches,
+  )
+  function MediaQuery({children}) {
+    return useStore(isQueryMatches) ? children : null
+  }
+  MediaQuery.displayName = `MediaQuery(${query})`
+  return MediaQuery
+}
