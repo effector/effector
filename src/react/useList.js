0e72baba8186c1925c332c76c9acd0cb114b6e29
Zero Bias <zerobias@icloud.com>
2019-07-21 02:27:57 +0300

    add validations for useList

--- src/react/useList.js
+++ src/react/useList.js
@@ -1,13 +1,17 @@
 //@flow
 /* eslint-disable react-hooks/rules-of-hooks */
 import * as React from 'react'
-import type {Store} from 'effector'
+import {type Store, is} from 'effector'
 import {useStoreMap} from './useStore'
 
 export function useList<T>(
   list: Store<T[]>,
   renderItem: (item: T, index: number) => React.Node,
 ): React.Node {
+  if (!is.store(list))
+    throw Error('expect useList first argument to be a store')
+  if (typeof renderItem !== 'function')
+    throw Error('expect useList second argument to be a function')
   const Item = React.useMemo(() => {
     const Item = ({index}) => {
       const item = useStoreMap({
