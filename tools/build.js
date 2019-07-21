ab21f7b7e721828e9afeb944896954177a920710
Zero Bias <zerobias@icloud.com>
2019-07-21 03:43:47 +0300

    set env and clean cache before build

--- tools/build.js
+++ tools/build.js
@@ -1,11 +1,14 @@
 const {rollup} = require('rollup')
 const {input, output} = require('./builder/rollup.config')
+const {resolve} = require('path')
 
 module.exports = (async function runner() {
   console.log('generate build script')
   const build = await rollup(input)
   await build.write(output)
   console.log('build')
+  process.env.IS_BUILD = 'true'
+  delete require.cache[resolve(__dirname, '../babel.config.js')]
   const {exec} = require('./builder.js')
   await exec()
 })()
