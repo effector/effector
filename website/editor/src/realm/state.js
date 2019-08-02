2e651d9478453ca454066b3840c5899ccf34cc46
Zero Bias <zerobias@icloud.com>
2019-08-02 18:41:43 +0300

    add realm/state

---
+++ website/editor/src/realm/state.js
@@ -0,0 +1,27 @@
+//@flow
+
+import {
+  createStore,
+  type Event,
+  type Store,
+  type Effect,
+  type Domain,
+} from 'effector'
+import type {StoreView} from 'effector-react'
+
+export const intervals: Store<IntervalID[]> = createStore([])
+export const timeouts: Store<TimeoutID[]> = createStore([])
+
+export const stats: Store<{|
+  event: Event<any>[],
+  store: Store<any>[],
+  effect: Effect<any, any, any>[],
+  domain: Domain[],
+  component: StoreView<any, any>[],
+|}> = createStore({
+  event: [],
+  store: [],
+  effect: [],
+  domain: [],
+  component: [],
+})
