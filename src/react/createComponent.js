0333c14f276688643991022edb2987321c6e1d81
andretshurotshka <andwebar@gmail.com>
2019-08-22 21:35:48 +0500

    Remove comments

--- src/react/createComponent.js
+++ src/react/createComponent.js
@@ -52,49 +52,4 @@ export function createComponent<Props: {...}, State>(
   RenderComponent.mounted = mounted
   RenderComponent.unmounted = unmounted
   return RenderComponent
-  // return class RenderComponent extends React.Component<
-  //   Props,
-  //   {currentState: State, ...},
-  // > {
-  //   static displayName = `${storeName}.View`
-  //   static mounted = mounted
-  //   static unmounted = unmounted
-  //   _unsubscribe: Unsubscribe | null = null
-  //   _hasUnmounted: boolean = false
-  //   store = typeof shape === 'function' ? storeFn(this.props) : store
-  //   state = {currentState: this.store.getState()}
-  //   componentDidMount() {
-  //     mounted({props: this.props, state: this.store.getState()})
-  //     this.subscribe()
-  //   }
-  //   componentWillUnmount() {
-  //     unmounted({props: this.props, state: this.store.getState()})
-  //     this.unsubscribe()
-  //     this._hasUnmounted = true
-  //   }
-  //   render() {
-  //     return renderProp(this.props, this.state.currentState)
-  //   }
-  //   subscribe() {
-  //     this._unsubscribe = this.store.watch((state: State) => {
-  //       if (this._hasUnmounted) {
-  //         return
-  //       }
-
-  //       this.setState(prevState => {
-  //         if (state === prevState.currentState) {
-  //           return null
-  //         }
-  //         return {currentState: state}
-  //       })
-  //     })
-  //   }
-  //   unsubscribe() {
-  //     if (typeof this._unsubscribe === 'function') {
-  //       this._unsubscribe()
-  //     }
-
-  //     this._unsubscribe = null
-  //   }
-  // }
 }
