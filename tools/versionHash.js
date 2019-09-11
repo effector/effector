c567f7db35758118ec0fac755f3b7ef2fe60ce76
Zero Bias <zerobias@icloud.com>
2019-09-11 13:53:16 +0300

    use github sha env

--- tools/versionHash.js
+++ tools/versionHash.js
@@ -1,15 +1,18 @@
 const {resolve} = require('path')
 const {outputJSONSync, readJSONSync} = require('fs-extra')
 
-const versionHash = process.env.VERSION_HASH
-if (typeof versionHash !== 'string') throw Error('no VERSION_HASH')
+const versionHash = process.env.VERSION_HASH || process.env.GITHUB_SHA
+if (typeof versionHash !== 'string') throw Error('no VERSION_HASH/GITHUB_SHA')
 
 const packagePath = resolve(__dirname, '..', 'npm/effector/package.json')
 
 const effectorPackage = readJSONSync(packagePath)
 
 effectorPackage.name = '@effector/canary'
-effectorPackage.version = `${effectorPackage.version}-${versionHash}`
+effectorPackage.version = `${effectorPackage.version}-${versionHash.slice(
+  0,
+  8,
+)}`
 effectorPackage.repository = 'git://github.com/zerobias/effector.git'
 const registry = process.env.VERSION_HASH_REGISTRY
 if (registry) {
