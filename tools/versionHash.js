1f34b03102721e60bb42eb4aae803ef8bfb13cd1
Zero Bias <zerobias@icloud.com>
2019-09-11 13:40:43 +0300

    add support for github registry to prepublish script

--- tools/versionHash.js
+++ tools/versionHash.js
@@ -10,6 +10,13 @@ const effectorPackage = readJSONSync(packagePath)
 
 effectorPackage.name = '@effector/canary'
 effectorPackage.version = `${effectorPackage.version}-${versionHash}`
+effectorPackage.repository = 'git://github.com/zerobias/effector.git'
+const registry = process.env.VERSION_HASH_REGISTRY
+if (registry) {
+  effectorPackage.publishConfig = effectorPackage.publishConfig || {}
+  effectorPackage.publishConfig.registry = registry
+  effectorPackage.name = '@zerobias/effector'
+}
 
 outputJSONSync(packagePath, effectorPackage, {
   spaces: 2,
