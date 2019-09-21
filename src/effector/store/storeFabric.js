4fe0f43c56a1dd34df8a3a4c68d66836e2c9c40d
Zero Bias <zerobias@icloud.com>
2019-09-21 05:56:21 +0300

    move storeMethods to storeFabric

--- src/effector/store/storeFabric.js
+++ src/effector/store/storeFabric.js
@@ -10,22 +10,23 @@ import {
   createStateRef,
   readRef,
   bind,
+  writeRef,
+  is,
 } from '../stdlib'
-import {createEvent, forward} from '../event'
+import {
+  createEvent,
+  forward,
+  createLink,
+  createLinkNode,
+  type Event,
+} from '../event'
 
 import type {Store} from './index.h'
 import type {StoreConfigPart as ConfigPart} from '../config'
 import {createName, type CompositeName} from '../compositeName'
 import {thru} from '../thru'
-import {
-  reset,
-  off,
-  on,
-  observable,
-  watch,
-  subscribe,
-  mapStore,
-} from './storeMethods'
+import type {Subscriber} from '../index.h'
+import {watchUnit} from '../watcher'
 
 export function storeFabric<State>(props: {
   +currentState: State,
@@ -87,4 +88,116 @@ export function storeFabric<State>(props: {
   })
   addLinkToOwner(store, updates)
   return store
+}
+
+function reset(storeInstance: Store<any>, ...events: Array<Event<any>>) {
+  for (const event of events)
+    storeInstance.on(event, () => storeInstance.defaultState)
+  return storeInstance
+}
+
+function off(storeInstance: Store<any>, event: Event<any>) {
+  const currentSubscription = storeInstance.subscribers.get(event)
+  if (currentSubscription !== undefined) {
+    currentSubscription()
+    storeInstance.subscribers.delete(event)
+  }
+  return storeInstance
+}
+
+function on(storeInstance: Store<any>, event: any, handler: Function) {
+  storeInstance.off(event)
+  storeInstance.subscribers.set(
+    event,
+    createLink(event, storeInstance, {
+      scope: {
+        handler,
+        state: storeInstance.stateRef,
+      },
+      node: [
+        step.compute({
+          fn(newValue, {handler, state}) {
+            const result = handler(readRef(state), newValue)
+            if (result === undefined) return
+            return writeRef(state, result)
+          },
+        }),
+      ],
+      meta: {op: 'on'},
+    }),
+  )
+  return storeInstance
+}
+function observable(storeInstance: Store<any>) {
+  const result = {
+    subscribe(observer: Subscriber<any>) {
+      if (observer !== Object(observer))
+        throw Error('expect observer to be an object') // or function
+      return subscribe(storeInstance, state => {
+        if (observer.next) {
+          observer.next(state)
+        }
+      })
+    },
+  }
+  //$off
+  result[$$observable] = function() {
+    return this
+  }
+  return result
+}
+function watch(
+  storeInstance: Store<any>,
+  eventOrFn: Event<*> | Function,
+  fn?: Function,
+) {
+  if (!fn || !is.unit(eventOrFn)) {
+    if (typeof eventOrFn !== 'function')
+      throw Error('watch requires function handler')
+    return subscribe(storeInstance, eventOrFn)
+  }
+  if (typeof fn !== 'function')
+    throw Error('second argument should be a function')
+  return eventOrFn.watch(payload => fn(storeInstance.getState(), payload))
+}
+function subscribe(storeInstance: Store<any>, handler: Function) {
+  if (typeof handler !== 'function')
+    throw Error('expect listener to be a function')
+  handler(storeInstance.getState())
+  return watchUnit(storeInstance, handler)
+}
+
+function mapStore<A, B>(
+  store: Store<A>,
+  fn: (state: A, lastState?: B) => B,
+  firstState?: B,
+): Store<B> {
+  let lastResult
+  const storeState = store.getState()
+  if (storeState !== undefined) {
+    lastResult = fn(storeState, firstState)
+  }
+
+  const innerStore: Store<any> = storeFabric({
+    config: {name: '' + store.shortName + ' → *'},
+    currentState: lastResult,
+    parent: store.domainName,
+  })
+  createLinkNode(store, innerStore, {
+    scope: {
+      handler: fn,
+      state: innerStore.stateRef,
+    },
+    node: [
+      step.compute({
+        fn: (upd, {state, handler}) => handler(upd, readRef(state)),
+      }),
+      step.check.defined(),
+      step.check.changed({
+        store: innerStore.stateRef,
+      }),
+    ],
+    meta: {op: 'map'},
+  })
+  return innerStore
 }
