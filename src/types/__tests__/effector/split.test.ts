032951b25767f654ff8f7cce4c970b851393d8d5
Zero Bias <zerobias@icloud.com>
2019-10-07 21:20:10 +0300

    add type tests for split type inference (for PR #215)

---
+++ src/types/__tests__/effector/split.test.ts
@@ -0,0 +1,22 @@
+// @flow
+/* eslint-disable no-unused-vars */
+import {createEvent, Event, split} from 'effector'
+import setupLocation from '../../setupLocation'
+const typecheck = '{global}'
+
+it('should infer type by given predicate', () => {
+  const event: Event<number | string> = createEvent()
+  const {onlyNumbers} = split(event, {
+    onlyNumbers: (value): value is number => typeof value === 'number',
+  })
+  const shouldBeOk: Event<number> = onlyNumbers
+  expect(typecheck).toMatchInlineSnapshot(`
+    "
+    --typescript--
+    no errors
+
+    --flow--
+    no errors
+    "
+  `)
+})
