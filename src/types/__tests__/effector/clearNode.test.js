0b635307c42d3f26ebbf22073290fe8ffcedb17d
Zero Bias <zerobias@icloud.com>
2019-10-21 09:14:32 +0300

    add type test for incorrect clearNode call

--- src/types/__tests__/effector/clearNode.test.js
+++ src/types/__tests__/effector/clearNode.test.js
@@ -67,3 +67,28 @@ test('clearNode(domain)', () => {
     "
   `)
 })
+
+test('clearNode(incorrect)', () => {
+  try {
+    clearNode(null)
+  } catch (err) {}
+
+  expect(typecheck).toMatchInlineSnapshot(`
+    "
+    --typescript--
+    Argument of type 'null' is not assignable to parameter of type 'Unit<any> | Step'.
+
+
+    --flow--
+    Cannot call 'clearNode' with 'null' bound to 'unit' because: Either property 'kind' is missing in null [1] but exists in 'Unit' [2]. Or property 'family' is missing in null [1] but exists in 'Step' [3]
+      clearNode(null)
+                ^^^^
+          clearNode(null)
+                [1] ^^^^
+          unit: Unit<any> | Step,
+            [2] ^^^^^^^^^
+          unit: Unit<any> | Step,
+                        [3] ^^^^
+    "
+  `)
+})
