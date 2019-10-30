fb41caa5bcefa61c3bbc8643b3b52465bfd9cc81
Illia Osmanov <illia.osmanov@stfalcon.com>
2019-10-30 11:42:41 +0200

    added custom wrapper for vue package. access to store from this

--- packages/effector-vue/index.d.ts
+++ packages/effector-vue/index.d.ts
@@ -1,10 +1,26 @@
 import Vue, {ComponentOptions, WatchOptions, VueConstructor} from 'vue'
+import { ExtendedVue } from "vue/types/vue"
 import {Store} from 'effector'
+
+type Inference<S> = S extends Store<infer State>
+    ? State
+    : S extends { [storeName: string]: Store<any> }
+      ? { [K in keyof S]: State<S[K]> }
+      : never;
 
 declare module 'vue/types/vue' {
   interface Vue {
     $watchAsStore: typeof watchAsStore
     $store: typeof store
+  }
+
+  interface VueConstructor<V extends Vue> {
+    extend<S, Data, Methods, Computed, PropNames extends string = never>(
+      options?: ThisTypedComponentOptionsWithArrayProps<S & V, Data, Methods, Computed, PropNames>
+    ): ExtendedVue<V, Data, Methods, Computed, Record<PropNames, any>>;
+    extend<S, Data, Methods, Computed, Props>(
+      options?: ThisTypedComponentOptionsWithRecordProps<S & V, Data, Methods, Computed, Props>
+    ): ExtendedVue<V, Data, Methods, Computed, Props>;
   }
 }
 
@@ -26,3 +42,30 @@ declare function watchAsStore<State>(
 declare function store<State>(exp: string): Store<State>
 declare function store<State>(fn: () => State): Store<State>
 declare function VueEffector(vm: VueConstructor<Vue>, options?: Object): void
+
+declare function createComponent<V extends Vue, Data, Methods, Computed, Props>(
+  options: ThisTypedComponentOptionsWithRecordProps<V, Data, Methods, Computed, Props>,
+): ExtendedVue<V, Data, Methods, Computed, Props>;
+
+declare function createComponent<V extends Vue, Data, Methods, Computed, PropNames extends string>(
+  options: ThisTypedComponentOptionsWithArrayProps<V, Data, Methods, Computed, PropNames>,
+): ExtendedVue<V, Data, Methods, Computed, PropNames>;
+
+declare function createComponent<
+  S extends { [field: string]: Store<any> },
+  V extends Vue, Data, Methods, Computed, Props
+>(
+  options: ThisTypedComponentOptionsWithRecordProps<
+    Inference<S> & V, Data, Methods, Computed, Props
+  >,
+  store?: S
+): ExtendedVue<V, Data, Methods, Computed, Props>;
+declare function createComponent<
+  S extends { [field: string]: Store<any> },
+  V extends Vue, Data, Methods, Computed, PropNames extends string
+>(
+  options: ThisTypedComponentOptionsWithArrayProps<
+    Inference<S> & V, Data, Methods, Computed, PropNames
+  >,
+  store?: S,
+): ExtendedVue<V, Data, Methods, Computed, Props>
--- src/vue/__tests__/index.test.js
+++ src/vue/__tests__/index.test.js
@@ -3,7 +3,7 @@
 /*::import typeof VueType from 'vue'*/
 const Vue = require('vue/dist/vue')
 import {Store, combine, createStore} from 'effector'
-import {VueEffector} from '../'
+import {VueEffector, createComponent} from '../'
 
 const counter = createStore(0)
 
@@ -22,6 +22,13 @@ const component = {
   },
 }
 
+const componentWithWrapper = createComponent({
+  name: 'ComponentWithWrapper'
+}, {counter})
+
 const vm = new Vue(component).$mount()
+const vmWithWrapper = new Vue(componentWithWrapper).$mount()
+
+console.error(vmWithWrapper)
 
 console.error(vm)
