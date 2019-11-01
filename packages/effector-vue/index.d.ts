f9b5233eb15f3ccd1aea0080676ce8fb1b07b792
Zero Bias <zerobias@icloud.com>
2019-11-01 09:31:16 +0300

    prettify vue public typings

--- packages/effector-vue/index.d.ts
+++ packages/effector-vue/index.d.ts
@@ -2,15 +2,15 @@ import Vue, {ComponentOptions, WatchOptions, VueConstructor} from 'vue'
 import {
   ThisTypedComponentOptionsWithArrayProps,
   ThisTypedComponentOptionsWithRecordProps,
-} from "vue/types/options";
-import { ExtendedVue } from "vue/types/vue"
+} from 'vue/types/options'
+import {ExtendedVue} from 'vue/types/vue'
 import {Store} from 'effector'
 
 type Inference<S> = S extends Store<infer State>
-    ? State
-    : S extends { [storeName: string]: Store<any> }
-      ? { [K in keyof S]: State<S[K]> }
-      : never;
+  ? State
+  : S extends {[storeName: string]: Store<any>}
+  ? {[K in keyof S]: S[K] extends Store<infer U> ? U : never}
+  : never
 
 declare module 'vue/types/vue' {
   interface Vue {
@@ -20,11 +20,23 @@ declare module 'vue/types/vue' {
 
   interface VueConstructor<V extends Vue> {
     extend<S, Data, Methods, Computed, PropNames extends string = never>(
-      options?: ThisTypedComponentOptionsWithArrayProps<S & V, Data, Methods, Computed, PropNames>
-    ): ExtendedVue<V, Data, Methods, Computed, Record<PropNames, any>>;
+      options?: ThisTypedComponentOptionsWithArrayProps<
+        S & V,
+        Data,
+        Methods,
+        Computed,
+        PropNames
+      >,
+    ): ExtendedVue<V, Data, Methods, Computed, Record<PropNames, any>>
     extend<S, Data, Methods, Computed, Props>(
-      options?: ThisTypedComponentOptionsWithRecordProps<S & V, Data, Methods, Computed, Props>
-    ): ExtendedVue<V, Data, Methods, Computed, Props>;
+      options?: ThisTypedComponentOptionsWithRecordProps<
+        S & V,
+        Data,
+        Methods,
+        Computed,
+        Props
+      >,
+    ): ExtendedVue<V, Data, Methods, Computed, Props>
   }
 }
 
@@ -48,28 +60,62 @@ declare function store<State>(fn: () => State): Store<State>
 declare function VueEffector(vm: VueConstructor<Vue>, options?: Object): void
 
 declare function createComponent<V extends Vue, Data, Methods, Computed, Props>(
-  options: ThisTypedComponentOptionsWithRecordProps<V, Data, Methods, Computed, Props>,
-): ExtendedVue<V, Data, Methods, Computed, Props>;
-
-declare function createComponent<V extends Vue, Data, Methods, Computed, PropNames extends string>(
-  options: ThisTypedComponentOptionsWithArrayProps<V, Data, Methods, Computed, PropNames>,
-): ExtendedVue<V, Data, Methods, Computed, PropNames>;
-
-declare function createComponent<
-  S extends { [field: string]: Store<any> },
-  V extends Vue, Data, Methods, Computed, Props
->(
   options: ThisTypedComponentOptionsWithRecordProps<
-    Inference<S> & V, Data, Methods, Computed, Props
+    V,
+    Data,
+    Methods,
+    Computed,
+    Props
   >,
-  store?: S
-): ExtendedVue<V, Data, Methods, Computed, Props>;
+): ExtendedVue<V, Data, Methods, Computed, Props>
+
 declare function createComponent<
-  S extends { [field: string]: Store<any> },
-  V extends Vue, Data, Methods, Computed, PropNames extends string
+  V extends Vue,
+  Data,
+  Methods,
+  Computed,
+  PropNames extends string
 >(
   options: ThisTypedComponentOptionsWithArrayProps<
-    Inference<S> & V, Data, Methods, Computed, PropNames
+    V,
+    Data,
+    Methods,
+    Computed,
+    PropNames
+  >,
+): ExtendedVue<V, Data, Methods, Computed, PropNames>
+
+declare function createComponent<
+  S extends {[field: string]: Store<any>},
+  V extends Vue,
+  Data,
+  Methods,
+  Computed,
+  Props
+>(
+  options: ThisTypedComponentOptionsWithRecordProps<
+    Inference<S> & V,
+    Data,
+    Methods,
+    Computed,
+    Props
+  >,
+  store?: S,
+): ExtendedVue<V, Data, Methods, Computed, Props>
+declare function createComponent<
+  S extends {[field: string]: Store<any>},
+  V extends Vue,
+  Data,
+  Methods,
+  Computed,
+  PropNames extends string
+>(
+  options: ThisTypedComponentOptionsWithArrayProps<
+    Inference<S> & V,
+    Data,
+    Methods,
+    Computed,
+    PropNames
   >,
   store?: S,
 ): ExtendedVue<V, Data, Methods, Computed, Props>
