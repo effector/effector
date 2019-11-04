2cf4003806b25d2132f5461f45cf78ae4549ea57
Zero Bias <zerobias@icloud.com>
2019-11-04 13:33:13 +0300

    replace object spread with explicit Object.assign
    project's build pipeline has some issues with object spreads

--- src/vue/component.js
+++ src/vue/component.js
@@ -2,15 +2,16 @@
 
 import Vue from 'vue'
 
-function createComponent<S>(options: Object, store?: S) {
-  return Vue.extend({
-    ...options,
-    ...(store && {
-      effector() {
-        return store
+export function createComponent<S>(options: Object, store?: S) {
+  return Vue.extend(
+    Object.assign(
+      {},
+      options,
+      store && {
+        effector() {
+          return store
+        },
       },
-    }),
-  })
+    ),
+  )
 }
-
-export {createComponent}
