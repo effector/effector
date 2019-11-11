0ec55888150957bf38fb27e63b7cec0589d9374d
Zero Bias <zerobias@icloud.com>
2019-11-11 07:50:43 +0300

    add react rerendering test

---
+++ src/react/__tests__/rerendering.test.js
@@ -0,0 +1,62 @@
+//@flow
+
+import * as React from 'react'
+import {render, container, act} from 'effector/fixtures/react'
+
+import {
+  createStore,
+  createEvent,
+  createEffect,
+  createStoreObject,
+  merge,
+} from 'effector'
+import {useStore} from 'effector-react'
+import {argumentHistory} from 'effector/fixtures/index'
+
+it('should rerender only once', async() => {
+  const fn = jest.fn()
+  const getPresentation = createEffect({
+    async handler() {
+      await 1
+      return 'ok'
+    },
+  })
+  const presentation = createStore(null).on(
+    getPresentation.done,
+    (_, {result}) => result,
+  )
+  const loading = createStore(true)
+    .on(getPresentation, () => true)
+    .on(getPresentation.finally, () => false)
+  const presentationStore = createStoreObject({
+    loading,
+    presentation,
+  })
+
+  const View = () => {
+    const presentation = useStore(presentationStore)
+    React.useEffect(() => {
+      getPresentation()
+    }, [])
+    fn(presentation)
+    return (
+      <div>
+        <p>is loading: {presentation.loading.toString()}</p>
+        <p>presentation: {presentation.presentation}</p>
+      </div>
+    )
+  }
+  await render(<View />)
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "loading": true,
+        "presentation": null,
+      },
+      Object {
+        "loading": false,
+        "presentation": "ok",
+      },
+    ]
+  `)
+})
