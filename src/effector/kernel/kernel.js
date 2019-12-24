a1b35fbb82a40eb5a51e5ee352db8599180b1782
Zero Bias <zerobias@icloud.com>
2019-12-24 20:13:09 +0300

    use hybrid prioritization; replace leftist heap with skew heap

--- src/effector/kernel/kernel.js
+++ src/effector/kernel/kernel.js
@@ -3,41 +3,73 @@
 import type {Graphite, Graph, ID} from '../stdlib'
 import {type PriorityTag, getPriority} from './getPriority'
 import {getGraph, readRef} from '../stdlib'
-import type {Layer} from './layer'
-import {Stack, createStack} from './stack'
+
+/**
+ * Position in the current branch,
+ * including call stack, priority type
+ * and index of next step in the executed Graph
+ */
+type Layer = {|
+  +firstIndex: number,
+  +stack: Stack,
+  +resetStop: boolean,
+  +type: PriorityTag,
+  +id: number,
+|}
+
+/**
+ * Call stack
+ */
+type Stack = {
+  value: any,
+  a: any,
+  b: any,
+  parent: Stack | null,
+  node: Graph,
+}
 
 /**
  * Dedicated local metadata
  */
 type Local = {
-  isChanged: boolean,
-  isFailed: boolean,
+  skip: boolean,
+  fail: boolean,
   ref: ID,
   scope: {[key: string]: any, ...},
 }
 
-const pushValueToList = (list, value) => {
-  const item = {
-    right: null,
-    value,
-  }
-  if (list.size === 0) {
-    list.first = item
-  } else {
-    list.last.right = item
-  }
-  list.size += 1
-  list.last = item
+/**
+ * Skew heap
+ */
+type Heap = {
+  /** heap node value */
+  v: Layer,
+  /** left heap node */
+  l: Heap | null,
+  /** right heap node */
+  r: Heap | null,
 }
-const popMinFromList = list => {
-  if (list.size === 0) return
-  if (list.size === 1) {
-    list.last = null
+
+let heap: Heap | null = null
+
+const merge = (a: Heap | null, b: Heap | null): Heap | null => {
+  if (!a) return b
+  if (!b) return a
+
+  let ret
+  if (
+    (a.v.type === b.v.type && a.v.id > b.v.id) ||
+    getPriority(a.v.type) > getPriority(b.v.type)
+  ) {
+    ret = a
+    a = b
+    b = ret
   }
-  const item = list.first
-  list.first = item.right
-  list.size -= 1
-  return item.value
+  ret = merge(a.r, b)
+  a.r = a.l
+  a.l = ret
+
+  return a
 }
 
 const queue = []
@@ -47,27 +79,81 @@ while (ix < 5) {
   ix += 1
 }
 
-let layerID = 0
-const barriers = new Set()
 const deleteMin = () => {
   for (let i = 0; i < 5; i++) {
-    if (queue[i].size > 0) return popMinFromList(queue[i])
+    const list = queue[i]
+    if (list.size > 0) {
+      if (i === 2 || i === 3) {
+        list.size -= 1
+        const value = heap.v
+        heap = merge(heap.l, heap.r)
+        return value
+      }
+      if (list.size === 1) {
+        list.last = null
+      }
+      const item = list.first
+      list.first = item.right
+      list.size -= 1
+      return item.value
+    }
   }
 }
-const pushHeap = (firstIndex: number, stack: Stack, type: PriorityTag) => {
-  pushValueToList(queue[getPriority(type)], {
+const pushFirstHeapItem = (
+  type: PriorityTag,
+  node: Graph,
+  parent: Stack | null,
+  value: any,
+) =>
+  pushHeap(
+    0,
+    ({
+      a: null,
+      b: null,
+      node,
+      parent,
+      value,
+    }: Stack),
+    type,
+  )
+const pushHeap = (
+  firstIndex: number,
+  stack: Stack,
+  type: PriorityTag,
+  id = ++layerID,
+) => {
+  const priority = getPriority(type)
+  const list = queue[priority]
+  const value = {
     firstIndex,
     stack,
     resetStop: currentResetStop,
     type,
-    id: ++layerID,
-  })
+    id,
+  }
+  if (priority === 2 || priority === 3) {
+    heap = merge(heap, {v: value, l: 0, r: 0})
+  } else {
+    const item = {
+      right: null,
+      value,
+    }
+    if (list.size === 0) {
+      list.first = item
+    } else {
+      list.last.right = item
+    }
+    list.last = item
+  }
+  list.size += 1
 }
+
+let layerID = 0
+const barriers = new Set()
 
 let alreadyStarted = false
 
 let currentResetStop = false
-
 const exec = () => {
   const lastStartedState = alreadyStarted
   alreadyStarted = true
@@ -80,8 +166,8 @@ const exec = () => {
     const {firstIndex, stack, resetStop, type} = value
     graph = stack.node
     const local: Local = {
-      isChanged: true,
-      isFailed: false,
+      skip: false,
+      fail: false,
       ref: '',
       scope: graph.scope,
     }
@@ -99,7 +185,7 @@ const exec = () => {
           if (stepn !== firstIndex || type !== priority) {
             if (!barriers.has(id)) {
               barriers.add(id)
-              pushHeap(stepn, stack, priority)
+              pushHeap(stepn, stack, priority, id)
             }
             continue mem
           }
@@ -132,11 +218,10 @@ const exec = () => {
         case 'check':
           switch (data.type) {
             case 'defined':
-              local.isChanged = stack.value !== undefined
+              local.skip = stack.value === undefined
               break
             case 'changed':
-              local.isChanged =
-                stack.value !== readRef(graph.reg[data.store.id])
+              local.skip = stack.value === readRef(graph.reg[data.store.id])
               break
           }
           break
@@ -146,7 +231,7 @@ const exec = () => {
            * tryRun will return null
            * thereby forcing that branch to stop
            */
-          local.isChanged = !!tryRun(local, data, stack)
+          local.skip = !tryRun(local, data, stack)
           break
         case 'run':
           /** exec 'compute' step when stepn === firstIndex */
@@ -158,12 +243,12 @@ const exec = () => {
           stack.value = tryRun(local, data, stack)
           break
       }
-      meta.stop = local.isFailed || !local.isChanged
+      meta.stop = local.fail || local.skip
     }
     if (!meta.stop) {
       currentResetStop = true
       for (let stepn = 0; stepn < graph.next.length; stepn++) {
-        pushHeap(0, createStack(graph.next[stepn], stack, stack.value), 'child')
+        pushFirstHeapItem('child', graph.next[stepn], stack, stack.value)
       }
       currentResetStop = false
     }
@@ -174,13 +259,13 @@ const exec = () => {
   alreadyStarted = lastStartedState
 }
 export const launch = (unit: Graphite, payload: any, upsert?: boolean) => {
-  pushHeap(0, createStack(getGraph(unit), null, payload), 'pure')
+  pushFirstHeapItem('pure', getGraph(unit), null, payload)
   if (upsert && alreadyStarted) return
   exec()
 }
 export const upsertLaunch = (units: Graphite[], payloads: any[]) => {
   for (let i = 0; i < units.length; i++) {
-    pushHeap(0, createStack(getGraph(units[i]), null, payloads[i]), 'pure')
+    pushFirstHeapItem('pure', getGraph(units[i]), null, payloads[i])
   }
   if (alreadyStarted) return
   exec()
@@ -191,6 +276,6 @@ const tryRun = (local: Local, {fn}, stack: Stack) => {
     return fn(stack.value, local.scope, stack)
   } catch (err) {
     console.error(err)
-    local.isFailed = true
+    local.fail = true
   }
 }
