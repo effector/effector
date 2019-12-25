be6beae9f5f142d152c6592ac3db3633e1e676aa
Zero Bias <zerobias@icloud.com>
2019-12-26 01:04:56 +0300

    run darkTheme watcher only after document will be loaded

--- website/client/static/js/darkTheme.js
+++ website/client/static/js/darkTheme.js
@@ -3,16 +3,17 @@
  * however, automatic switching looks broken in chrome desktop,
  * so we need to explicitly change it in case of chages of user theme preferences
  */
+document.addEventListener('load', () => {
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)')
 
-const isDark = window.matchMedia('(prefers-color-scheme: dark)')
-
-function applyTheme(e) {
-  if (e.matches) {
-    document.querySelector('html').dataset.theme = 'dark'
-  } else {
-    document.querySelector('html').dataset.theme = ''
+  function applyTheme(e) {
+    if (e.matches) {
+      document.querySelector('html').dataset.theme = 'dark'
+    } else {
+      document.querySelector('html').dataset.theme = ''
+    }
   }
-}
 
-isDark.addListener(applyTheme)
-applyTheme(isDark)
+  isDark.addListener(applyTheme)
+  applyTheme(isDark)
+})
