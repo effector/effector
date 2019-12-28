512dda851fa0b9a5b18c30f6572238df42c6c637
Zero Bias <zerobias@icloud.com>
2019-12-28 23:13:56 +0300

    remember explicitly selected theme in docs

--- website/client/static/js/darkTheme.js
+++ website/client/static/js/darkTheme.js
@@ -3,17 +3,46 @@
  * however, automatic switching looks broken in chrome desktop,
  * so we need to explicitly change it in case of chages of user theme preferences
  */
+const isDark = window.matchMedia('(prefers-color-scheme: dark)')
+// theme key used by docusaurus
+const THEME = 'theme'
+// current system theme
+const SYSTEM_THEME = 'theme/system'
+// is theme was explicitly changed
+const IS_EXPLICIT_THEME = 'theme/explicit'
+
+setItem(SYSTEM_THEME, isDark.matches ? 'dark' : 'light')
+if (getItem(IS_EXPLICIT_THEME) === null) {
+  setItem(IS_EXPLICIT_THEME, false)
+}
 setTimeout(() => {
-  const isDark = window.matchMedia('(prefers-color-scheme: dark)')
-
-  function applyTheme(e) {
-    if (e.matches) {
-      document.querySelector('html').dataset.theme = 'dark'
-    } else {
-      document.querySelector('html').dataset.theme = ''
-    }
-  }
-
+  window.addEventListener('storage', e => {
+    if (e.key !== THEME) return
+    let systemTheme = getItem(SYSTEM_THEME)
+    if (systemTheme === 'light') systemTheme = ''
+    setItem(IS_EXPLICIT_THEME, e.newValue !== systemTheme)
+  })
   isDark.addListener(applyTheme)
   applyTheme(isDark)
 }, 1000)
+function applyTheme(e) {
+  setItem(SYSTEM_THEME, e.matches ? 'dark' : 'light')
+  if (getItem(IS_EXPLICIT_THEME)) return
+  const theme = e.matches ? 'dark' : ''
+  document.documentElement.dataset.theme = theme
+  setItem(THEME, theme)
+}
+function setItem(key, value) {
+  try {
+    localStorage.setItem(key, value)
+  } catch (err) {}
+}
+function getItem(key) {
+  let result = null
+  try {
+    result = localStorage.getItem(key)
+  } catch (err) {}
+  if (result === 'true') result = true
+  if (result === 'false') result = false
+  return result
+}
