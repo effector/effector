e1c63105d9ffd34d6110dca0b26b352d0e8cc162
Zero Bias <zerobias@icloud.com>
2020-02-19 18:32:14 +0300

    add hydrate to client-side rendering in react-ssr example

--- examples/react-ssr/src/client.tsx
+++ examples/react-ssr/src/client.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createBrowserHistory} from 'history'
-import {fork, allSettled} from 'effector/fork'
+import {fork, hydrate, allSettled} from 'effector/fork'
 import {App, app, location$, startClient} from './app'
 
 const history = createBrowserHistory()
@@ -12,9 +12,11 @@ location$.updates.watch(location => {
   }
 })
 
-const clientScope = fork(app, {
+hydrate(app, {
   values: window.__initialState__,
 })
+
+const clientScope = fork(app)
 
 allSettled(startClient, {
   scope: clientScope,
