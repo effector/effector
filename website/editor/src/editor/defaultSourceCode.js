abe43e4a798a3609a23a7075b403ff3a9eaf8e63
Igor Ryzhov <vinokur141@gmail.com>
2020-02-19 13:14:01 +0500

    add reset usage to example

--- website/editor/src/editor/defaultSourceCode.js
+++ website/editor/src/editor/defaultSourceCode.js
@@ -1,23 +1,28 @@
 //@flow
 
-export default `const increment = createEvent()
-const decrement = createEvent()
-const resetCounter = createEvent()
-
-const counter = createStore(0)
-  .on(increment, state => state + 1)
-  .on(decrement, state => state - 1)
-  .reset(resetCounter)
-
-counter.watch(n => console.log('counter: ', n))
-// counter: 0
-increment.watch(() => console.log('increment'))
-decrement.watch(() => console.log('decrement'))
-
-increment()
-// counter: 1
-// increment
-decrement()
-// counter: 0
-// decrement
+export default `
+  const increment = createEvent()
+  const decrement = createEvent()
+  const resetCounter = createEvent()
+  
+  const counter = createStore(0)
+    .on(increment, state => state + 1)
+    .on(decrement, state => state - 1)
+    .reset(resetCounter)
+  
+  counter.watch(n => console.log('counter: ', n))
+  // counter: 0
+  increment.watch(() => console.log('increment'))
+  decrement.watch(() => console.log('decrement'))
+  resetCounter.watch(() => console.log('reset counter'))
+  
+  increment()
+  // counter: 1
+  // increment
+  decrement()
+  // counter: 0
+  // decrement
+  resetCounter()
+  // counter: 0
+  // reset counter
 `
