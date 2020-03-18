2abbff449536047f1328a3ab2f49febb5c7da022
Zero Bias <zerobias@icloud.com>
2020-03-18 21:26:47 +0300

    add test for fork attach support

--- src/react/__tests__/scopes.test.js
+++ src/react/__tests__/scopes.test.js
@@ -4,7 +4,7 @@ import fetch from 'cross-fetch'
 import * as React from 'react'
 import {render, container, act} from 'effector/fixtures/react'
 import {argumentHistory} from 'effector/fixtures'
-import {createDomain, forward, sample} from 'effector'
+import {createDomain, forward, sample, attach} from 'effector'
 
 import {fork, allSettled, serialize} from 'effector/fork'
 import {Provider, useStore, useList} from 'effector-react/ssr'
@@ -116,5 +116,125 @@ it('works', async () => {
       ],
     }
   `)
+  expect(indirectCallFn).toBeCalled()
+})
+
+test('attach support', async () => {
+  const indirectCallFn = jest.fn()
+
+  const app = createDomain()
+  const start = app.createEvent()
+  const indirectCall = app.createEvent()
+  const sendStats = app.createEffect({
+    async handler(user) {
+      await new Promise(resolve => {
+        // let bob loading longer
+        setTimeout(resolve, user === 'bob' ? 500 : 100)
+      })
+    },
+  })
+
+  const baseUrl = app.createStore('https://ssr.effector.dev/api')
+
+  const fetchJson = app.createEffect({
+    async handler(url) {
+      return (
+        await fetch(url, {
+          method: 'POST',
+        })
+      ).json()
+    },
+  })
+
+  const fetchUser = attach({
+    source: {baseUrl},
+    effect: fetchJson,
+    mapParams: (user, {baseUrl}) => `${baseUrl}/${user}`,
+  })
+
+  //assume that calling start() will trigger some effects
+  forward({
+    from: start,
+    to: fetchUser,
+  })
+
+  const user = app.createStore('guest')
+  const friends = app.createStore([])
+  const friendsTotal = friends.map(list => list.length)
+
+  user.on(fetchUser.doneData, (_, result) => result.name)
+  friends.on(fetchUser.doneData, (_, result) => result.friends)
+
+  sample({
+    source: user,
+    clock: fetchUser.done,
+    target: sendStats,
+  })
+  sample({
+    source: user,
+    clock: indirectCall,
+  }).watch(indirectCallFn)
+
+  sendStats.done.watch(() => {
+    indirectCall()
+  })
+
+  const aliceScope = fork(app)
+  const bobScope = fork(app)
+  const carolScope = fork(app)
+  await Promise.all([
+    allSettled(start, {
+      scope: aliceScope,
+      params: 'alice',
+    }),
+    allSettled(start, {
+      scope: bobScope,
+      params: 'bob',
+    }),
+    allSettled(start, {
+      scope: carolScope,
+      params: 'carol',
+    }),
+  ])
+  const User = () => <h2>{useStore(user)}</h2>
+  const Friends = () => useList(friends, friend => <li>{friend}</li>)
+  const Total = () => <small>Total: {useStore(friendsTotal)}</small>
+
+  const App = ({root}) => (
+    <Provider value={root}>
+      <User />
+      <b>Friends:</b>
+      <ol>
+        <Friends />
+      </ol>
+      <Total />
+    </Provider>
+  )
+
+  await render(<App root={bobScope} />)
+  expect(container.firstChild).toMatchInlineSnapshot(`
+<h2>
+  bob
+</h2>
+`)
+  expect(serialize(aliceScope)).toMatchInlineSnapshot(`
+Object {
+  "-emnm5v": "alice",
+  "8r4q44": "https://ssr.effector.dev/api",
+  "ndtnwz": Array [
+    "bob",
+    "carol",
+  ],
+}
+`)
+  expect(serialize(bobScope)).toMatchInlineSnapshot(`
+Object {
+  "-emnm5v": "bob",
+  "8r4q44": "https://ssr.effector.dev/api",
+  "ndtnwz": Array [
+    "alice",
+  ],
+}
+`)
   expect(indirectCallFn).toBeCalled()
 })
