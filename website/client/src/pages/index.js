8935b9aaf5b98fb33351c7a1094fc25d658e07f9
Victor <yumaa.verdin@gmail.com>
2020-03-21 11:40:48 +0300

    Update code example
    Slightly updated code example, in the old version it was not obvious that reducer functions actually works

--- website/client/src/pages/index.js
+++ website/client/src/pages/index.js
@@ -15,24 +15,24 @@ import {createStore, createEvent} from 'effector'
 const add = createEvent()
 const sub = createEvent()
 const reset = createEvent()
-  
+
 const counter = createStore(0)
- .on(add, (count, num) => count + num)
- .on(sub, (count, num) => count - num)
- .reset(reset)
-  
-counter.watch(n => console.log('counter: ', n))
+  .on(add, (count, n) => count + n)
+  .on(sub, (count, n) => count - n)
+  .reset(reset)
+
+counter.watch(n => console.log('counter:', n))
 // counter: 0
-add.watch(() => console.log('add'))
-sub.watch(() => console.log('subtract'))
+add.watch(n => console.log('add', n))
+sub.watch(n => console.log('subtract', n))
 reset.watch(() => console.log('reset counter'))
-  
-add(2)
-// add
-// counter: 2
+
+add(5)
+// add 5
+// counter: 5
 sub(1)
-// subtract
-// counter: 1
+// subtract 1
+// counter: 4
 reset()
 // reset counter
 // counter: 0
