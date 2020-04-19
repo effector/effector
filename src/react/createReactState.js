1f77feaacfca7c7f62683873c532d915435ec786
Zero Bias <zerobias@icloud.com>
2020-04-20 02:47:03 +0300

    simplify createReactState implementation

--- src/react/createReactState.js
+++ src/react/createReactState.js
@@ -3,18 +3,16 @@
 import * as React from 'react'
 import type {Store} from 'effector'
 
-import {createStoreConsumer} from './createStoreConsumer'
+import {useStore} from './useStore'
 import {withDisplayName} from './withDisplayName'
 
 export function createReactState<State: Object, Com: React.ComponentType<*>>(
   store: Store<State>,
   Component: Com,
 ): React.ComponentType<$Exact<$Diff<React.ElementConfig<Com>, State>>> {
-  const Store = createStoreConsumer(store)
-  const ConnectedComponent = props => (
-    <Store>{state => <Component {...props} {...state} />}</Store>
-  )
   const wrappedComponentName =
     Component.displayName || Component.name || 'Unknown'
-  return withDisplayName(`Connect(${wrappedComponentName})`, ConnectedComponent)
+  return withDisplayName(`Connect(${wrappedComponentName})`, props => (
+    <Component {...props} {...useStore(store)} />
+  ))
 }
