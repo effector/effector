73fe7e491ca57c117e5f3356d197a6ac67727115
Sergey Sova <s.sova@redmadrobot.com>
2020-04-23 13:30:49 +0300

    effector: add tests for .on([event,store], fn)

--- src/effector/__tests__/store/on.test.js
+++ src/effector/__tests__/store/on.test.js
@@ -121,3 +121,165 @@ it('replace old links', () => {
 
   expect(store.getState()).toBe('x')
 })
+
+it('supports array of events', () => {
+  const fn = jest.fn()
+  const triggerA = createEvent()
+  const triggerB = createEvent()
+
+  const b = createStore(0)
+
+  b.on([triggerA, triggerB], (b, payload) => {
+    fn({b, payload})
+    return b + 1
+  })
+
+  triggerA('lol')
+  expect(fn).toHaveBeenCalledTimes(1)
+
+  triggerB('')
+  expect(fn).toHaveBeenCalledTimes(2)
+  triggerA(' ')
+  expect(fn).toHaveBeenCalledTimes(3)
+
+  triggerB('long word')
+  expect(fn).toHaveBeenCalledTimes(4)
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "b": 0,
+        "payload": "lol",
+      },
+      Object {
+        "b": 1,
+        "payload": "",
+      },
+      Object {
+        "b": 2,
+        "payload": " ",
+      },
+      Object {
+        "b": 3,
+        "payload": "long word",
+      },
+    ]
+  `)
+})
+it('supports array of stores', () => {
+  const fn = jest.fn()
+  const hello = createEvent<string>()
+  const world = createEvent<string>()
+  const $clockA = createStore('hello').on(hello, (_, word) => word)
+  const $clockB = createStore('world').on(world, (_, word) => word)
+
+  const $source = createStore(['word'])
+
+  $source.on([$clockA, $clockB], (state, payload) => {
+    fn({state, payload})
+    return [...state, payload]
+  })
+
+  hello('lol')
+  expect(fn).toHaveBeenCalledTimes(1)
+
+  world('')
+  expect(fn).toHaveBeenCalledTimes(2)
+  hello(' ')
+  expect(fn).toHaveBeenCalledTimes(3)
+
+  world('long word')
+  expect(fn).toHaveBeenCalledTimes(4)
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "payload": "lol",
+        "state": Array [
+          "word",
+        ],
+      },
+      Object {
+        "payload": "",
+        "state": Array [
+          "word",
+          "lol",
+        ],
+      },
+      Object {
+        "payload": " ",
+        "state": Array [
+          "word",
+          "lol",
+          "",
+        ],
+      },
+      Object {
+        "payload": "long word",
+        "state": Array [
+          "word",
+          "lol",
+          "",
+          " ",
+        ],
+      },
+    ]
+  `)
+})
+
+it('supports array of stores and events', () => {
+  const fn = jest.fn()
+  const hello = createEvent<string>()
+  const world = createEvent<string>()
+  const $clockA = createStore('hello').on(hello, (_, word) => word)
+
+  const $source = createStore(['word'])
+
+  $source.on([$clockA, world], (state, payload) => {
+    fn({state, payload})
+    return [...state, payload]
+  })
+
+  hello('lol')
+  expect(fn).toHaveBeenCalledTimes(1)
+
+  world('')
+  expect(fn).toHaveBeenCalledTimes(2)
+  hello(' ')
+  expect(fn).toHaveBeenCalledTimes(3)
+
+  world('long word')
+  expect(fn).toHaveBeenCalledTimes(4)
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "payload": "lol",
+        "state": Array [
+          "word",
+        ],
+      },
+      Object {
+        "payload": "",
+        "state": Array [
+          "word",
+          "lol",
+        ],
+      },
+      Object {
+        "payload": " ",
+        "state": Array [
+          "word",
+          "lol",
+          "",
+        ],
+      },
+      Object {
+        "payload": "long word",
+        "state": Array [
+          "word",
+          "lol",
+          "",
+          " ",
+        ],
+      },
+    ]
+  `)
+})
