8361f40811004ee932c7fa2038eca6cfda5f7367
Zero Bias <zerobias@icloud.com>
2020-05-11 04:45:02 +0300

    add test for variant with key reader function

--- src/dom/__tests__/index.test.ts
+++ src/dom/__tests__/index.test.ts
@@ -552,58 +552,109 @@ describe('visible', () => {
     )
   })
 })
-
-test('variant', async () => {
-  const [s1, s2] = await exec(async () => {
-    type Text = {
-      type: 'plain' | 'bold' | 'italic'
-      text: string
-    }
-    const toItalic = createEvent()
-    const text = createStore<Text[]>([
-      {type: 'plain', text: 'Bold: '},
-      {type: 'bold', text: 'text'},
-    ])
-    text.on(toItalic, list =>
-      list.map(e => (e.type === 'bold' ? {type: 'italic', text: e.text} : e)),
-    )
-    using(el, () => {
-      h('p', () => {
-        list({source: text, key: 'text'}, ({store}) => {
-          h('div', () => {
-            const text = remap(store, 'text')
-            variant({
-              source: store,
-              key: 'type',
-              cases: {
-                plain() {
-                  h('span', {text})
+describe('variant', () => {
+  test('with string key', async () => {
+    const [s1, s2] = await exec(async () => {
+      type Text = {
+        type: 'plain' | 'bold' | 'italic'
+        text: string
+      }
+      const toItalic = createEvent()
+      const text = createStore<Text[]>([
+        {type: 'plain', text: 'Bold: '},
+        {type: 'bold', text: 'text'},
+      ])
+      text.on(toItalic, list =>
+        list.map(e => (e.type === 'bold' ? {type: 'italic', text: e.text} : e)),
+      )
+      using(el, () => {
+        h('p', () => {
+          list({source: text, key: 'text'}, ({store}) => {
+            h('div', () => {
+              const text = remap(store, 'text')
+              variant({
+                source: store,
+                key: 'type',
+                cases: {
+                  plain() {
+                    h('span', {text})
+                  },
+                  bold() {
+                    h('b', {text})
+                  },
+                  italic() {
+                    h('i', {text})
+                  },
                 },
-                bold() {
-                  h('b', {text})
-                },
-                italic() {
-                  h('i', {text})
-                },
-              },
+              })
             })
           })
         })
       })
+      await act()
+      await act(() => {
+        toItalic()
+      })
     })
-    await act()
-    await act(() => {
-      toItalic()
-    })
+    expect(s1).toMatchInlineSnapshot(
+      `"<p><div><span>Bold: </span></div><div><b>text</b></div></p>"`,
+    )
+    expect(s2).toMatchInlineSnapshot(
+      `"<p><div><span>Bold: </span></div><div><i>text</i></div></p>"`,
+    )
   })
-  expect(s1).toMatchInlineSnapshot(
-    `"<p><div><span>Bold: </span></div><div><b>text</b></div></p>"`,
-  )
-  expect(s2).toMatchInlineSnapshot(
-    `"<p><div><span>Bold: </span></div><div><i>text</i></div></p>"`,
-  )
+  test('with key function', async () => {
+    const [s1, s2] = await exec(async () => {
+      type Text = {
+        type: 'plain' | 'bold' | 'italic'
+        text: string
+      }
+      const toItalic = createEvent()
+      const text = createStore<Text[]>([
+        {type: 'plain', text: 'Bold: '},
+        {type: 'bold', text: 'text'},
+      ])
+      text.on(toItalic, list =>
+        list.map(e => (e.type === 'bold' ? {type: 'italic', text: e.text} : e)),
+      )
+      using(el, () => {
+        h('p', () => {
+          list({source: text, key: 'text'}, ({store}) => {
+            h('div', () => {
+              const text = remap(store, 'text')
+              variant({
+                source: store,
+                //@ts-ignore
+                key: ({type}) => type,
+                cases: {
+                  plain() {
+                    h('span', {text})
+                  },
+                  bold() {
+                    h('b', {text})
+                  },
+                  italic() {
+                    h('i', {text})
+                  },
+                },
+              })
+            })
+          })
+        })
+      })
+      await act()
+      await act(() => {
+        toItalic()
+      })
+    })
+    expect(s1).toMatchInlineSnapshot(
+      `"<p><div><span>Bold: </span></div><div><b>text</b></div></p>"`,
+    )
+    expect(s2).toMatchInlineSnapshot(
+      `"<p><div><span>Bold: </span></div><div><i>text</i></div></p>"`,
+    )
+  })
 })
-
 describe('node reader', () => {
   it('called after dom node mounting', async () => {
     const [s1] = await exec(async () => {
