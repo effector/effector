1844e1638d64601dd5361a78ff024491f40e45ac
Zero Bias <zerobias@icloud.com>
2020-05-22 01:37:00 +0300

    move hydration tests from effector-react to effector

--- src/effector/__tests__/fork.test.js
+++ src/effector/__tests__/fork.test.js
@@ -1,7 +1,8 @@
 //@noflow
 
-import {createDomain} from 'effector'
-import {fork, serialize, allSettled} from 'effector/fork'
+import {argumentHistory} from 'effector/fixtures'
+import {createDomain, forward, combine} from 'effector'
+import {fork, allSettled, serialize, hydrate} from 'effector/fork'
 
 it('serialize stores to object of sid as keys', () => {
   const app = createDomain()
@@ -62,7 +63,7 @@ it('serialize stores in nested domain', () => {
 `)
 })
 
-test('allSettled first argument validation', async () => {
+test('allSettled first argument validation', async() => {
   const app = createDomain()
 
   await expect(
@@ -72,4 +73,208 @@ test('allSettled first argument validation', async () => {
   ).rejects.toThrowErrorMatchingInlineSnapshot(
     `"first argument should be unit"`,
   )
+})
+
+test('watch calls during hydration', async() => {
+  const fxHandlerFn = jest.fn()
+  const storeWatchFn = jest.fn()
+  const eventWatchFn = jest.fn()
+  const combineWatchFn = jest.fn()
+  const combineUpdatesWatchFn = jest.fn()
+  const combineFnWatchFn = jest.fn()
+  const combineFnUpdatesWatchFn = jest.fn()
+  const mapWatchFn = jest.fn()
+  const mapUpdatesWatchFn = jest.fn()
+
+  const app = createDomain()
+  const start = app.createEvent()
+  const fx = app.createEffect({
+    handler: fxHandlerFn,
+  })
+
+  const store = app.store(-1).on(start, x => x + 1)
+
+  forward({
+    from: store,
+    to: fx,
+  })
+
+  const combined = combine({a: store, b: store})
+  const combinedFn = combine(store, store, (a, b) => ({a, b}))
+
+  const mapped = store.map(x => `"${x}"`)
+
+  store.watch(storeWatchFn)
+  store.updates.watch(eventWatchFn)
+  combined.watch(combineWatchFn)
+  combined.updates.watch(combineUpdatesWatchFn)
+  combinedFn.watch(combineFnWatchFn)
+  combinedFn.updates.watch(combineFnUpdatesWatchFn)
+  mapped.watch(mapWatchFn)
+  mapped.updates.watch(mapUpdatesWatchFn)
+
+  hydrate(app, {
+    values: {
+      ...serialize(fork(app)),
+      [store.sid]: 0,
+    },
+  })
+
+  await allSettled(start, {
+    scope: fork(app),
+    params: null,
+  })
+
+  expect(argumentHistory(storeWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      -1,
+      0,
+      1,
+    ]
+  `)
+  expect(argumentHistory(eventWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      1,
+    ]
+  `)
+  expect(argumentHistory(combineWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "a": -1,
+        "b": -1,
+      },
+      Object {
+        "a": 0,
+        "b": 0,
+      },
+      Object {
+        "a": 1,
+        "b": 1,
+      },
+    ]
+  `)
+  expect(argumentHistory(combineUpdatesWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "a": 1,
+        "b": 1,
+      },
+    ]
+  `)
+  expect(argumentHistory(combineFnWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "a": -1,
+        "b": -1,
+      },
+      Object {
+        "a": 0,
+        "b": 0,
+      },
+      Object {
+        "a": 1,
+        "b": 1,
+      },
+    ]
+  `)
+  expect(argumentHistory(combineFnUpdatesWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "a": 1,
+        "b": 1,
+      },
+    ]
+  `)
+  expect(argumentHistory(mapWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      "\\"-1\\"",
+      "\\"0\\"",
+      "\\"1\\"",
+    ]
+  `)
+  expect(argumentHistory(mapUpdatesWatchFn)).toMatchInlineSnapshot(`
+    Array [
+      "\\"1\\"",
+    ]
+  `)
+  expect(argumentHistory(fxHandlerFn)).toMatchInlineSnapshot(`
+    Array [
+      1,
+    ]
+  `)
+})
+
+describe('hydrate edge cases', () => {
+  test('#1', async() => {
+    const app = createDomain()
+
+    const listsContainer$ = app.createStore({
+      a: [],
+      b: [],
+    })
+
+    const greaterThan$ = app.createStore(2)
+
+    const listA$ = listsContainer$.map(x => x.a)
+    const filteredA$ = {combine}.combine(listA$, greaterThan$, (xs, gt) =>
+      xs.filter(x => x > gt),
+    )
+    const listB$ = listsContainer$.map(x => x.b)
+    const filteredB$ = {combine}.combine(listB$, greaterThan$, (xs, gt) =>
+      xs.filter(x => x > gt),
+    )
+
+    hydrate(app, {
+      values: {
+        ...serialize(fork(app)),
+        [listsContainer$.sid]: {
+          a: [0, 1, 2, 3],
+          b: [1, 8, 5],
+        },
+      },
+    })
+    expect(filteredA$.getState()).toMatchInlineSnapshot(`
+      Array [
+        3,
+      ]
+    `)
+    expect(filteredB$.getState()).toMatchInlineSnapshot(`
+      Array [
+        8,
+        5,
+      ]
+    `)
+  })
+  test('#2', async() => {
+    const app = createDomain()
+
+    const greaterThan = app.createStore(2)
+
+    const listsContainer = app.createStore({
+      a: [],
+      b: [],
+    })
+
+    const byType = listsContainer.map(val => val.a)
+    const filtered = combine(byType, greaterThan, (map, gt) =>
+      map.filter(x => x > gt),
+    )
+
+    const forked = fork(app)
+
+    hydrate(app, {
+      values: {
+        [listsContainer.sid]: {
+          a: [0, 1, 2, 3],
+          b: [1, 8, 5],
+        },
+      },
+    })
+
+    expect(filtered.getState()).toMatchInlineSnapshot(`
+      Array [
+        3,
+      ]
+    `)
+  })
 })
--- src/react/__tests__/scopes.test.tsx
+++ src/react/__tests__/scopes.test.tsx
@@ -7,7 +7,7 @@ import {createDomain, forward, sample, attach, combine} from 'effector'
 import {fork, allSettled, serialize, hydrate, Scope} from 'effector/fork'
 import {Provider, useStore, useList} from 'effector-react/ssr'
 
-it('works', async () => {
+it('works', async() => {
   const indirectCallFn = jest.fn()
 
   const app = createDomain()
@@ -117,7 +117,7 @@ it('works', async () => {
   expect(indirectCallFn).toBeCalled()
 })
 
-test('attach support', async () => {
+test('attach support', async() => {
   const indirectCallFn = jest.fn()
 
   const app = createDomain()
@@ -237,211 +237,7 @@ test('attach support', async () => {
   expect(indirectCallFn).toBeCalled()
 })
 
-test('watch calls during hydration', async () => {
-  const fxHandlerFn = jest.fn()
-  const storeWatchFn = jest.fn()
-  const eventWatchFn = jest.fn()
-  const combineWatchFn = jest.fn()
-  const combineUpdatesWatchFn = jest.fn()
-  const combineFnWatchFn = jest.fn()
-  const combineFnUpdatesWatchFn = jest.fn()
-  const mapWatchFn = jest.fn()
-  const mapUpdatesWatchFn = jest.fn()
-
-  const app = createDomain()
-  const start = app.createEvent()
-  const fx = app.createEffect({
-    handler: fxHandlerFn,
-  })
-
-  const store = app.store(-1).on(start, x => x + 1)
-
-  forward({
-    from: store,
-    to: fx,
-  })
-
-  const combined = combine({a: store, b: store})
-  const combinedFn = combine(store, store, (a, b) => ({a, b}))
-
-  const mapped = store.map(x => `"${x}"`)
-
-  store.watch(storeWatchFn)
-  store.updates.watch(eventWatchFn)
-  combined.watch(combineWatchFn)
-  combined.updates.watch(combineUpdatesWatchFn)
-  combinedFn.watch(combineFnWatchFn)
-  combinedFn.updates.watch(combineFnUpdatesWatchFn)
-  mapped.watch(mapWatchFn)
-  mapped.updates.watch(mapUpdatesWatchFn)
-
-  hydrate(app, {
-    values: {
-      ...serialize(fork(app)),
-      [store.sid as string]: 0,
-    },
-  })
-
-  await allSettled(start, {
-    scope: fork(app),
-    params: null,
-  })
-
-  expect(argumentHistory(storeWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      -1,
-      0,
-      1,
-    ]
-  `)
-  expect(argumentHistory(eventWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      1,
-    ]
-  `)
-  expect(argumentHistory(combineWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "a": -1,
-        "b": -1,
-      },
-      Object {
-        "a": 0,
-        "b": 0,
-      },
-      Object {
-        "a": 1,
-        "b": 1,
-      },
-    ]
-  `)
-  expect(argumentHistory(combineUpdatesWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "a": 1,
-        "b": 1,
-      },
-    ]
-  `)
-  expect(argumentHistory(combineFnWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "a": -1,
-        "b": -1,
-      },
-      Object {
-        "a": 0,
-        "b": 0,
-      },
-      Object {
-        "a": 1,
-        "b": 1,
-      },
-    ]
-  `)
-  expect(argumentHistory(combineFnUpdatesWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "a": 1,
-        "b": 1,
-      },
-    ]
-  `)
-  expect(argumentHistory(mapWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      "\\"-1\\"",
-      "\\"0\\"",
-      "\\"1\\"",
-    ]
-  `)
-  expect(argumentHistory(mapUpdatesWatchFn)).toMatchInlineSnapshot(`
-    Array [
-      "\\"1\\"",
-    ]
-  `)
-  expect(argumentHistory(fxHandlerFn)).toMatchInlineSnapshot(`
-    Array [
-      1,
-    ]
-  `)
-})
-
-describe('hydrate edge cases', () => {
-  test('#1', async () => {
-    const app = createDomain()
-
-    const listsContainer$ = app.createStore({
-      a: [],
-      b: [],
-    })
-
-    const greaterThan$ = app.createStore(2)
-
-    const listA$ = listsContainer$.map(x => x.a)
-    const filteredA$ = {combine}.combine(listA$, greaterThan$, (xs, gt) => {
-      return xs.filter(x => x > gt)
-    })
-    const listB$ = listsContainer$.map(x => x.b)
-    const filteredB$ = {combine}.combine(listB$, greaterThan$, (xs, gt) => {
-      return xs.filter(x => x > gt)
-    })
-
-    hydrate(app, {
-      values: {
-        ...serialize(fork(app)),
-        [listsContainer$.sid as any]: {
-          a: [0, 1, 2, 3],
-          b: [1, 8, 5],
-        },
-      },
-    })
-    expect(filteredA$.getState()).toMatchInlineSnapshot(`
-      Array [
-        3,
-      ]
-    `)
-    expect(filteredB$.getState()).toMatchInlineSnapshot(`
-      Array [
-        8,
-        5,
-      ]
-    `)
-  })
-  test('#2', async () => {
-    const app = createDomain()
-
-    const greaterThan = app.createStore(2)
-
-    const listsContainer = app.createStore({
-      a: [],
-      b: [],
-    })
-
-    const byType = listsContainer.map(val => val.a)
-    const filtered = combine(byType, greaterThan, (map, gt) =>
-      map.filter(x => x > gt),
-    )
-
-    const forked = fork(app)
-
-    hydrate(app, {
-      values: {
-        [listsContainer.sid as any]: {
-          a: [0, 1, 2, 3],
-          b: [1, 8, 5],
-        },
-      },
-    })
-
-    expect(filtered.getState()).toMatchInlineSnapshot(`
-      Array [
-        3,
-      ]
-    `)
-  })
-})
-
-test('computed values support', async () => {
+test('computed values support', async() => {
   const app = createDomain()
 
   const fetchUser = app.createEffect<string, {name: string; friends: string[]}>(
@@ -505,7 +301,7 @@ test('computed values support', async () => {
   `)
 })
 
-test('allSettled effect calls', async () => {
+test('allSettled effect calls', async() => {
   const fn = jest.fn()
   const app = createDomain()
 
