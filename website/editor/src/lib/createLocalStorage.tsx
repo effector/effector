f67a27abb12bb7e1750ea261174a1862b4a0d095
Sergey Sova <s.sova@redmadrobot.com>
2020-05-24 02:42:44 +0300

    refactor local storage lib in editor

--- website/editor/src/lib/createLocalStorage.tsx
+++ website/editor/src/lib/createLocalStorage.tsx
@@ -1,79 +1,83 @@
-import {Store} from 'effector'
-import {createStore} from 'effector'
+import {Store, createStore} from 'effector'
 import throttle from 'lodash.throttle'
+
+interface Local {
+  prevent: boolean
+  unsavedState: any | undefined
+  storageKey: string
+  save(key: string, value: any): void
+}
+
+let leavePagePrevented = false
+const stores: Array<Store<any> & Local> = []
 
 export function createLocalStore<State>(
   name: string,
   initialState: State,
-  options: {interval: number, prevent: boolean} = {},
+  options: {interval?: number; prevent?: boolean} = {},
 ): Store<State> {
   const {interval = 250, prevent = false} = options
 
-  if (!createLocalStore.preventedLeavePage) {
-    window.addEventListener('beforeunload', createLocalStore.preventLeavePage)
-    createLocalStore.preventedLeavePage = true
+  if (!leavePagePrevented) {
+    window.addEventListener('beforeunload', preventLeavePage)
+    leavePagePrevented = true
   }
 
-  function load(key: string): State {
-    try {
-      const result = localStorage.getItem(key)
-      return result === null || result === undefined
-        ? initialState
-        : JSON.parse(result)
-    } catch (e) {
-      console.error(`Can't load store '${name}' from localStorage.`)
-    }
-    return initialState
-  }
+  const $store: Store<State> & Partial<Local> = createStore(initialState, {
+    name: `Local(${name})`,
+  })
 
-  const store: Store<State> & {
-    storageKey?: string,
-    prevent?: boolean,
-    save?: Function,
-    unsavedState?: State,
-  } = createStore < State > (initialState, {name})
-  store.setState(load(name))
-  store.storageKey = name
-  store.prevent = prevent
-  store.save = (key, value) => {
+  // @ts-ignore
+  $store.setState(load(name))
+  $store.storageKey = name
+  $store.prevent = prevent
+  $store.save = (key, value) => {
     try {
       localStorage.setItem(key, JSON.stringify(value))
-      store.unsavedState = undefined
-    } catch (e) {
-      console.error(`Can't save store '${name}' to localStorage.`)
+      $store.unsavedState = undefined
+    } catch (error) {
+      console.error(`Can't save store 'Local(${name})' to localStorage.`)
     }
   }
 
-  const throttledSave = throttle(store.save, interval)
+  const throttledSave = throttle($store.save, interval)
 
-  createLocalStore.stores = createLocalStore.stores || []
-  createLocalStore.stores.push(store)
+  stores.push($store as Store<State> & Local)
 
-  store.watch(state => {
-    store.unsavedState = state
+  $store.watch(state => {
+    $store.unsavedState = state
     throttledSave(name, state)
   })
 
-  return store
+  return $store
+
+  function load(key: string): State {
+    try {
+      const result: string | null = localStorage.getItem(key)
+      return result === null || result === undefined
+        ? initialState
+        : JSON.parse(result)
+    } catch (error) {
+      console.error(`Can't load store 'Local(${name})' from localStorage.`)
+    }
+    return initialState
+  }
 }
 
-createLocalStore.preventLeavePage = e => {
-  e.preventDefault()
+function preventLeavePage(event: BeforeUnloadEvent) {
+  event.preventDefault()
 
-  if (
-    createLocalStore.stores.some(
-      store => store.prevent && store.unsavedState !== undefined,
-    )
-  ) {
-    e.returnValue = 'There is pending work. Sure you want to leave?'
+  if (stores.some(store => store.prevent && store.unsavedState !== undefined)) {
+    event.returnValue = 'There is pending work. Sure you want to leave?'
   }
 
   setTimeout(() => {
-    createLocalStore.stores.forEach(store => {
-      store.unsavedState !== undefined &&
+    stores.forEach(store => {
+      if (store.unsavedState !== undefined) {
         store.save(store.storageKey, store.unsavedState)
+      }
     })
   })
 
-  return e.returnValue
+  return event.returnValue
 }
