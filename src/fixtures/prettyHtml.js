42b3608683dcf11b81908c86933006a14878ed40
Zero Bias <zerobias@icloud.com>
2020-06-12 05:29:48 +0300

    replace " with ' in html snapshots to improve readability

--- src/fixtures/prettyHtml.js
+++ src/fixtures/prettyHtml.js
@@ -3,7 +3,9 @@ const {format} = require('prettier')
 module.exports = function prettyHtml(content) {
   if (typeof content !== 'string') return content
   try {
-    const result = format(content, {parser: 'html', printWidth: 60}).trim()
+    const result = format(content, {parser: 'html', printWidth: 60})
+      .replace(/"/g, "'")
+      .trim()
     return `\n${result}\n`
   } catch (error) {
     return content
