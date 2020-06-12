542d760fa7d9050bd7eb77325cb3b9d1ed469604
Zero Bias <zerobias@icloud.com>
2020-06-12 05:32:57 +0300

    forest 0.16.3

--- tools/builder/packages.config.js
+++ tools/builder/packages.config.js
@@ -39,7 +39,7 @@ const version = {
   effector: '20.16.1',
   'effector-react': '20.7.4',
   'effector-vue': '20.5.1',
-  forest: '0.16.2',
+  forest: '0.16.3',
   'effector-dom': '0.0.15',
 
   'bs-effector': '20.0.0',
