0c2242f85d7a6c3cf499f698f52610fad62877c3
Spoki4 <ivan.sherstnyuk@gmail.com>
2020-08-21 20:54:02 +0300

    Refactoring effector-vue mixin and fix non v-model test

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,5 +1,5 @@
 import Vue, {VueConstructor, ComponentOptions} from 'vue'
-import {createEvent, restore, is, combine, Store, withRegion, clearNode, forward} from 'effector'
+import {createEvent, restore, is, combine, Store, withRegion, clearNode, forward, Unit} from 'effector'
 
 export interface EffectorVue extends Vue {
   $watchAsStore: typeof watchAsStore;
@@ -19,82 +19,60 @@ const effectorMixin: ComponentOptions<Vue> = {
   beforeCreate() {
     let shape = this.$options.effector;
 
-    if (typeof shape === "function") shape = shape.call(this);
+    if (typeof shape === "function") {
+      // @ts-ignore
+      shape = shape.call(this)
+    }
     if (!shape) return;
     if (!this.$options.computed) this.$options.computed = {}
+
+    let obj: { [key: string]: Unit<any> } = {}
+
+    // normalize effector field data
+    if (is.store(shape)) {
+      obj = { state: shape }
+    } else if (typeof shape === 'object') {
+      obj = { ...shape }
+    } else {
+      throw Error('property should be Store')
+    }
 
     // @ts-ignore
     this.__clear = createEvent();
 
     // @ts-ignore
-    this.__forwards = [];
-    let computed: Record<string, any> = {};
-
-    // @ts-ignore
     withRegion(this.__clear, () => {
-      if (is.store(shape)) {
-        const key = 'state';
-        const reactive = Vue.observable({
-          [key]: shape.getState()
-        })
+      const state: Record<string, Store<any>> = {};
+      let nextID = 0;
 
+      for(const key in obj) {
+        const v = obj[key];
+
+        if (is.store(v)) {
+          state[key] = v;
+        } else if (is.event(v) || is.effect(v)) {
+          state[key] = restore(v.map(() => ++nextID), null);
+        } else {
+          throw Error(`Effector property ${key} should be Store or Unit (will be transform to Store<number>)`);
+        }
+      }
+
+      const store = combine(state)
+      const reactive = Vue.observable({ state: store.defaultState })
+
+      store.watch(value => reactive.state = value)
+
+      for (const key in state) {
         const updated = createEvent();
-        const unwatch = forward({from: updated, to: shape})
+        forward({ from: updated, to: state[key] });
 
         // @ts-ignore
-        this.__forwards.push(unwatch);
-        shape.watch(value => {
-          reactive[key] = value;
-        })
-
-        computed[key] = {
-          get: () => reactive[key],
+        this.$options.computed[key] = {
+          get: () => reactive.state[key],
           set: updated
         };
-      } else if (typeof shape === 'object' && shape !== null) {
-        const state: Record<string, Store<any>> = {};
-        let nextID = 0;
-
-        for(const key in shape) {
-          const v = shape[key];
-
-          if (is.store(v)) {
-            state[key] = v;
-          } else if (is.unit(v)) {
-            state[key] = restore(v.map(() => ++nextID), null);
-          } else {
-            throw Error('property should be Store or Unit (will be transform to Store<number>)');
-          }
-        }
-
-        const store = combine(state);
-        const reactive = Vue.observable(store.defaultState);
-
-        store.watch(value => {
-          for (const key in value) {
-            reactive[key] = value[key]
-          }
-        })
-
-        for (const key in reactive) {
-          const updated = createEvent();
-          const unwatch = forward({from: updated, to: state[key]});
-
-          // @ts-ignore
-          this.__forwards.push(unwatch);
-          computed[key] = {
-            get: () => reactive[key],
-            set: updated
-          };
-        }
-      } else {
-        throw Error('property should be Store')
       }
     })
-
-    for (const key in computed) {
-      this.$options.computed[key] = computed[key];
-    }
   },
 
   beforeDestroy() {
@@ -102,11 +80,6 @@ const effectorMixin: ComponentOptions<Vue> = {
     if (this.__clear) {
       // @ts-ignore
       clearNode(this.__clear);
-
-      // @ts-ignore
-      this.__forwards.forEach(unwatch => {
-        unwatch()
-      });
     }
   }
 }
