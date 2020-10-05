92e74220114926bdb8998403e50fb23b4c77e814
Zero Bias <zerobias@icloud.com>
2020-10-05 11:40:14 +0300

    add support for adding watchers for given scope

--- src/effector/watch.ts
+++ src/effector/watch.ts
@@ -6,12 +6,18 @@ import {createSubscription} from './subscription'
 import {addToRegion} from './region'
 import {throwError} from './throw'
 import {isFunction} from './is'
+import {forkPage} from './kernel'
+import {getGraph} from './getter'
 
 export const watchUnit = (
   unit: Unit,
   handler: (payload: any) => any,
 ): Subscription => {
   if (!isFunction(handler)) throwError('.watch argument should be a function')
+  if (forkPage) {
+    const forkedNode = forkPage.nodeMap[getGraph(unit).id]
+    if (forkedNode) unit = forkedNode
+  }
   return createSubscription(
     addToRegion(
       createNode({
