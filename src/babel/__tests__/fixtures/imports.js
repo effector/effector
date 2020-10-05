57d6034c0a3656bcd12ea9e3ae947bea50419486
Zero Bias <zerobias@icloud.com>
2020-10-05 11:56:02 +0300

    add test for method call without import

--- src/babel/__tests__/fixtures/imports.js
+++ src/babel/__tests__/fixtures/imports.js
@@ -4,5 +4,6 @@ import {createStore as createStoreEffector} from 'effector'
 import {createStore} from 'redux'
 
 const x = createStoreEffector(0)
+const e = createEvent()
 
 const reduxStore = createStore(() => 0)
