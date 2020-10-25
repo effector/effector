70ba022e06bba800bc32a5358a2fa687c8ed656d
Illia Osmanov (iosmanov) <iosmanov@cisco.com>
2020-10-25 13:39:15 +0200

    added types for vue3 hooks

--- packages/effector-vue/composition.d.ts
+++ packages/effector-vue/composition.d.ts
@@ -1,3 +1,7 @@
+import {DeepReadonly, Ref, UnwrapRef} from '@vue/reactivity'
 import {Store} from 'effector'
 
-export function useStore<T>(store: Store<T>): T
+type UnwrapNestedRefs<T> = T extends Ref ? T : UnwrapRef<T>
+
+export function useStore<T>(store: Store<T>): DeepReadonly<UnwrapNestedRefs<T>>
+export function useModel<T>(store: Store<T>): Ref<UnwrapRef<T>>
