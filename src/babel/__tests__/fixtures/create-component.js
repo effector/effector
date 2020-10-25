92a0142325ece64cccf635dd3170e8c9d7a5d738
Zero Bias <zerobias@icloud.com>
2020-10-25 17:50:08 +0300

    remove tests for obsolete babel-plugin-react

--- src/babel/__tests__/fixtures/create-component.js
+++
@@ -1,6 +0,0 @@
-//@flow
-import {createStore} from 'effector'
-import {createComponent} from 'effector-react'
-
-const foo = createStore('foo')
-const Foo = createComponent(foo, (props, data) => null)
