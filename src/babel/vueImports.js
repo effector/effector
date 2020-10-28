4dea268dc5910402c1546748e4860e2da45a40bd
Zero Bias <zerobias@icloud.com>
2020-10-28 22:36:53 +0300

    make vueImports plugin configurable

--- src/babel/vueImports.js
+++ src/babel/vueImports.js
@@ -1,42 +1,57 @@
 module.exports = function(babel) {
   const t = babel.types
+  function defineReplacements(replacements) {
+    const packages = replacements.map(({pkg}) => pkg)
+    return (path, ctx) => {
+      const node = path.node
+      if (!packages.includes(node.source.value)) return
+      const programPath = path.find(path => path.isProgram())
+      if (!ctx.effVue_importMap) ctx.effVue_importMap = new Set()
+      const {pkg, val} = replacements[packages.indexOf(node.source.value)]
+      const importMap = ctx.effVue_importMap
+      if (!importMap.has(pkg)) {
+        importMap.add(pkg)
+        programPath.node.body.unshift(
+          t.importDeclaration(
+            [t.importDefaultSpecifier(t.identifier(val))],
+            t.stringLiteral(pkg),
+          ),
+        )
+      }
+      const imports = []
+      for (let i = 0; i < node.specifiers.length; i++) {
+        const specifier = node.specifiers[i]
+        if (t.isImportDefaultSpecifier(specifier)) return
+        if (!t.isImportSpecifier(specifier)) continue
+        imports.push(
+          t.objectProperty(
+            t.identifier(specifier.imported.name),
+            t.identifier(specifier.local.name),
+          ),
+        )
+      }
+      path.replaceWith(
+        t.VariableDeclaration('const', [
+          t.VariableDeclarator(t.objectPattern(imports), t.identifier(val)),
+        ]),
+      )
+    }
+  }
+  const replaceImports = defineReplacements([
+    {
+      pkg: '@vue/reactivity',
+      val: 'reactivity',
+    },
+    {
+      pkg: '@vue/runtime-core',
+      val: 'runtimeCore',
+    },
+  ])
   return {
     name: 'vue-imports',
     visitor: {
-      ImportDeclaration(path, state) {
-        const node = path.node
-        if (node.source.value !== '@vue/reactivity') return
-        const programPath = path.find(path => path.isProgram())
-        if (!this.effVue_importAdded) {
-          this.effVue_importAdded = true
-          programPath.node.body.unshift(
-            t.importDeclaration(
-              [t.importDefaultSpecifier(t.identifier('reactivity'))],
-              t.stringLiteral('@vue/reactivity'),
-            ),
-          )
-        }
-
-        const imports = []
-        for (let i = 0; i < node.specifiers.length; i++) {
-          const specifier = node.specifiers[i]
-          if (t.isImportDefaultSpecifier(specifier)) return
-          if (!t.isImportSpecifier(specifier)) continue
-          imports.push(
-            t.objectProperty(
-              t.identifier(specifier.imported.name),
-              t.identifier(specifier.local.name),
-            ),
-          )
-        }
-        path.replaceWith(
-          t.VariableDeclaration('const', [
-            t.VariableDeclarator(
-              t.objectPattern(imports),
-              t.identifier('reactivity'),
-            ),
-          ]),
-        )
+      ImportDeclaration(path) {
+        replaceImports(path, this)
       },
     },
   }
