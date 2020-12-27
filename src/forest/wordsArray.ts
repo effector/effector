a2cc79b688782fc6a1a540230503531b4b0d509d
Zero Bias <zerobias@icloud.com>
2020-12-28 02:06:16 +0300

    fix edge cases in text method

--- src/forest/wordsArray.ts
+++ src/forest/wordsArray.ts
@@ -5,7 +5,11 @@ export type Word =
   | StoreOrData<string>
   | StoreOrData<number>
 
-export function createWordsArray(x: TemplateStringsArray, args: Array<Word>) {
+export function createWordsArray(
+  x: TemplateStringsArray,
+  args: Array<Word>,
+): Array<Word> {
+  if (args.length === 0) return x as any
   const words: Array<Word> = [x[0]]
   for (let i = 0; i < args.length; i++) {
     words.push(args[i], x[i + 1])
