6271834033a791e456a2ad5c8a3fa6d28775712c
Zero Bias <zerobias@icloud.com>
2021-02-22 10:15:40 +0300

    improve site main page styles

--- website/client/src/pages/index.js
+++ website/client/src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import clsx from 'clsx'
 import Layout from '@theme/Layout'
-// import CodeBlock from '@theme/CodeBlock'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
@@ -157,18 +155,114 @@ const users = [
   },
 ]
 
+function CompaniesUsingEffector() {
+  return (
+    <footer className={styles.usersSection}>
+      <header>
+        <h1 data-users-header>Companies using effector</h1>
+      </header>
+      <section data-users-logos>
+        {users.map(({url, alt, view: View, yOffset}, i) => {
+          if (!View)
+            return (
+              <div key={i} data-users-logo data-logo-offset={yOffset}>
+                <img alt={alt} src={url} title={alt} />
+              </div>
+            )
+          return (
+            <div key={i} data-users-logo>
+              <View title={alt} />
+            </div>
+          )
+        })}
+      </section>
+      <div data-users-add-yours>
+        <i>
+          <span data-users-add-yours-plain-text>
+            Want to appear on this page?
+          </span>{' '}
+          <a href="https://github.com/effector/effector/issues/278">Tell us</a>
+        </i>
+      </div>
+    </footer>
+  )
+}
+
+function FeatureGrid() {
+  return (
+    <main>
+      <section className={styles.features}>
+        <div className="container">
+          <div className="row">
+            {features.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
 function Feature({imageUrl, title, description}) {
   const imgUrl = useBaseUrl(imageUrl)
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt="" />
-        </div>
-      )}
+    <div className="col col--4">
+      <div className="text--center">
+        <img data-feature-image src={imgUrl} alt={title} />
+      </div>
       <h3 className="text--center">{title}</h3>
       <p className="text--center">{description}</p>
     </div>
+  )
+}
+
+function Hero() {
+  const {siteConfig = {}} = useDocusaurusContext()
+  return (
+    <header className={`hero ${styles.heroBanner}`}>
+      <div className="container">
+        <div className="row" data-hero-row>
+          <div className="col col--6 margin-bottom--md" data-hero-content>
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div data-hero-controls>
+              <Link
+                className="button button--outline button--primary button--lg"
+                to={useBaseUrl('docs/introduction/installation')}>
+                Get Started
+              </Link>
+              <Link
+                className="button button--outline button--secondary button--lg"
+                to="https://share.effector.dev">
+                Try it out
+              </Link>
+            </div>
+            <div data-hero-explainer="first-row">
+              Explainer{' '}
+              <Link
+                to="https://www.youtube.com/watch?v=rslADuhtF4Y"
+                id="explainer_video_link"
+                key="explainer_video_link">
+                video
+              </Link>
+            </div>
+            <div data-hero-explainer="second-row">
+              Explainer{' '}
+              <Link
+                to="https://dev.to/yanlobat/effector-s-beginner-guide-3jl4"
+                id="explainer_article_link"
+                key="explainer_article_link">
+                article
+              </Link>
+            </div>
+          </div>
+          <div className="col col--6" data-hero-code>
+            <Code language="js">{codeExample}</Code>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
@@ -177,100 +271,9 @@ function Home() {
   const {siteConfig = {}} = context
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <header className={clsx('hero', styles.heroBanner)}>
-        <div className="container">
-          <div className={clsx('row', styles.rowRevers)}>
-            <div
-              className={clsx('col col--6 margin-bottom--md', styles.buttons)}>
-              <h1 className="hero__title">{siteConfig.title}</h1>
-              <p className="hero__subtitle">{siteConfig.tagline}</p>
-              <div className={styles.buttons}>
-                <Link
-                  className={clsx(
-                    'button button--outline button--primary button--lg',
-                    styles.getStarted,
-                  )}
-                  to={useBaseUrl('docs/introduction/installation')}>
-                  Get Started
-                </Link>
-                <Link
-                  className={
-                    'button button--outline button--secondary button--lg'
-                  }
-                  to="https://share.effector.dev">
-                  Try it out
-                </Link>
-              </div>
-              <div style={{marginTop: '30px'}}>
-                Explainer{' '}
-                <Link
-                  to="https://www.youtube.com/watch?v=rslADuhtF4Y"
-                  id="explainer_video_link"
-                  key="explainer_video_link">
-                  video
-                </Link>
-              </div>
-              <div style={{marginTop: '10px'}}>
-                Explainer{' '}
-                <Link
-                  to="https://dev.to/yanlobat/effector-s-beginner-guide-3jl4"
-                  id="explainer_article_link"
-                  key="explainer_article_link">
-                  article
-                </Link>
-              </div>
-            </div>
-            <div
-              className="col col--6"
-              style={{fontSize: 'calc(var(--ifm-code-font-size) * 0.9)'}}>
-              <Code language="js">{codeExample}</Code>
-            </div>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-      <footer className={styles.usersSection}>
-        <header>
-          <h1 data-users-header>Companies using effector</h1>
-        </header>
-        <section data-users-logos>
-          {users.map(({url, alt, view: View, yOffset}, i) => {
-            if (!View)
-              return (
-                <div key={i} data-users-logo data-logo-offset={yOffset}>
-                  <img alt={alt} src={url} title={alt} />
-                </div>
-              )
-            return (
-              <div key={i} data-users-logo>
-                <View title={alt} />
-              </div>
-            )
-          })}
-        </section>
-        <div data-users-add-yours>
-          <i>
-            <span data-users-add-yours-plain-text>
-              Want to appear on this page?
-            </span>{' '}
-            <a href="https://github.com/effector/effector/issues/278">
-              Tell us
-            </a>
-          </i>
-        </div>
-      </footer>
+      <Hero />
+      <FeatureGrid />
+      <CompaniesUsingEffector />
     </Layout>
   )
 }
