122ae4ee8ce11fa3f9ff30cb30b6d7743c65044d
Zero Bias <zerobias@icloud.com>
2021-03-17 13:21:00 +0300

    move createApi type tests to separate module

--- src/types/__tests__/effector/store.test.ts
+++ src/types/__tests__/effector/store.test.ts
@@ -3,14 +3,13 @@ import {
   createStore,
   createEvent,
   createEffect,
-  createApi,
   createStoreObject,
   restore,
   combine,
   Store,
   Event,
-  /*::type*/ CompositeName,
-  /*::type*/ kind,
+  CompositeName,
+  kind,
   sample,
 } from 'effector'
 
@@ -36,82 +35,6 @@ test('createStoreObject', () => {
   c.on(ev, (state, payload) => state)
   c.reset(ev)
   c.off(ev)
-  expect(typecheck).toMatchInlineSnapshot(`
-    "
-    no errors
-    "
-  `)
-})
-describe('createApi', () => {
-  const store: Store<number> = createStore(0)
-  test('check1', () => {
-    const {event} = createApi(store, {
-      event: (n, x: number) => x,
-    })
-    const createApi_check1: Event<number> = event
-    expect(typecheck).toMatchInlineSnapshot(`
-      "
-      no errors
-      "
-    `)
-  })
-  test('check2', () => {
-    const {event} = createApi(store, {
-      event: (n, x: number) => x,
-    })
-    //@ts-expect-error
-    const createApi_check2: Event<string> = event
-    expect(typecheck).toMatchInlineSnapshot(`
-      "
-      Type 'Event<number>' is not assignable to type 'Event<string>'.
-      "
-    `)
-  })
-  test('check3', () => {
-    const {event} = createApi(store, {
-      event: (n, x) => x,
-    })
-    //@ts-expect-error
-    const createApi_check3: Event<string> = event
-    expect(typecheck).toMatchInlineSnapshot(`
-      "
-      Type 'Event<void>' is not assignable to type 'Event<string>'.
-        Types of property 'watch' are incompatible.
-          Type '(watcher: (payload: void) => any) => Subscription' is not assignable to type '(watcher: (payload: string) => any) => Subscription'.
-            Types of parameters 'watcher' and 'watcher' are incompatible.
-              Types of parameters 'payload' and 'payload' are incompatible.
-                Type 'void' is not assignable to type 'string'.
-      "
-    `)
-  })
-})
-test('createApi void calls', () => {
-  const store = createStore(0)
-  const api = createApi(store, {
-    increment: count => count + 1,
-    decrement: count => count - 1,
-    double: count => count * 2,
-    multiply: (count, mp = 2) => count * mp,
-  })
-
-  api.double() // Expected 1 arguments, but got 0.
-  api.multiply() // Expected 1 arguments, but got 0.
-  expect(typecheck).toMatchInlineSnapshot(`
-    "
-    no errors
-    "
-  `)
-})
-test('createApi unknown/void support', () => {
-  const store = createStore(0)
-  const {foo, bar, baz} = createApi(store, {
-    foo: (_, __: void) => 0,
-    bar: (_, __: unknown) => 0,
-    baz: (_, __) => 0,
-  })
-  const unknownEvent: Event<unknown> = bar
-  const explicitVoid: Event<void> = foo
-  const implicitVoid: Event<void> = baz
   expect(typecheck).toMatchInlineSnapshot(`
     "
     no errors
