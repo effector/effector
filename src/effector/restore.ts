73bac543db33b0f2d227f985edbb59963124a0ba
Zero Bias <zerobias@icloud.com>
2021-08-23 22:06:55 +0300

    simplify restore

--- src/effector/restore.ts
+++ src/effector/restore.ts
@@ -6,27 +6,15 @@ import {OPEN_O} from './tag'
 import {forward} from './forward'
 
 export function restore(obj: any, defaultState: any, config?: any): any {
-  if (is.store(obj)) {
-    return obj
-  }
-  if (is.unit(obj)) {
+  if (is.store(obj)) return obj
+  if (is.event(obj) || is.effect(obj)) {
     const domain = getParent(obj)
-    let result
-    if (is.event(obj)) {
-      result = createStore(defaultState, {
-        parent: domain,
-        name: obj.shortName,
-        [OPEN_O]: config,
-      }).on(obj, (_, v) => v)
-    }
-    if (is.effect(obj)) {
-      result = createStore(defaultState, {
-        parent: domain,
-        name: obj.shortName,
-        [OPEN_O]: config,
-      })
-      forward({from: obj.doneData, to: result})
-    }
+    const result = createStore(defaultState, {
+      parent: domain,
+      name: obj.shortName,
+      [OPEN_O]: config,
+    })
+    forward({from: is.effect(obj) ? obj.doneData : obj, to: result})
     if (domain) domain.hooks.store(result)
     return result
   }
