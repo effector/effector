78be4a6d7eb4641161d2d2c84e9aca8d9d43f540
Sergey Sova <mail@sergeysova.com>
2022-05-14 11:37:29 +0400

    Add compat createGate snapshot test for babel plugin

--- src/babel/__tests__/fixtures/create-gate.js
+++ src/babel/__tests__/fixtures/create-gate.js
@@ -1,6 +1,7 @@
 import {createDomain} from 'effector'
 import {createGate} from 'effector-react'
 import {createGate as renamedGate} from 'effector-react/scope'
+import {createGate as compatGate} from 'effector-react/compat'
 
 const Gate1 = createGate()
 const Gate2 = createGate('gate2')
@@ -13,6 +14,14 @@ const Gate3 = createGate({
 const TheGate1 = renamedGate()
 const TheGate2 = renamedGate('gate2')
 const TheGate3 = renamedGate({
+  defaultState: {a: 1},
+  domain: createDomain(),
+  name: 'gate3',
+})
+
+const CompatGate1 = compatGate()
+const CompatGate2 = compatGate('gate2')
+const CompatGate3 = compatGate({
   defaultState: {a: 1},
   domain: createDomain(),
   name: 'gate3',
