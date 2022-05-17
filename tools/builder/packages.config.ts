cd899603f96e8f292199de03436a94958f8d17cc
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2022-05-17 13:33:26 +0300

    Fix quotes in effector-react source package

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -160,7 +160,7 @@ export default {
     'jsnext:main': 'effector-react.mjs',
     typings: 'index.d.ts',
     dependencies: {
-      "use-sync-external-store": "^1.0.0",
+      'use-sync-external-store': '^1.0.0',
     },
     peerDependencies: {
       react: '>=16.8.0 <19.0.0',
