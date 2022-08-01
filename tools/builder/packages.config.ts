01f98d69fedff6487e07aafad6d05f832cd3dd56
Dmitry Boldyrev <ribkatt@gmail.com>
2022-08-01 13:08:22 +0600

    effector-react 22.1.6; effector-solid 0.22.2

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -51,9 +51,9 @@ const keywords = [
 
 const version = {
   effector: '22.3.0',
-  'effector-react': '22.1.5',
+  'effector-react': '22.1.6',
   'effector-vue': '22.1.1',
-  'effector-solid': '0.22.1',
+  'effector-solid': '0.22.2',
   forest: '0.21.1',
 }
 
