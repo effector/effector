2891476f05383f26dc6b270442795e36469b539d
Igor Kamyshev <igor@kamyshev.me>
2022-09-21 12:52:16 +0700

    Prepare release effector-vue 22.2.0

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -62,7 +62,7 @@ const keywords = [
 const version = {
   effector: '22.3.0',
   'effector-react': '22.3.0',
-  'effector-vue': '22.1.2',
+  'effector-vue': '22.2.0',
   'effector-solid': '0.22.6',
   forest: '0.21.2',
 }
