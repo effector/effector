801324ce034cfdd5d6869dba6febd211e6580e9c
AlexandrHoroshih <xad71rus@gmail.com>
2022-09-30 23:43:26 +0700

    Add tests for basic scenarios

--- src/effector/__tests__/observable.test.ts
+++ src/effector/__tests__/observable.test.ts
@@ -1,6 +1,6 @@
 import * as redux from 'redux'
 import {from, periodic} from 'most'
-import {from as rxjsFrom} from 'rxjs'
+import {from as rxjsFrom, interval} from 'rxjs'
 import {
   fromObservable,
   createEvent,
@@ -9,6 +9,27 @@ import {
   createDomain,
 } from 'effector'
 import {argumentHistory} from 'effector/fixtures'
+
+function createEmitter<T>() {
+  const emitter = {
+    listener: (() => {}) as (p: T) => void,
+    subscribe: (cb: (p: T) => void) => {
+      emitter.listener = cb
+
+      return () => {
+        emitter.listener = () => {}
+      }
+    },
+    emit: (p: T) => {
+      emitter.listener(p)
+    },
+  }
+  return emitter
+}
+
+async function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 describe('fromObservable', () => {
   it('works with typical Symbol.observable library: redux', () => {
@@ -236,4 +257,171 @@ test('most support', async () => {
       "b",
     ]
   `)
+})
+
+describe('object shape', () => {
+  it('should work with observable and start event', () => {
+    const stream$ = rxjsFrom([1, 2, 3])
+    const appStarted = createEvent()
+    const event = fromObservable({
+      start: appStarted,
+      setup: stream$,
+    })
+    const mock = jest.fn()
+    event.watch(mock)
+    expect(mock).not.toBeCalled()
+
+    appStarted()
+
+    expect(mock).toBeCalledTimes(3)
+    expect(argumentHistory(mock)).toEqual([1, 2, 3])
+  })
+
+  it('should work with observable and start and stop events', async () => {
+    const stream$ = interval(10)
+    const appStarted = createEvent()
+    const appStopped = createEvent()
+    const event = fromObservable({
+      start: appStarted,
+      stop: appStopped,
+      setup: stream$,
+    })
+    const mock = jest.fn()
+    event.watch(mock)
+    expect(mock).not.toBeCalled()
+
+    await delay(20)
+
+    appStarted()
+
+    await delay(35)
+
+    expect(mock).toBeCalledTimes(3)
+    expect(argumentHistory(mock)).toMatchInlineSnapshot(`
+      Array [
+        0,
+        1,
+        2,
+      ]
+    `)
+
+    appStopped()
+
+    await delay(20)
+
+    expect(mock).toBeCalledTimes(3)
+  })
+
+  it('should work with function and start event', async () => {
+    const emitter = createEmitter<number>()
+
+    const appStarted = createEvent()
+    const event = fromObservable<number>({
+      start: appStarted,
+      setup: listener => {
+        emitter.subscribe(listener)
+      },
+    })
+    const mock = jest.fn()
+    event.watch(mock)
+
+    emitter.emit(1)
+    expect(mock).not.toBeCalled()
+
+    appStarted()
+
+    emitter.emit(1)
+    emitter.emit(1)
+    emitter.emit(1)
+
+    expect(mock).toBeCalledTimes(3)
+    expect(argumentHistory(mock)).toMatchInlineSnapshot(`
+      Array [
+        1,
+        1,
+        1,
+      ]
+    `)
+  })
+
+  it('should work with function and start and stop event', async () => {
+    const emitter = createEmitter<number>()
+
+    const appStarted = createEvent()
+    const appStopped = createEvent()
+    const event = fromObservable<number>({
+      start: appStarted,
+      stop: appStopped,
+      setup: listener => {
+        return emitter.subscribe(listener)
+      },
+    })
+    const mock = jest.fn()
+    event.watch(mock)
+
+    emitter.emit(1)
+    expect(mock).not.toBeCalled()
+
+    appStarted()
+
+    emitter.emit(1)
+    emitter.emit(1)
+    emitter.emit(1)
+
+    expect(mock).toBeCalledTimes(3)
+    expect(argumentHistory(mock)).toMatchInlineSnapshot(`
+      Array [
+        1,
+        1,
+        1,
+      ]
+    `)
+
+    appStopped()
+
+    emitter.emit(1)
+
+    expect(mock).toBeCalledTimes(3)
+  })
+
+  it('should work with function and start and stop event and source', async () => {
+    const emitter = createEmitter<number>()
+
+    const $emitter = createStore(emitter)
+    const appStarted = createEvent()
+    const appStopped = createEvent()
+    const event = fromObservable({
+      source: $emitter,
+      start: appStarted,
+      setup: (listener, em) => {
+        return em.subscribe(listener)
+      },
+    })
+    const mock = jest.fn()
+    event.watch(mock)
+
+    emitter.emit(1)
+    expect(mock).not.toBeCalled()
+
+    appStarted()
+
+    emitter.emit(1)
+    emitter.emit(1)
+    emitter.emit(1)
+
+    expect(mock).toBeCalledTimes(3)
+    expect(argumentHistory(mock)).toMatchInlineSnapshot(`
+      Array [
+        1,
+        1,
+        1,
+      ]
+    `)
+
+    appStopped()
+
+    emitter.emit(1)
+
+    expect(mock).toBeCalledTimes(3)
+  })
 })
