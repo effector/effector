a47bd598465169540e7ed1e6aa422bda63cbb7ca
AlexandrHoroshih <xad71rus@gmail.com>
2022-11-08 00:14:15 +0700

    Refactor fork implementation
    - use common type for fork target
    - add new handlers on top of the old ones

--- src/effector/fork/fork.ts
+++ src/effector/fork/fork.ts
@@ -3,54 +3,44 @@ import {assert} from '../throw'
 import type {Domain, ValuesMap, HandlersMap, Scope} from '../unit.h'
 import {normalizeValues} from './util'
 import {createScope} from './createScope'
-import {forEach} from '../collection'
 
 type ForkConfig = {
   values?: ValuesMap
   handlers?: HandlersMap
-  scope?: Scope
 }
 
 export function fork(
-  domainOrConfig?: Domain | ForkConfig,
-  optiionalConfig?: ForkConfig,
+  targetOrConfig?: Domain | ForkConfig | Scope,
+  optionalConfig?: ForkConfig,
 ) {
-  let config: ForkConfig | void = domainOrConfig as any
-  let domain: Domain
-  if (is.domain(domainOrConfig)) {
-    domain = domainOrConfig
-    config = optiionalConfig
+  const hasTarget = is.unit(targetOrConfig)
+  let target: Domain | Scope | undefined = hasTarget
+    ? targetOrConfig
+    : undefined
+  let config: ForkConfig | undefined = hasTarget
+    ? optionalConfig
+    : targetOrConfig
+
+  const scope = createScope(target)
+
+  if (config?.values) {
+    const valuesSidMap = normalizeValues(config.values, unit =>
+      assert(is.store(unit), 'Values map can contain only stores as keys'),
+    )
+    Object.assign(scope.sidValuesMap, valuesSidMap)
+    scope.fromSerialize =
+      !Array.isArray(config.values) && !(config.values instanceof Map)
   }
-
-  const scope = createScope(domain!)
-
-  if (config) {
-    const oldScope = config.scope
-    if (oldScope) {
-      // Update old scope scopeRef
-      // to redirect all effect.finally and scope-binded events to new scope
-      oldScope.scopeRef.ref = scope
-
-      // transfer old scope scopeRef to the new scope,
-      // so any further fork(scope) calls will also redirect any effect.finally and scope-binded events to new scope
-      scope.scopeRef = oldScope.scopeRef
-    }
-    if (config.values) {
-      const valuesSidMap = normalizeValues(config.values, unit =>
-        assert(is.store(unit), 'Values map can contain only stores as keys'),
-      )
-      Object.assign(scope.sidValuesMap, valuesSidMap)
-      scope.fromSerialize =
-        !Array.isArray(config.values) && !(config.values instanceof Map)
-    }
-    if (config.handlers) {
-      scope.handlers = normalizeValues(config.handlers, unit =>
+  if (config?.handlers) {
+    Object.assign(
+      scope.handlers,
+      normalizeValues(config.handlers, unit =>
         assert(
           is.effect(unit),
           `Handlers map can contain only effects as keys`,
         ),
-      )
-    }
+      ),
+    )
   }
   return scope
 }
