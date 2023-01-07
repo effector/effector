7b0b76651dd30c615dac952377ff291d24bea96d
Dmitry Boldyrev <ribkatt@gmail.com>
2023-01-07 15:00:12 +0600

    Add fxID tests for effects in computation chain
    This commit should fail

--- src/effector/__tests__/effect/fxID.test.ts
+++ src/effector/__tests__/effect/fxID.test.ts
@@ -1,4 +1,14 @@
-import {attach, createEffect, Unit, Node, step} from 'effector'
+import {
+  attach,
+  sample,
+  createEvent,
+  createEffect,
+  Unit,
+  Node,
+  step,
+  allSettled,
+  fork,
+} from 'effector'
 
 function getNode(unit: Unit<any>): Node {
   //@ts-expect-error
@@ -193,5 +203,75 @@ describe('attach support', () => {
       await attached()
       expect(typeof fxID).toBe('string')
     })
+  })
+})
+
+describe('computation chain support', () => {
+  test('fxID in fx seq', async () => {
+    const trigger = createEvent()
+    const fx = createEffect(() => {})
+    sample({clock: trigger, target: fx})
+    let fxID: any
+    getNode(fx).seq.push(
+      step.compute({
+        fn(data, _, stack) {
+          fxID = stack.meta?.fxID
+          return data
+        },
+      }),
+    )
+    await allSettled(trigger, {scope: fork()})
+    expect(typeof fxID).toBe('string')
+  })
+  test('fxID in fx.finally seq', async () => {
+    const trigger = createEvent()
+    const fx = createEffect(() => {})
+    sample({clock: trigger, target: fx})
+    let fxID: any
+    getNode(fx.finally).seq.push(
+      step.compute({
+        fn(data, _, stack) {
+          fxID = stack.meta?.fxID
+          return data
+        },
+      }),
+    )
+    await allSettled(trigger, {scope: fork()})
+    expect(typeof fxID).toBe('string')
+  })
+  test('fxID in fx.done seq', async () => {
+    const trigger = createEvent()
+    const fx = createEffect(() => {})
+    sample({clock: trigger, target: fx})
+    let fxID: any
+    getNode(fx.done).seq.push(
+      step.compute({
+        fn(data, _, stack) {
+          const finallyStack = stack.parent?.parent
+          fxID = finallyStack?.meta?.fxID
+          return data
+        },
+      }),
+    )
+    await allSettled(trigger, {scope: fork()})
+    expect(typeof fxID).toBe('string')
+  })
+  test('fxID in fx.doneData seq', async () => {
+    const trigger = createEvent()
+    const fx = createEffect(() => {})
+    sample({clock: trigger, target: fx})
+    let fxID: any
+    getNode(fx.doneData).seq.push(
+      step.compute({
+        fn(data, _, stack) {
+          const doneStack = stack.parent?.parent
+          const finallyStack = doneStack?.parent?.parent
+          fxID = finallyStack?.meta?.fxID
+          return data
+        },
+      }),
+    )
+    await allSettled(trigger, {scope: fork()})
+    expect(typeof fxID).toBe('string')
   })
 })
