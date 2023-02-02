f74a156f51e9451cf6217505ce28f846209d4f0e
Dmitry Boldyrev <ribkatt@gmail.com>
2023-02-02 23:59:43 +0600

    Remove deprecated createReactState and createContextComponent

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,7 +1,5 @@
 export {createStoreConsumer} from './createStoreConsumer'
-export {createContextComponent} from './createContextComponent'
 export {createComponent} from './createComponent'
 export {connect} from './connect'
-export {createReactState} from './createReactState'
 export {createGate, useGate} from './createGate'
 export {useEvent, useList, useStore, useStoreMap, useUnit} from './nossr'
