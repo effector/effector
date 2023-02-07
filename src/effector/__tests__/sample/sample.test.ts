69bf17565dbfed7a0771ecd08890397c30aab90b
Dmitry Boldyrev <ribkatt@gmail.com>
2023-02-08 01:04:02 +0600

    Add test for non-batched case for returning to old behavior

--- src/effector/__tests__/sample/sample.test.ts
+++ src/effector/__tests__/sample/sample.test.ts
@@ -779,4 +779,35 @@ describe('event/effect sampling behavior (issue #633)', () => {
     */
     expect(argumentHistory(fn)).toEqual([1, 2])
   })
+
+  test('non-batched effect behavior', () => {
+    const fn = jest.fn()
+    const triggerEffect = createEvent()
+
+    const targetFx = createEffect(() => {})
+    const initFx = createEffect(() => {})
+
+    sample({
+      clock: triggerEffect,
+      target: [initFx.prepend(() => 1), initFx.prepend(() => 2)],
+    })
+
+    sample({
+      clock: initFx,
+      filter: targetFx.pending.map(val => !val),
+      target: targetFx,
+      greedy: true,
+    })
+
+    targetFx.watch(params => fn(params))
+
+    triggerEffect()
+    /*
+    [effect] targetFx 1
+    [effect] targetFx 2
+    [effect] targetFx.done {params: 1, result: undefined}
+    [effect] targetFx.done {params: 2, result: undefined}
+    */
+    expect(argumentHistory(fn)).toEqual([1, 2])
+  })
 })
