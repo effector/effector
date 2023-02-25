94d9316e2da6e5be1b2b0f7b014913ac2db15d58
AlexandrHoroshih <xad71rus@gmail.com>
2023-02-25 17:46:30 +0700

    Add tests for real-world use-cases

--- src/effector/__tests__/inspect.test.ts
+++ src/effector/__tests__/inspect.test.ts
@@ -9,6 +9,7 @@ import {
   combine,
 } from 'effector'
 import {argumentHistory} from 'effector/fixtures'
+import {performance} from 'perf_hooks'
 
 function compactMessage(m: Message) {
   return `${m.type} of '${m.name}' [${m.kind}] to value of '${
@@ -187,12 +188,6 @@ describe('inspect API', () => {
 })
 
 function compactDeclaration(d: Declaration) {
-  if (d.type === 'factory') {
-    return `factory call: ${d.method} of ${d.from} to ${
-      d.name
-    } variable (parent factory: ${typeof d.factory}, sid: ${typeof d.sid}, loc: ${typeof d.loc})`
-  }
-
   return `${d.derived ? 'derived ' : ''}${d.type} ${d.name} (${
     d.kind
   }) created (sid ${typeof d.sid}, parent factory: ${typeof d.factory}, id: ${typeof d.id}, loc: ${typeof d.loc})`
@@ -244,4 +239,211 @@ describe('inspectGraph API', () => {
 
     unsub()
   })
+})
+
+describe('real use cases', () => {
+  test('measure effect timings', async () => {
+    const start = createEvent()
+
+    const fx1 = createEffect(() => new Promise(r => setTimeout(r, 12)))
+    const fx2 = createEffect(() => new Promise(r => setTimeout(r, 22)))
+    const fx3 = createEffect(() => new Promise(r => setTimeout(r, 32)))
+
+    sample({
+      clock: start,
+      target: [fx1, fx2, fx3],
+    })
+
+    const scope = fork()
+
+    const times: Record<string, number> = {}
+    const startRecord = (name: string) => {
+      const start = Date.now()
+
+      return () => {
+        times[name] = Date.now() - start
+      }
+    }
+
+    const timers = new Map<string, () => void>()
+
+    const unsub = inspect({
+      scope,
+      fn: m => {
+        if (m.kind === 'effect') {
+          timers.set(m.stack.fxID as string, startRecord(m.name!))
+        }
+        if (m.kind === 'event' && m.meta.named === 'finally') {
+          const stop = timers.get(m.stack.fxID as string)
+          stop?.()
+        }
+      },
+    })
+
+    await allSettled(start, {scope})
+
+    const floor = (n: number) => Math.floor(n / 10) * 10
+
+    expect(floor(times.fx1)).toEqual(10)
+    expect(floor(times.fx2)).toEqual(20)
+    expect(floor(times.fx3)).toEqual(30)
+
+    unsub()
+  })
+  test('monitor out-of-scope computations', async () => {
+    const start = createEvent()
+
+    const scope = fork()
+
+    const outOfScope = jest.fn()
+    inspect({
+      fn: () => outOfScope(),
+    })
+
+    allSettled(start, {scope})
+
+    expect(outOfScope).not.toBeCalled()
+
+    start()
+
+    expect(outOfScope).toBeCalled()
+  })
+  test('monitor sid-less stores', async () => {
+    const missingSid = jest.fn()
+
+    inspectGraph({
+      fn: d => {
+        if (d.kind === 'store' && !d.sid) missingSid(d.name)
+      },
+    })
+
+    const $a = createStore(null)
+    const $b = createStore(null, {sid: null as unknown as string})
+    const $c = createStore(null)
+
+    expect(missingSid).toBeCalledTimes(1)
+    expect(missingSid).toBeCalledWith('$b')
+  })
+  test('monitor stores with duplicated sid`s', async () => {
+    const duplicatedSid = jest.fn()
+
+    const sidMap: Record<string, boolean> = {}
+
+    inspectGraph({
+      fn: d => {
+        if (d.kind === 'store' && d.sid) {
+          if (sidMap[d.sid]) {
+            duplicatedSid(d.name)
+          } else {
+            sidMap[d.sid] = true
+          }
+        }
+      },
+    })
+
+    const $a = createStore(null, {sid: '$a'})
+    const $b = createStore(null)
+    const $c = createStore(null, {sid: '$a'})
+
+    expect(duplicatedSid).toBeCalledTimes(1)
+  })
+  test('profile computations', async () => {
+    const start = createEvent()
+
+    const end = sample({
+      clock: start,
+      fn: () => {
+        let c = 0
+        while (c < 10_000) {
+          c++
+        }
+      },
+    })
+
+    const timeLog = jest.fn()
+    let time = 0
+    let tracking = false
+
+    const scope = fork()
+
+    inspect({
+      scope,
+      fn: m => {
+        if (!tracking) {
+          tracking = true
+          time = performance.now()
+          queueMicrotask(() => {
+            tracking = false
+            timeLog({starter: m.name, ms: performance.now() - time})
+          })
+        }
+      },
+    })
+
+    await allSettled(start, {scope})
+
+    expect(tracking).toBe(false)
+    expect(timeLog).toBeCalledTimes(1)
+    expect(timeLog).toBeCalledWith({starter: 'start', ms: expect.any(Number)})
+    expect(argumentHistory(timeLog)[0].ms).toBeGreaterThan(0)
+  })
+  test('list units by file', () => {
+    const unitsByFile: Record<string, string[]> = {}
+    inspectGraph({
+      fn: d => {
+        if (d.loc) {
+          const file = d.loc.file.split('/').at(-1) || ''
+          const units = unitsByFile[file] || []
+          units.push(d.name!)
+          unitsByFile[file] = units
+        }
+      },
+    })
+    const $a = createStore(null)
+    const $b = createStore(null)
+    const $c = createStore(null)
+
+    expect(unitsByFile).toEqual({
+      'inspect.test.ts': ['$a', '$b', '$c'],
+    })
+  })
+  test('track pure function errors in custom way', () => {
+    // something that logs errors directly into our monitoring systems
+    // providing additional context
+    const appLogger = {
+      log: jest.fn(message => ({
+        message,
+        logContext: {
+          appName: 'my-app',
+          appVersion: '1.0.0',
+        },
+      })),
+    }
+
+    const start = createEvent()
+    const started = sample({
+      clock: start,
+      fn: () => {
+        throw new Error('unexpected error, branch computation stopped')
+      },
+    })
+
+    const scope = fork()
+
+    inspect({
+      scope,
+      fn: m => {
+        if (m.type === 'error') {
+          appLogger.log(`name: ${m.name}, error: ${(m.value as Error).message}`)
+        }
+      },
+    })
+
+    allSettled(start, {scope})
+
+    expect(argumentHistory(appLogger.log)[0]).toEqual(
+      'name: started, error: unexpected error, branch computation stopped',
+    )
+  })
+  test.todo('custom factories and operators handling')
 })
