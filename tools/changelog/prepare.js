7812e0cdae9057943eddb72c95c2361b84716425
AlexandrHoroshih <xad71rus@gmail.com>
2023-03-12 01:31:16 +0700

    Init changesets setup

---
+++ tools/changelog/prepare.js
@@ -0,0 +1,18 @@
+/**
+ * @type{import('@changesets/types').GetReleaseLine}
+ */
+async function getReleaseLine(changeset, _type) {
+
+}
+
+/**
+ * @type{import('@changesets/types').GetDependencyReleaseLine}
+ */
+async function getDependencyReleaseLine(changesets, dependenciesUpdated) {
+
+}
+
+module.exports = {
+  getReleaseLine,
+  getDependencyReleaseLine,
+}
