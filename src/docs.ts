54eaf4d80ef88e85f7f85b28e538bff6c963b242
Sergey Sova <mail@sergeysova.com>
2023-03-13 14:51:43 +0400

    Add breadcrumbs

--- src/docs.ts
+++ src/docs.ts
@@ -1,11 +1,13 @@
 import { CollectionEntry, getCollection } from "astro:content";
 import { getPathParamsFromId } from "./languages";
 
+type DocsEntry = CollectionEntry<"docs">;
+
 export async function getTranslatedDocs() {
   // Slugs without lang prefix is required to generate all documents even translated yet.
   const sourceSlugs = new Set<string>();
   // Slug WITH lang prefix per entry mapping, here the translated pages
-  const docs: Record<string, CollectionEntry<"docs">> = {};
+  const docs: Record<string, DocsEntry> = {};
 
   const docsList = await getCollection("docs");
 
@@ -19,5 +21,30 @@ export async function getTranslatedDocs() {
     docs[`${lang}/${slug}`] = doc;
   });
 
-  return { docs, sourceSlugs: Array.from(sourceSlugs) };
+  return { docs, sourceSlugs };
+}
+
+export function getBreadcrumbs({
+  path: originalPath,
+  docs,
+}: {
+  path: string;
+  docs: Record<string, DocsEntry>;
+}) {
+  const path = originalPath.replace(/\/$/, "").replace(/^\//, "");
+  const breadcrumbs = [];
+
+  let foundIndex = 0;
+  while ((foundIndex = path.indexOf("/", foundIndex + 1)) !== -1) {
+    const part = path.slice(0, foundIndex);
+    breadcrumbs.push(part);
+  }
+  breadcrumbs.push(path);
+
+  return breadcrumbs
+    .filter((path) => docs[path])
+    .map((path) => ({
+      title: docs[path].data.title,
+      path: `/${path}`,
+    }));
 }
