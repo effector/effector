8b96be141b13d6144dcfd84920fbea3ca58cb33d
Sergey Sova <mail@sergeysova.com>
2023-03-14 15:09:22 +0400

    Remove extra / from github edit url

--- src/consts.ts
+++ src/consts.ts
@@ -30,7 +30,7 @@ export const GITHUB_DOCS_ROOT = ""; // there would be 'beta/' slash at the end i
 // Used to convert pathname into file path in the repository
 export const GITHUB_DOCS_FILE_PREFIX = "src/content/docs";
 
-export const GITHUB_EDIT_URL = `https://github.com/${GITHUB_REPO}/edit/${GITHUB_BRANCH}/`;
+export const GITHUB_EDIT_URL = `https://github.com/${GITHUB_REPO}/edit/${GITHUB_BRANCH}`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.gg/yHcMcuRWeC`;
 
