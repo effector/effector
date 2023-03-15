daa1770173f07285cc4390a595caccc55b0236bb
AlexandrHoroshih <xad71rus@gmail.com>
2023-03-15 21:39:24 +0700

    Add test for a bug

--- src/react/__tests__/base/createGate.test.tsx
+++ src/react/__tests__/base/createGate.test.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
 //@ts-expect-error
 import {render, cleanup, container, act} from 'effector/fixtures/react'
-import {createGate, useGate, useStore} from 'effector-react'
+import {createGate, useGate, useStore, Provider} from 'effector-react'
 import {createGate as createGateScope} from 'effector-react/scope'
 
 import {argumentHistory} from 'effector/fixtures'
 import {
   allSettled,
+  createEffect,
   createEvent,
   createStore,
   fork,
   forward,
+  sample,
   serialize,
 } from 'effector'
 
@@ -216,6 +218,30 @@ test('gate should be correctly serialized via fork #672', async () => {
 
   const states = serialize(scope)
   expect(states[Gate.state.sid!]).toBe('another')
+})
+
+test('gate component should be isomorphic to scope', async () => {
+  const Gate = createGate('default')
+  const logged = jest.fn()
+  const logFx = createEffect(() => {})
+
+  sample({
+    clock: Gate.open,
+    target: logFx,
+  })
+
+  const scope = fork({
+    handlers: [[logFx, () => logged('scoped')]],
+  })
+
+  await render(
+    <Provider value={scope}>
+      <Gate />
+    </Provider>,
+  )
+
+  expect(logged).toBeCalledTimes(1)
+  expect(logged).toBeCalledWith('scoped')
 })
 
 test('gate properties hook', async () => {
