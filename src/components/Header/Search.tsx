3bd4ea3a1ce1d0eea93b2e64234dcecab07abfd8
Sergey Sova <mail@sergeysova.com>
2023-03-19 13:41:40 +0400

    Slightly redesign search, add Lexend Deca Light Otf

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,18 +1,11 @@
 /** @jsxImportSource react */
 import { useState, useCallback, useRef } from "react";
-import { ALGOLIA } from "../../consts";
-import "@docsearch/css";
-import "./Search.css";
-
 import { createPortal } from "react-dom";
-import * as docSearchReact from "@docsearch/react";
+import { useDocSearchKeyboardEvents, DocSearchModal } from "@docsearch/react";
+import "@docsearch/css";
 
-/** FIXME: This is still kinda nasty, but DocSearch is not ESM ready. */
-const DocSearchModal =
-  docSearchReact.DocSearchModal || (docSearchReact as any).default.DocSearchModal;
-const useDocSearchKeyboardEvents =
-  docSearchReact.useDocSearchKeyboardEvents ||
-  (docSearchReact as any).default.useDocSearchKeyboardEvents;
+import { ALGOLIA } from "../../consts";
+import "./Search.css";
 
 export default function Search() {
   const [isOpen, setIsOpen] = useState(false);
@@ -46,14 +39,20 @@ export default function Search() {
   return (
     <>
       <button type="button" ref={searchButtonRef} onClick={onOpen} className="search-input">
-        <svg width="18" height="18" fill="none" viewBox="0 0 24 24">
-          <path
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <circle cx="10" cy="10" r="7" />
+          <line x1="21" y1="21" x2="15" y2="15" />
         </svg>
 
         <span className="search-text">Search</span>
@@ -77,8 +76,8 @@ export default function Search() {
             indexName={ALGOLIA.indexName}
             appId={ALGOLIA.appId}
             apiKey={ALGOLIA.apiKey}
-            transformItems={(items) => {
-              return items.map((item) => {
+            transformItems={(items) =>
+              items.map((item) => {
                 // We transform the absolute URL into a relative URL to
                 // work better on localhost, preview URLS.
                 const a = document.createElement("a");
@@ -88,8 +87,8 @@ export default function Search() {
                   ...item,
                   url: `${a.pathname}${hash}`,
                 };
-              });
-            }}
+              })
+            }
           />,
           document.body,
         )}
