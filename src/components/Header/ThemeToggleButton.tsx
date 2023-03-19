f59a76692c32a10ccd926b254fea5689f13a3253
Sergey Sova <mail@sergeysova.com>
2023-03-19 15:30:30 +0400

    Redesign language switcher
    EFX-22

--- src/components/Header/ThemeToggleButton.tsx
+++ src/components/Header/ThemeToggleButton.tsx
@@ -1,62 +1,102 @@
+/** @jsxImportSource preact */
 import type { FunctionalComponent } from "preact";
 import { useState, useEffect } from "preact/hooks";
 import "./ThemeToggleButton.css";
 
-const themes = ["light", "dark"];
+const themes = ["light", "auto", "dark"];
 
 const icons = [
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="currentColor"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
   >
-    <path
-      fillRule="evenodd"
-      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-      clipRule="evenodd"
-    />
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="12" cy="12" r="4" />
+    <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
   </svg>,
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="currentColor"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
   >
-    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="12" cy="12" r="9" />
+    <path d="M13 12h5" />
+    <path d="M13 15h4" />
+    <path d="M13 18h1" />
+    <path d="M13 9h4" />
+    <path d="M13 6h1" />
+  </svg>,
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+    <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2" />
+    <path d="M19 11h2m-1 -1v2" />
   </svg>,
 ];
 
 const ThemeToggle: FunctionalComponent = () => {
-  const [theme, setTheme] = useState(() => {
+  const [selectedTheme, setSelectedTheme] = useState(() => {
     if (import.meta.env.SSR) {
       return undefined;
     }
     if (typeof localStorage !== undefined && localStorage.getItem("theme")) {
       return localStorage.getItem("theme");
     }
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    }
-    return "light";
+    return "auto";
   });
 
   useEffect(() => {
     const root = document.documentElement;
-    if (theme === "light") {
-      root.classList.remove("theme-dark");
-    } else {
-      root.classList.add("theme-dark");
+    function setTheme(color: string) {
+      if (color === "light") {
+        root.classList.remove("theme-dark");
+      } else {
+        root.classList.add("theme-dark");
+      }
     }
-  }, [theme]);
+
+    if (selectedTheme === "auto") {
+      const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+      setAutomaticTheme();
+      matcher.addEventListener("change", setAutomaticTheme);
+      return () => matcher.removeEventListener("change", setAutomaticTheme);
+      function setAutomaticTheme() {
+        setTheme(matcher.matches ? "dark" : "light");
+      }
+    } else {
+      setTheme(selectedTheme || "light");
+    }
+  }, [selectedTheme]);
 
   return (
     <div className="theme-toggle">
       {themes.map((t, i) => {
         const icon = icons[i];
-        const checked = t === theme;
+        const checked = t === selectedTheme;
         return (
           <label className={checked ? " checked" : ""}>
             {icon}
@@ -69,7 +109,7 @@ const ThemeToggle: FunctionalComponent = () => {
               aria-label={`Use ${t} theme`}
               onChange={() => {
                 localStorage.setItem("theme", t);
-                setTheme(t);
+                setSelectedTheme(t);
               }}
             />
           </label>
