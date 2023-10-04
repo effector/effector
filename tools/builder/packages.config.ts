3854f4bcac9da06983d266678c96bcb7b4f043dc
AlexandrHoroshih <xad71rus@gmail.com>
2023-10-04 21:08:43 +0700

    Set rc versions

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -60,10 +60,10 @@ const keywords = [
 ]
 
 const version = {
-  effector: '23.0.0-rc.1',
-  'effector-react': '23.0.0-rc.1',
+  effector: '23.0.0-rc.0',
+  'effector-react': '23.0.0-rc.0',
   'effector-vue': '22.2.0',
-  'effector-solid': '0.23.0-rc.1',
+  'effector-solid': '0.23.0-rc.0',
   forest: '0.21.2',
 }
 
