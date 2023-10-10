9b94d18e2a34acfa56bae8b5522bf2f27337361e
AlexandrHoroshih <xad71rus@gmail.com>
2023-10-10 11:24:15 +0700

    Release rc 2

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -60,10 +60,10 @@ const keywords = [
 ]
 
 const version = {
-  effector: '23.0.0-rc.1',
-  'effector-react': '23.0.0-rc.1',
+  effector: '23.0.0-rc.2',
+  'effector-react': '23.0.0-rc.2',
   'effector-vue': '22.2.0',
-  'effector-solid': '0.23.0-rc.1',
+  'effector-solid': '0.23.0-rc.2',
   forest: '0.21.2',
 }
 
