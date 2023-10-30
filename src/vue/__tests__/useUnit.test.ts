49526e51245c3cbe89fe5c86461a4e0e2e53f1c9
Igor Kamyshev <igor@kamyshev.me>
2023-10-30 17:28:56 +0700

    Add useUnit for vue/composition

---
+++ src/vue/__tests__/useUnit.test.ts
@@ -0,0 +1,118 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import {shallowMount} from 'vue-test-utils-next'
+import {useUnit} from 'effector-vue/composition'
+import {createEvent, createStore, createWatch} from 'effector'
+
+jest.mock('vue', () => require('vue-next'))
+
+describe('mirror useStore tests', () => {
+  it('list of primitive values rendered correct', () => {
+    const length = 3
+    const $numbers = createStore(Array.from({length}, (_, idx) => idx))
+    const wrapper = shallowMount({
+      template: `
+          <ul id="app">
+            <li v-for="(n, key) in numbers" :key="key" data-test="item">{{n}}</li>
+          </ul>
+        `,
+      setup() {
+        const numbers = useUnit($numbers)
+        return {numbers}
+      },
+    })
+    expect(wrapper.findAll('[data-test="item"]')).toHaveLength(length)
+  })
+
+  it('add new item and re-render list', async () => {
+    const userAdded = createEvent()
+    const $users = createStore([{name: 'John', surname: 'Doe'}])
+
+    $users.on(userAdded, state => [...state, {name: 'Alan', surname: 'Doe'}])
+
+    const wrapper = shallowMount({
+      template: `
+          <ul id="app">
+            <li v-for="(item, key) in users" :key="key" data-test="item">{{item.name}}</li>
+          </ul>
+        `,
+      setup() {
+        const users = useUnit($users)
+        return {users}
+      },
+    })
+    userAdded()
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('[data-test="item"]')).toHaveLength(2)
+  })
+})
+
+describe('useUnit', () => {
+  describe('scopeless', () => {
+    it('returns event as function', async () => {
+      const someEvent = createEvent()
+      const wrapper = shallowMount({
+        template: `
+          <button data-test="btn" @click="greet">Click me</button>
+        `,
+        setup() {
+          const greet = useUnit(someEvent)
+          return {greet}
+        },
+      })
+      const listener = jest.fn()
+      const unwatch = createWatch({unit: someEvent, fn: listener})
+
+      wrapper.find('[data-test="btn"]').trigger('click')
+
+      await wrapper.vm.$nextTick()
+
+      expect(listener).toBeCalledTimes(1)
+
+      unwatch()
+    })
+
+    it('supports objects of stores', async () => {
+      const userAdded = createEvent()
+      const $users = createStore([{name: 'John', surname: 'Doe'}]).on(
+        userAdded,
+        state => [...state, {name: 'Alan', surname: 'Doe'}],
+      )
+
+      const titleChanged = createEvent<string>()
+      const $title = createStore('Hello').on(titleChanged, (_, title) => title)
+
+      const wrapper = shallowMount({
+        template: `
+              <h1 data-test="title">{{title}}</h1>
+              <ul id="app">
+                <li v-for="(item, key) in users" :key="key" data-test="item">{{item.name}}</li>
+              </ul>
+            `,
+        setup() {
+          const {users, title} = useUnit({users: $users, title: $title})
+          return {users, title}
+        },
+      })
+      expect(wrapper.findAll('[data-test="item"]')).toHaveLength(1)
+      expect(wrapper.find('[data-test="title"]').element.textContent).toBe(
+        'Hello',
+      )
+
+      userAdded()
+
+      await wrapper.vm.$nextTick()
+      expect(wrapper.findAll('[data-test="item"]')).toHaveLength(2)
+      expect(wrapper.find('[data-test="title"]').element.textContent).toBe(
+        'Hello',
+      )
+
+      titleChanged('World')
+      await wrapper.vm.$nextTick()
+      expect(wrapper.findAll('[data-test="item"]')).toHaveLength(2)
+      expect(wrapper.find('[data-test="title"]').element.textContent).toBe(
+        'World',
+      )
+    })
+  })
+})
---
+++ src/vue/useUnit.ts
@@ -0,0 +1,99 @@
+import {is, Store, createWatch, Unit, Event, scopeBind} from 'effector'
+import {onUnmounted, readonly, shallowRef} from 'vue-next'
+
+import {stateReader} from './lib/state-reader'
+import {getScope} from './lib/get-scope'
+import {throwError} from './lib/throw'
+
+export function useUnit<Shape extends {[key: string]: Unit<any>}>(
+  config: Shape | {'@@unitShape': () => Shape},
+) {
+  const {scope} = getScope()
+
+  const isSingleUnit = is.unit(config)
+  let normShape: {[key: string]: Unit<any>} = {}
+  if (isSingleUnit) {
+    normShape = {unit: config}
+  } else if ('@@unitShape' in config) {
+    if (typeof config['@@unitShape'] === 'function') {
+      normShape = config['@@unitShape']()
+    } else {
+      throwError('expect @@unitShape to be a function')
+    }
+  } else {
+    normShape = config
+  }
+  const isList = Array.isArray(normShape)
+
+  const shape = isList ? [] : ({} as any)
+  const storeKeys: string[] = []
+  const storeValues: Array<Store<any>> = []
+  const eventKeys: string[] = []
+  const eventValues: Array<Unit<any>> = []
+  for (const key in normShape) {
+    const unit = normShape[key]
+    if (!is.unit(unit)) throwError('expect useUnit argument to be a unit')
+    if (is.event(unit) || is.effect(unit)) {
+      shape[key] = scope ? scopeBind(unit as Event<any>, {scope}) : unit
+      eventKeys.push(key)
+      eventValues.push(unit)
+    } else {
+      shape[key] = null
+      storeKeys.push(key)
+      storeValues.push(unit as Store<any>)
+    }
+  }
+
+  const states: Record<string, any> = {}
+  for (const key of storeKeys) {
+    const state = stateReader(normShape[key])
+    const _ = shallowRef(state)
+    const stop = createWatch({
+      unit: normShape[key],
+      fn: value => {
+        _.value = shallowRef(value).value
+      },
+      scope,
+    })
+
+    states[key] = {
+      state,
+      stop,
+      _,
+    }
+  }
+
+  onUnmounted(() => {
+    for (const val of Object.values(states)) {
+      val.stop()
+    }
+  })
+
+  if (isSingleUnit && is.store(config)) {
+    return readonly(states.unit._)
+  }
+
+  if (isSingleUnit && is.event(config)) {
+    if (scope) {
+      return scopeBind(config, {scope})
+    } else {
+      return config
+    }
+  }
+
+  const result: Record<string, any> = {}
+
+  for (const key of eventKeys) {
+    if (scope) {
+      result[key] = scopeBind(normShape[key], {scope})
+    } else {
+      result[key] = normShape[key]
+    }
+  }
+
+  for (const [key, value] of Object.entries(states)) {
+    result[key] = readonly(value._)
+  }
+
+  return result
+}
