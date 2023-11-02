c5a8d02516ee85b41b90ad48aa01efbc2ac80b01
Igor Kamyshev <igor@kamyshev.me>
2023-11-02 13:46:11 +0700

    New deprecations

--- packages/effector-vue/ssr.d.ts
+++ packages/effector-vue/ssr.d.ts
@@ -1,5 +1,8 @@
-import {Plugin} from 'vue';
-import {Scope, Event} from 'effector';
+import {Plugin} from 'vue'
+import {Scope, Event} from 'effector'
 
+/** @deprecated since v23.0.0 */
 export function VueSSRPlugin(config: {scope: Scope; scopeName?: string}): Plugin
+
+/** @deprecated since v23.0.0 */
 export function useEvent<T>(event: Event<T>): (payload: T) => void
