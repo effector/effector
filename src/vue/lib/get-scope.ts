76d74edfb0b4d14d498d1e2eaa6767b1c52f4b99
Igor Kamyshev <igor@kamyshev.me>
2023-11-03 18:57:30 +0700

    Add test for EffectorScopePlugin and fix bug in useUnit

--- src/vue/lib/get-scope.ts
+++ src/vue/lib/get-scope.ts
@@ -1,10 +1,11 @@
-import {Scope} from "effector"
-import {getCurrentInstance, inject} from "vue-next"
+import {Scope} from 'effector'
+import {getCurrentInstance, inject} from 'vue-next'
 
 export function getScope() {
   let scope: Scope | undefined
   let ctx = getCurrentInstance()
-  let scopeName: string | undefined = ctx?.appContext.config.globalProperties.scopeName
+  let scopeName: string | undefined =
+    ctx?.appContext.config.globalProperties.scopeName
 
   if (scopeName) {
     scope = inject(scopeName)
