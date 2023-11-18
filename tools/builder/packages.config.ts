93824314689f5890389bd8b9c106136fe8f7a029
AlexandrHoroshih <xad71rus@gmail.com>
2023-11-18 15:45:06 +0700

    Release rc-5

--- tools/builder/packages.config.ts
+++ tools/builder/packages.config.ts
@@ -60,10 +60,10 @@ const keywords = [
 ]
 
 const version = {
-  effector: '23.0.0-rc.4',
-  'effector-react': '23.0.0-rc.4',
-  'effector-vue': '23.0.0-rc.4',
-  'effector-solid': '0.23.0-rc.4',
+  effector: '23.0.0-rc.5',
+  'effector-react': '23.0.0-rc.5',
+  'effector-vue': '23.0.0-rc.5',
+  'effector-solid': '0.23.0-rc.5',
   forest: '0.21.2',
 }
 
