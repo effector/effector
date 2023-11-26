0c59f3465970499d008b4513fa85a140232348d4
AlexandrHoroshih <xad71rus@gmail.com>
2023-11-26 16:44:55 +0700

    Refactor

--- src/effector/fork/scopeBind.ts
+++ src/effector/fork/scopeBind.ts
@@ -8,29 +8,30 @@ export function scopeBind(
   {scope, safe}: {scope?: Scope; safe?: true} = {},
 ) {
   assert(scope || forkPage || safe, 'scopeBind: scope not found')
-  const savedForkPage = scope || forkPage!
+  const targetForkPage = scope || forkPage!
 
   return (x: any) => {
     let final: any
     let failed = false
 
     const lastForkPage = forkPage
-    setForkPage(savedForkPage)
+    function restoreLastForkPage() {
+      setForkPage(lastForkPage)
+    }
+
+    setForkPage(targetForkPage)
     try {
       final = unit(x)
     } catch (err) {
       final = err
       failed = true
     }
-    setForkPage(lastForkPage)
+    restoreLastForkPage()
 
     if (failed) throw final
 
     if (final instanceof Promise) {
-      function restore() {
-        setForkPage(lastForkPage)
-      }
-      final.then(restore, restore)
+      final.then(restoreLastForkPage, restoreLastForkPage)
     }
 
     return final
