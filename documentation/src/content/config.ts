5d3aefab90afd53b97d629c29ac99ac5497cc5e0
Sergey Sova <mail@sergeysova.com>
2023-11-30 18:23:54 +0400

    Ignore redirect pages in search

--- documentation/src/content/config.ts
+++ documentation/src/content/config.ts
@@ -5,7 +5,9 @@ const docs = defineCollection({
   schema: z.object({
     title: z.string().default(SITE.title),
     description: z.string().default(SITE.description),
-    lang: z.union([z.literal("en"), z.literal("ru"), z.literal("uz‘")]).default(SITE.defaultLanguage),
+    lang: z
+      .union([z.literal("en"), z.literal("ru"), z.literal("uz")])
+      .default(SITE.defaultLanguage),
     dir: z.union([z.literal("ltr"), z.literal("rtl")]).default("ltr"),
     image: z
       .object({
