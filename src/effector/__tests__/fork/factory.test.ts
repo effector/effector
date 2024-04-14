cf2a29fc9ecde14f03608965b764840868c40f48
AlexandrHoroshih <xad71rus@gmail.com>
2024-04-14 19:14:53 +0700

    Add test for derived sids support

--- src/effector/__tests__/fork/factory.test.ts
+++ src/effector/__tests__/fork/factory.test.ts
@@ -1,5 +1,5 @@
-import {fork, serialize} from 'effector'
-import {createField, createFieldset} from './factory'
+import {allSettled, createEvent, createStore, fork, serialize} from 'effector'
+import {createField, createFieldset, topLevelFactory} from './factory'
 
 test('factory support', async () => {
   const username = createField('username', 'guest')
@@ -33,6 +33,41 @@ test('nested factory support', async () => {
     Object {
       "-fjbluz|-fjbluz|1104zu|1|2|-77rc2s|2": "alice",
       "-fjbluz|-fjbluz|11jxl7|3|4|-77rc2s|2": 21,
+    }
+  `)
+})
+
+test('derived sids support', async () => {
+  const pushUpdate = createEvent()
+  const inlineFactory = () => {
+    const $interalStore = createStore(0, {
+      // not-unique sid
+      sid: '$interalStore',
+    }).on(pushUpdate, state => state + 1)
+
+    return {
+      $interalStore,
+    }
+  }
+
+  topLevelFactory(() => {
+    inlineFactory() // 1
+    inlineFactory() // 2
+
+    topLevelFactory(() => {
+      inlineFactory() // 3
+    })
+  })
+
+  const scope = fork()
+
+  await allSettled(pushUpdate, {scope})
+
+  expect(serialize(scope)).toMatchInlineSnapshot(`
+    Object {
+      "ot2oxd|$interalStore|1": 1,
+      "ot2oxd|$interalStore|4": 1,
+      "ot2oxd|ot2oxd|ov9vcj|6|7|$interalStore|1": 1,
     }
   `)
 })
