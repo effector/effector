94f2781f64c0dc6601b7b6ba57a7ce3127e27037
Igor Kamyshev <igor@kamyshev.me>
2024-06-20 09:04:13 +0700

    Delete unused method from old implementation of fork

--- src/effector/fork/util.ts
+++ src/effector/fork/util.ts
@@ -1,39 +1,8 @@
-import {getMeta, getOwners, getLinks} from '../getter'
 import {is} from '../is'
 import {assert} from '../throw'
 import type {Store, Effect, ValuesMap, HandlersMap} from '../unit.h'
-import type {Node, Unit} from '../index.h'
-import {add, forEach, includes} from '../collection'
-import {STORE} from '../tag'
-
-export function traverseStores(
-  root: Node,
-  fn: (node: Node, sid: string | null) => void,
-  needSidlessStores: true,
-): void
-export function traverseStores(
-  root: Node,
-  fn: (node: Node, sid: string) => void,
-): void
-export function traverseStores(
-  root: Node,
-  fn: (node: Node, sid: string) => void,
-  needSidlessStores?: boolean,
-) {
-  const list = [] as Node[]
-  ;(function visit(node) {
-    if (includes(list, node)) return
-    add(list, node)
-    if (getMeta(node, 'op') === STORE) {
-      if (needSidlessStores || getMeta(node, 'sid')) {
-        fn(node, getMeta(node, 'sid'))
-      }
-    }
-    forEach(node.next, visit)
-    forEach(getOwners(node), visit)
-    forEach(getLinks(node), visit)
-  })(root)
-}
+import type {Unit} from '../index.h'
+import {forEach} from '../collection'
 
 type StoreOrEffect = Store<any> | Effect<any, any, any>
 
