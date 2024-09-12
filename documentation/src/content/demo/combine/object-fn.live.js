d9b0d04454409cda9e7165be9c297144be99a3bd
Dmitry <zerobias@icloud.com>
2024-09-12 15:58:44 +0500

    Improve combine docs (#1164)
    * Add new chapter to combine api, add note about purity, refactor demo
    
    * Fix minor issues
    
    * Make right sidebar more compact

--- documentation/src/content/demo/combine/object-fn.live.js
+++ documentation/src/content/demo/combine/object-fn.live.js
@@ -4,8 +4,5 @@ const $r = createStore(255);
 const $g = createStore(0);
 const $b = createStore(255);
 
-const $color = combine({ r: $r, g: $g, b: $b });
-$color.watch(console.log); // => {r: 255, b: 0, b: 255}
-
 const $sum = combine({ r: $r, g: $g, b: $b }, ({ r, g, b }) => r + g + b);
 $sum.watch(console.log); // => 510
