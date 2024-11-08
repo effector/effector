b98a3fde800e82ba34f5b333dac428e2eec540ca
Dmitry Boldyrev <ribkatt@gmail.com>
2024-11-08 18:29:29 +0500

    Add test for filter inference issue

--- src/types/__tests__/effector/sampleFilter.test.ts
+++ src/types/__tests__/effector/sampleFilter.test.ts
@@ -757,3 +757,24 @@ test('incorrect filter (should fail)', () => {
     "
   `)
 })
+
+test('type inference issue', () => {
+  const nullableAB = createEvent<{a: string; b: number} | null>()
+  const strt = createEvent<number>()
+
+  sample({
+    clock: nullableAB,
+    target: strt,
+    filter: clk => clk !== null,
+    //@ts-expect-error
+    fn: val => ({a: 1, b: val ? val.b : ''}),
+  })
+
+  expect(typecheck).toMatchInlineSnapshot(`
+    "
+    Unmarked error at test line 5 'clock: nullableAB,'
+    Object literal may only specify known properties, and 'clock' does not exist in type '{ error: \\"fn result should extend target type\\"; targets: { fnResult: { a: number; b: string | number; }; targetType: number; }; }'.
+    lack of expected error at test line 9 'fn: val => ({a: 1, b: val ? val.b : ''}),'
+    "
+  `)
+})
