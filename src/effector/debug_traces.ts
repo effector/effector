b11582c412dcbc7efa1795642fd48bd84a4cc1d0
AlexandrHoroshih <xad71rus@gmail.com>
2025-07-18 12:43:14 +0700

    Simpler warning

--- src/effector/debug_traces.ts
+++ src/effector/debug_traces.ts
@@ -4,19 +4,7 @@ export function setDebugTraces(value: boolean) {
   enabled = value
 
   if (value) {
-    console.error(
-      new Error(
-        `
----
- The 'effector/enable_debug_traces' module was imported and debug traces are now enabled.
-
- You will see a detailed trace to the unit location in error messages, where applicable.
-
- Note that using this API may cause a significant performance impact, so it is not recommended for use in production.
----
-        `,
-      ),
-    )
+    console.log(`[effector/debug_traces] Debug traces enabled`)
   }
 }
 
